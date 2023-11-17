9fb793d57f772ff4514baed5d4ae8587e31182b9
Jon Häggblad <jon.haggblad@gmail.com>
2023-11-17 11:46:48 +0100

    Add TaggedPacket

--- src/mixnet_processor.rs
+++ src/mixnet_processor.rs
@@ -9,6 +9,7 @@ use nym_sdk::mixnet::{
     IncludedSurbs, MixnetClient, MixnetClientBuilder, MixnetMessageSender, Recipient, StoragePaths,
 };
 use nym_task::{TaskClient, TaskManager};
+use serde::{Deserialize, Serialize};
 use std::net::IpAddr;
 use std::path::PathBuf;
 use talpid_routing::{Node, RequiredRoute, RouteManager};
@@ -57,6 +58,36 @@ pub struct MixnetProcessor {
     recipient: Recipient,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct TaggedPacket {
+    packet: bytes::Bytes,
+    return_address: Recipient,
+    return_mix_hops: Option<u8>,
+}
+
+impl TaggedPacket {
+    fn new(packet: bytes::Bytes, return_address: Recipient, return_mix_hops: Option<u8>) -> Self {
+        TaggedPacket {
+            packet,
+            return_address,
+            return_mix_hops,
+        }
+    }
+    fn to_tagged_bytes(&self) -> Result<Vec<u8>, bincode::Error> {
+        use bincode::Options;
+        let bincode_serializer = make_bincode_serializer();
+        let packet: Vec<u8> = bincode_serializer.serialize(self)?;
+        Ok(packet)
+    }
+}
+
+fn make_bincode_serializer() -> impl bincode::Options {
+    use bincode::Options;
+    bincode::DefaultOptions::new()
+        .with_big_endian()
+        .with_varint_encoding()
+}
+
 impl MixnetProcessor {
     pub fn new(device: AsyncDevice, mixnet_client: MixnetClient, recipient: Recipient) -> Self {
         MixnetProcessor {
@@ -77,13 +108,24 @@ impl MixnetProcessor {
         let mut mixnet_stream = self
             .mixnet_client
             .map(|reconstructed_message| Ok(TunPacket::new(reconstructed_message.message.clone())));
+
         while !shutdown.is_shutdown() {
             tokio::select! {
                 _ = shutdown.recv() => {
                     trace!("MixnetProcessor: Received shutdown");
                 }
                 Some(Ok(packet)) = stream.next() => {
-                    let ret = sender.send_message(recipient, packet.get_bytes(), IncludedSurbs::ExposeSelfAddress).await;
+                    // TODO: properly investigate the binary format here and the overheard
+                    let Ok(packet) = TaggedPacket::new(packet.into_bytes(), recipient, None).to_tagged_bytes() else {
+                        error!("Failed to serialize packet");
+                        continue;
+                    };
+
+                    // The enum here about IncludedSurbs and ExposeSelfAddress is misleading. It is
+                    // not being used. Basically IncludedSurbs::ExposeSelfAddress just omits the
+                    // surbs, assuming that it is exposed in side the message. (This is the case
+                    // for SOCKS5 too).
+                    let ret = sender.send_message(recipient, &packet, IncludedSurbs::ExposeSelfAddress).await;
                     if ret.is_err() {
                         error!("Could not forward IP packet to the mixnet. The packet will be dropped.");
                     }
