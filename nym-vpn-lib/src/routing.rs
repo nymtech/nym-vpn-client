3b71dc7b0c009c1b2c5788068e3ba746ef833c60
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2023-12-14 13:20:16 +0200

    Switch linux to ip, which comes preinstalled

--- nym-vpn-lib/src/routing.rs
+++ nym-vpn-lib/src/routing.rs
@@ -170,6 +170,19 @@ pub async fn setup_routing(
         device_mtu = dev.get_ref().mtu(),
     );
 
+    #[cfg(target_os = "linux")]
+    Command::new("ip")
+        .args([
+            "-6",
+            "addr",
+            "add",
+            "fda7:576d:ac1a::1/48",
+            "dev",
+            &device_name,
+        ])
+        .spawn()?;
+
+    #[cfg(target_os = "macos")]
     Command::new("ifconfig")
         .args([&device_name, "inet6", "add", "fda7:576d:ac1a::1/48"])
         .spawn()?;
