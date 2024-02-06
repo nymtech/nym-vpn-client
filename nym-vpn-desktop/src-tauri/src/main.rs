b0b7ba6bbce6d4a1257ddb909ad955bac54acffd
Jon Häggblad <jon.haggblad@gmail.com>
2024-02-06 10:40:30 +0100

    rustfmt

--- nym-vpn-desktop/src-tauri/src/main.rs
+++ nym-vpn-desktop/src-tauri/src/main.rs
@@ -94,16 +94,46 @@ async fn main() -> Result<()> {
         std::env::set_var("STAKE_DENOM", "unyx");
         std::env::set_var("STAKE_DENOM_DISPLAY", "nyx");
         std::env::set_var("DENOMS_EXPONENT", "6");
-        std::env::set_var("REWARDING_VALIDATOR_ADDRESS", "n1pefc2utwpy5w78p2kqdsfmpjxfwmn9d39k5mqa");
-        std::env::set_var("MIXNET_CONTRACT_ADDRESS", "n1xr3rq8yvd7qplsw5yx90ftsr2zdhg4e9z60h5duusgxpv72hud3sjkxkav");
-        std::env::set_var("VESTING_CONTRACT_ADDRESS", "n1unyuj8qnmygvzuex3dwmg9yzt9alhvyeat0uu0jedg2wj33efl5qackslz");
-        std::env::set_var("COCONUT_BANDWIDTH_CONTRACT_ADDRESS", "n13902g92xfefeyzuyed49snlm5fxv5ms6mdq5kvrut27hasdw5a9q9vyw6c");
-        std::env::set_var("GROUP_CONTRACT_ADDRESS", "n18nczmqw6adwxg2wnlef3hf0etf8anccafp2pjpul5rrtmv96umyq5mv7t5");
-        std::env::set_var("MULTISIG_CONTRACT_ADDRESS", "n1q3zzxl78rlmxv3vn0uf4vkyz285lk8q2xzne299yt9x6mpfgk90qukuzmv");
-        std::env::set_var("COCONUT_DKG_CONTRACT_ADDRESS", "n1jsz20ggp5a6v76j060erkzvxmeus8htlpl77yxp878f0gf95cyaq6p2pee");
-        std::env::set_var("NAME_SERVICE_CONTRACT_ADDRESS", "n12ne7qtmdwd0j03t9t5es8md66wq4e5xg9neladrsag8fx3y89rcs36asfp");
-        std::env::set_var("SERVICE_PROVIDER_DIRECTORY_CONTRACT_ADDRESS", "n1ps5yutd7sufwg058qd7ac7ldnlazsvmhzqwucsfxmm445d70u8asqxpur4");
-        std::env::set_var("EPHEMERA_CONTRACT_ADDRESS", "n19lc9u84cz0yz3fww5283nucc9yvr8gsjmgeul0");
+        std::env::set_var(
+            "REWARDING_VALIDATOR_ADDRESS",
+            "n1pefc2utwpy5w78p2kqdsfmpjxfwmn9d39k5mqa",
+        );
+        std::env::set_var(
+            "MIXNET_CONTRACT_ADDRESS",
+            "n1xr3rq8yvd7qplsw5yx90ftsr2zdhg4e9z60h5duusgxpv72hud3sjkxkav",
+        );
+        std::env::set_var(
+            "VESTING_CONTRACT_ADDRESS",
+            "n1unyuj8qnmygvzuex3dwmg9yzt9alhvyeat0uu0jedg2wj33efl5qackslz",
+        );
+        std::env::set_var(
+            "COCONUT_BANDWIDTH_CONTRACT_ADDRESS",
+            "n13902g92xfefeyzuyed49snlm5fxv5ms6mdq5kvrut27hasdw5a9q9vyw6c",
+        );
+        std::env::set_var(
+            "GROUP_CONTRACT_ADDRESS",
+            "n18nczmqw6adwxg2wnlef3hf0etf8anccafp2pjpul5rrtmv96umyq5mv7t5",
+        );
+        std::env::set_var(
+            "MULTISIG_CONTRACT_ADDRESS",
+            "n1q3zzxl78rlmxv3vn0uf4vkyz285lk8q2xzne299yt9x6mpfgk90qukuzmv",
+        );
+        std::env::set_var(
+            "COCONUT_DKG_CONTRACT_ADDRESS",
+            "n1jsz20ggp5a6v76j060erkzvxmeus8htlpl77yxp878f0gf95cyaq6p2pee",
+        );
+        std::env::set_var(
+            "NAME_SERVICE_CONTRACT_ADDRESS",
+            "n12ne7qtmdwd0j03t9t5es8md66wq4e5xg9neladrsag8fx3y89rcs36asfp",
+        );
+        std::env::set_var(
+            "SERVICE_PROVIDER_DIRECTORY_CONTRACT_ADDRESS",
+            "n1ps5yutd7sufwg058qd7ac7ldnlazsvmhzqwucsfxmm445d70u8asqxpur4",
+        );
+        std::env::set_var(
+            "EPHEMERA_CONTRACT_ADDRESS",
+            "n19lc9u84cz0yz3fww5283nucc9yvr8gsjmgeul0",
+        );
         std::env::set_var("STATISTICS_SERVICE_DOMAIN_ADDRESS", "http://0.0.0.0");
         std::env::set_var("EXPLORER_API", "https://sandbox-explorer.nymtech.net/api");
         std::env::set_var("NYXD", "https://rpc.sandbox.nymtech.net");
