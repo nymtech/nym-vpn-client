83ae9d91331b22014941ea0f7048de12ce0321fb
Jon Häggblad <jon.haggblad@gmail.com>
2024-02-07 14:20:56 +0100

    Add script to bump nym repo dependency to latest

---
+++ scripts/update-nym-to-latest-rev.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# File to update
+cargo_file="nym-vpn-lib/Cargo.toml"
+
+# Check if the Cargo.toml file exists
+if [ ! -f "$cargo_file" ]; then
+    echo "File: "$cargo_file" does not exist!"
+    exit 1
+fi
+
+# GitHub API URL for the latest commit on the develop branch
+api_url="https://api.github.com/repos/nymtech/nym/commits/develop"
+
+# Fetch the latest commit SHA from the develop branch
+latest_commit=$(curl -s $api_url | jq -r '.sha')
+
+# Check if we got a valid commit SHA
+if [[ -z "$latest_commit" || "$latest_commit" == "null" ]]; then
+    echo "Failed to fetch the latest commit SHA. Exiting..."
+    exit 1
+fi
+
+echo "Latest commit SHA: $latest_commit"
+
+# Update the Cargo.toml file with the latest commit SHA
+sed -i.bak -E "s/(nym-.* = \{ git = \"https:\/\/github\.com\/nymtech\/nym\", rev = \")([a-f0-9]+)/\1$latest_commit/" "$cargo_file"
+
+if [ $? -eq 0 ]; then
+    echo "Updated Cargo.toml with the latest commit SHA: $latest_commit"
+else
+    echo "Failed to update Cargo.toml"
+fi
+
