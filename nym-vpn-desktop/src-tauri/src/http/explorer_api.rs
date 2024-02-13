7940eff3819fab5f7823ba99a9fa6bc250df80f8
pierre <dommerc.pierre@gmail.com>
2024-02-13 17:15:46 +0100

    use nym_explorer_api_requests crate

--- nym-vpn-desktop/src-tauri/src/http/explorer_api.rs
+++ nym-vpn-desktop/src-tauri/src/http/explorer_api.rs
@@ -1,24 +1,5 @@
-use serde::{Deserialize, Serialize};
-
 // TODO get the URL endpoint from config
 pub const EXPLORER_API_URL: &str = "https://sandbox-explorer.nymtech.net/api/v1";
 pub const GATEWAYS_ENDPOINT: &str = "/gateways/";
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct JsonLocation {
-    pub two_letter_iso_country_code: String,
-    pub three_letter_iso_country_code: String,
-    pub country_name: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct JsonGatewayInfo {
-    pub identity_key: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct JsonGateway {
-    pub owner: String,
-    pub location: Option<JsonLocation>,
-    pub gateway: JsonGatewayInfo,
-}
+pub type JsonGateway = nym_explorer_api_requests::PrettyDetailedGatewayBond;
