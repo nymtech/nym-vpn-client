011debc01061a6bbd71e1d39fcf617f8babde676
pierre <dommerc.pierre@gmail.com>
2024-02-21 16:39:10 +0100

    add nym logo svg

--- nym-vpn-desktop/src/assets/icons/index.ts
+++ nym-vpn-desktop/src/assets/icons/index.ts
@@ -4,3 +4,4 @@ export { default as GitHubIcon } from './github.svg?react';
 export { default as DiscordIcon } from './discord.svg?react';
 export { default as MixnetIcon } from './mixnet.svg?react';
 export { default as ElementIcon } from './element.svg?react';
+export { default as NymIcon } from './nym.svg?react';
