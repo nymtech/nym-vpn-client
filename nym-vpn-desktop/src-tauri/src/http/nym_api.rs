9919635b15516e975bb9dea35b6fb76c0c6ce7a5
pierre <dommerc.pierre@gmail.com>
2024-02-26 15:48:57 +0100

    add get_exit_countries command (rust)

---
+++ nym-vpn-desktop/src-tauri/src/http/nym_api.rs
@@ -0,0 +1,31 @@
+use std::env;
+use tracing::{debug, error, info};
+
+use crate::http::client::HTTP_CLIENT;
+
+use super::client::HttpError;
+
+pub const NYM_API_URL: &str = "https://sandbox-nym-api1.nymtech.net/api/v1";
+pub const GATEWAYS_ENDPOINT: &str = "/gateways/described";
+
+pub type NymApiJsonGateway = nym_api_requests::models::DescribedGateway;
+
+pub async fn get_gateways() -> Result<Vec<NymApiJsonGateway>, HttpError> {
+    let explorer_api_url = env::var("EXPLORER_API")
+        .map(|url| format!("{}/v1", url))
+        .unwrap_or_else(|_| NYM_API_URL.to_string());
+    let url = format!("{}{}", explorer_api_url, GATEWAYS_ENDPOINT);
+
+    info!("fetching countries from Nym API [{url}]");
+    let res = HTTP_CLIENT.get(url).send().await.map_err(|e| {
+        error!("HTTP request GET {GATEWAYS_ENDPOINT} failed: {e}");
+        HttpError::RequestError(e.status())
+    })?;
+
+    debug!("deserializing json response");
+    let json: Vec<NymApiJsonGateway> = res.json().await.map_err(|e| {
+        error!("HTTP request GET {GATEWAYS_ENDPOINT} failed to deserialize json response: {e}");
+        HttpError::ResponseError(e.to_string())
+    })?;
+    Ok(json)
+}
