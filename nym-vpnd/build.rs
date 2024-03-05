e06dab0368fadfec3ec971559e17d77c23b0e46c
Jon Häggblad <jon.haggblad@gmail.com>
2024-03-05 16:26:34 +0100

    Add flag to pass env file to nym-vpnd

---
+++ nym-vpnd/build.rs
@@ -0,0 +1,14 @@
+// Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: Apache-2.0
+
+use vergen::EmitBuilder;
+
+fn main() {
+    EmitBuilder::builder()
+        .all_build()
+        .all_git()
+        .all_rustc()
+        .all_cargo()
+        .emit()
+        .expect("failed to extract build metadata");
+}
