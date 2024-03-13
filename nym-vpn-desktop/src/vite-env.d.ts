6ca79c411cb69b672fef866d9417cb4f6ff64928
pierre <dommerc.pierre@gmail.com>
2024-03-13 17:05:17 +0100

    init sentry client

--- nym-vpn-desktop/src/vite-env.d.ts
+++ nym-vpn-desktop/src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly APP_NOSPLASH: string | undefined;
   readonly APP_CREDENTIAL: string | undefined;
+  readonly APP_SENTRY_DSN: string | undefined;
 }
 
 interface ImportMeta {
