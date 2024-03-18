3ed91bc1038058cb228b9104b4dc9275161a0e97
Jon Häggblad <jon.haggblad@gmail.com>
2024-03-19 00:12:18 +0100

    Further improve release scripts (#185)
    * Rework create-nym-vpn-desktop-release.sh
    
    * Rework create-nym-vpn-cli-release.sh
    
    * Fix error handling in bump script
    
    * Use common function
    
    * Add script for bumping desktop client to dev version
    
    * exec permission
    
    * Fix forgotten line

--- scripts/bump-nym-vpn-cli-to-next-dev-version.sh
+++ scripts/bump-nym-vpn-cli-to-next-dev-version.sh
@@ -5,34 +5,7 @@
 # set -x
 set -euo pipefail
 
-# Function to increment version and append -dev suffix
-increment_version() {
-    local version=$1
-    local IFS='.'  # Internal Field Separator for splitting version parts
-    read -r -a parts <<< "$version"  # Read version into an array
-
-    # Validate version format (basic check)
-    if [[ ! $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-        echo "Error: Version format must be X.Y.Z (e.g., 0.0.7)" >&2
-        exit 1
-    fi
-
-    # Increment the patch version
-    ((parts[2]++))
-
-    # Reassemble the version and append -dev suffix
-    local new_version="${parts[0]}.${parts[1]}.${parts[2]}-dev"
-
-    echo "$new_version"
-}
-
-check_unstaged_changes() {
-    # Confirm we don't have unstaged changes
-    if ! git diff --exit-code > /dev/null; then
-        echo "Error: There are unstaged changes. Please commit or stash them before running this script."
-        exit 1
-    fi
-}
+source "$(dirname "$0")/common.sh"
 
 get_current_version() {
     echo "$(cargo get package.version --entry="nym-vpn-cli")"
@@ -46,21 +19,20 @@ run_cargo_set_version() {
     echo "Running in dry-run mode: $command --dry-run"
     $command --dry-run
 
-    # Ask for user confirmation
-    read -p "Was this the intended change? (Y/N): " answer
-    if [[ $answer =~ ^[Yy]$ ]]; then
-        echo "Running command without dry-run: $command"
-        $command
-    else
-        echo "Exiting without making changes."
-        exit 1
-    fi
+    ask_for_confirmation "$command"
 }
 
 main() {
     check_unstaged_changes
+    confirm_root_directory
     local version=$(get_current_version)
     local next_version=$(increment_version "$version")
+
+    if [[ -z "$next_version" ]]; then
+        echo "Error: next_version is empty. Exiting."
+        exit 1
+    fi
+
     run_cargo_set_version "$next_version"
 }
 
