102e9914d43f0615761dae167acde3b25b65a47f
Jon Häggblad <jon.haggblad@gmail.com>
2024-03-20 10:52:46 +0100

    Send events

--- nym-vpn-lib/src/mixnet_processor.rs
+++ nym-vpn-lib/src/mixnet_processor.rs
@@ -191,11 +191,11 @@ impl ConnectionMonitor {
         }
     }
 
-    async fn run(mut self, mut shutdown: TaskClient) -> Result<()> {
+    async fn run(mut self, mut task_client: TaskClient) -> Result<()> {
         info!("Connection monitor is running");
         loop {
             tokio::select! {
-                _ = shutdown.recv_with_delay() => {
+                _ = task_client.recv_with_delay() => {
                     info!("ConnectionMonitor: Received shutdown");
                     break;
                 }
@@ -209,14 +209,22 @@ impl ConnectionMonitor {
                 }
                 // Every 5 seconds, check if we have received a self-ping in the last 5 seconds
                 _ = tokio::time::sleep(Duration::from_secs(5)) => {
+                    let msg = ConnectionMonitorStatus::Hello;
+                    task_client.send_status_msg(Box::new(msg));
                     if let Some(latest_self_ping) = self.stats.latest_self_ping {
                         if latest_self_ping.elapsed() > Duration::from_secs(5) {
                             error!("Haven't received a self-ping in the last 5 seconds");
                             error!("Connection to entry gateway seems down!");
+                            // WIP(JON): we need to make sure things are not put in a pile with no
+                            // receiver emptying it
+                            let msg = ConnectionMonitorStatus::EntryGatewayDown;
+                            task_client.send_status_msg(Box::new(msg));
                         }
                     } else {
                         error!("Haven't received a self-ping yet");
                         error!("Connection to entry gateway seems down!");
+                        let msg = ConnectionMonitorStatus::EntryGatewayDown;
+                        task_client.send_status_msg(Box::new(msg));
                     }
                 }
             }
@@ -224,6 +232,17 @@ impl ConnectionMonitor {
         info!("ConnectionMonitor: Exiting");
         Ok(())
     }
+}
+
+// Just like in nym_task::TaskManager and TaskStatus, strictly speaking this is not an error, but a
+// status message. We're just piggybacking on the error trait for now. In the future, we might want
+// to create a separate trait in nym_task::TaskManager
+#[derive(thiserror::Error, Debug)]
+pub enum ConnectionMonitorStatus {
+    #[error("connection to entry gateway seems down!")]
+    EntryGatewayDown,
+    #[error("hello")]
+    Hello,
 }
 
 pub fn start_connection_monitor(
