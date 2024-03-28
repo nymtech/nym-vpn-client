8ececa103e42620fb6a6210907cbb62a8b70857b
Pierre Dommerc <dommerc.pierre@gmail.com>
2024-03-28 10:54:13 +0100

    chore: enforce eol to lf (#232)

--- nym-vpn-desktop/prettier.config.js
+++ nym-vpn-desktop/prettier.config.js
@@ -4,6 +4,8 @@ const config = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
+  // this is the default be for the sake of clarity, be explicit
+  endOfLine: 'lf',
 };
 
 export default config;
