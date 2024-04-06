3745a4985ead740a8dede57dba89d96b9d3d92ab
Jon Häggblad <jon.haggblad@gmail.com>
2024-04-06 17:37:15 +0200

    Trim shared mixnet client

--- crates/nym-connection-monitor/src/mixnet_beacon.rs
+++ crates/nym-connection-monitor/src/mixnet_beacon.rs
@@ -15,10 +15,7 @@ use nym_sdk::{
 use tokio::task::JoinHandle;
 use tracing::{debug, error, trace};
 
-use crate::{
-    error::{Error, Result},
-    // mixnet_connect::SharedMixnetClient,
-};
+use crate::error::{Error, Result};
 
 const MIXNET_SELF_PING_INTERVAL: Duration = Duration::from_millis(1000);
 
@@ -26,22 +23,22 @@ const MIXNET_SELF_PING_INTERVAL: Duration = Duration::from_millis(1000);
 pub struct SharedMixnetClient(Arc<tokio::sync::Mutex<Option<MixnetClient>>>);
 
 impl SharedMixnetClient {
-    pub fn new(mixnet_client: MixnetClient) -> Self {
-        Self(Arc::new(tokio::sync::Mutex::new(Some(mixnet_client))))
-    }
-
+    // pub fn new(mixnet_client: MixnetClient) -> Self {
+    //     Self(Arc::new(tokio::sync::Mutex::new(Some(mixnet_client))))
+    // }
+    //
     pub async fn lock(&self) -> tokio::sync::MutexGuard<'_, Option<MixnetClient>> {
         self.0.lock().await
     }
 
-    pub async fn nym_address(&self) -> Recipient {
-        *self.lock().await.as_ref().unwrap().nym_address()
-    }
-
-    pub async fn split_sender(&self) -> MixnetClientSender {
-        self.lock().await.as_ref().unwrap().split_sender()
-    }
-
+    // pub async fn nym_address(&self) -> Recipient {
+    //     *self.lock().await.as_ref().unwrap().nym_address()
+    // }
+    //
+    // pub async fn split_sender(&self) -> MixnetClientSender {
+    //     self.lock().await.as_ref().unwrap().split_sender()
+    // }
+    //
     // pub async fn gateway_ws_fd(&self) -> Option<RawFd> {
     //     self.lock()
     //         .await
@@ -56,11 +53,11 @@ impl SharedMixnetClient {
         Ok(())
     }
 
-    pub async fn disconnect(self) -> Self {
-        let handle = self.lock().await.take().unwrap();
-        handle.disconnect().await;
-        self
-    }
+    // pub async fn disconnect(self) -> Self {
+    //     let handle = self.lock().await.take().unwrap();
+    //     handle.disconnect().await;
+    //     self
+    // }
 }
 
 struct MixnetConnectionBeacon {
