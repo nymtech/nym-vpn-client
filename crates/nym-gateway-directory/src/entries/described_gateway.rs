5d7e4b75de6e58c36aec8637440fb8074f1eba7b
Zane Schepke <zanecschepke@gmail.com>
2024-04-11 16:27:24 -0400

    filter exit gateways by build time and version

--- crates/nym-gateway-directory/src/entries/described_gateway.rs
+++ crates/nym-gateway-directory/src/entries/described_gateway.rs
@@ -2,7 +2,11 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use nym_explorer_client::Location;
+use nym_validator_client::nyxd::bip32::secp256k1::pkcs8::der::DateTime;
 use nym_validator_client::{client::IdentityKey, models::DescribedGateway};
+
+const BUILD_VERSION: &str = "1.1.34";
+const BUILD_TIME: &str = "2024-03-25T10:47:53.981548588Z";
 
 #[derive(Clone, Debug)]
 pub struct DescribedGatewayWithLocation {
@@ -20,6 +24,36 @@ impl DescribedGatewayWithLocation {
             .self_described
             .as_ref()
             .and_then(|d| d.ip_packet_router.as_ref())
+            .is_some()
+    }
+
+    pub fn has_current_api_version(&self) -> bool {
+        self.has_v6_exit_build_timestamp() && self.has_current_build_version()
+    }
+
+    fn has_v6_exit_build_timestamp(&self) -> bool {
+        self.gateway
+            .self_described
+            .as_ref()
+            .map(|d| {
+                d.build_information
+                    .build_timestamp
+                    .parse()
+                    .map_or(false, |build_time| {
+                        let expected_build_time: DateTime =
+                            BUILD_TIME.parse().expect("Invalid timestamp");
+                        build_time >= expected_build_time
+                    })
+            })
+            .is_some()
+    }
+
+    //can make this more flexible with backwards compatibility
+    fn has_current_build_version(&self) -> bool {
+        self.gateway
+            .self_described
+            .as_ref()
+            .map(|d| d.build_information.build_version == BUILD_VERSION)
             .is_some()
     }
 
