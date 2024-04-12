1c1cb039c6158d3d08a54c7c74526d2c3e76b260
Jon Häggblad <jon.haggblad@gmail.com>
2024-04-12 13:07:13 +0200

    tidy in vpnc

--- nym-vpnc/src/main.rs
+++ nym-vpnc/src/main.rs
@@ -1,14 +1,14 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: Apache-2.0
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use clap::{Parser, Subcommand};
 use nym_vpn_proto::{
     nym_vpnd_client::NymVpndClient, ConnectRequest, DisconnectRequest, StatusRequest,
 };
 use parity_tokio_ipc::Endpoint as IpcEndpoint;
-use tonic::transport::Endpoint;
+use tonic::transport::{Channel, Endpoint};
 
 #[derive(Parser)]
 #[clap(author = "Nymtech", version, about)]
@@ -35,47 +35,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn connect() -> Result<(), Box<dyn std::error::Error>> {
-    let socket_path = Path::new("/var/run/nym-vpn.socket");
-    let endpoint = Endpoint::from_static("http://[::1]:50051")
+fn get_socket_path() -> PathBuf {
+    Path::new("/var/run/nym-vpn.socket").to_path_buf()
+}
+
+async fn get_channel() -> Channel {
+    Endpoint::from_static("http://[::1]:50051")
         .connect_with_connector(tower::service_fn(move |_| {
-            IpcEndpoint::connect(socket_path)
+            IpcEndpoint::connect(get_socket_path())
         }))
         .await
-        .unwrap();
-    let mut client = NymVpndClient::new(endpoint);
+        .unwrap()
+}
+
+async fn connect() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = NymVpndClient::new(get_channel().await);
     let request = tonic::Request::new(ConnectRequest {});
     let response = client.vpn_connect(request).await?;
     println!("RESPONSE={:?}", response);
+    let inner = response.into_inner();
+    println!("{:?}", inner);
     Ok(())
 }
 
 async fn disconnect() -> Result<(), Box<dyn std::error::Error>> {
-    let socket_path = Path::new("/var/run/nym-vpn.socket");
-    let endpoint = Endpoint::from_static("http://[::1]:50051")
-        .connect_with_connector(tower::service_fn(move |_| {
-            IpcEndpoint::connect(socket_path)
-        }))
-        .await
-        .unwrap();
-    let mut client = NymVpndClient::new(endpoint);
+    let mut client = NymVpndClient::new(get_channel().await);
     let request = tonic::Request::new(DisconnectRequest {});
     let response = client.vpn_disconnect(request).await?;
     println!("RESPONSE={:?}", response);
+    let inner = response.into_inner();
+    println!("{:?}", inner);
     Ok(())
 }
 
 async fn status() -> Result<(), Box<dyn std::error::Error>> {
-    let socket_path = Path::new("/var/run/nym-vpn.socket");
-    let endpoint = Endpoint::from_static("http://[::1]:50051")
-        .connect_with_connector(tower::service_fn(move |_| {
-            IpcEndpoint::connect(socket_path)
-        }))
-        .await
-        .unwrap();
-    let mut client = NymVpndClient::new(endpoint);
+    let mut client = NymVpndClient::new(get_channel().await);
     let request = tonic::Request::new(StatusRequest {});
     let response = client.vpn_status(request).await?;
     println!("RESPONSE={:?}", response);
+    let inner = response.into_inner();
+    println!("{:?}", inner);
     Ok(())
 }
