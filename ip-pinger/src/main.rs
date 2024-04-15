f0ef732c2e414baf3e44a3e4b22143beca0c709b
Jon Häggblad <jon.haggblad@gmail.com>
2024-04-15 18:11:16 +0200

    Send icmp ping

--- ip-pinger/src/main.rs
+++ ip-pinger/src/main.rs
@@ -1,3 +1,4 @@
+use bytes::Bytes;
 use clap::{Args, Parser, Subcommand};
 use futures::channel::{mpsc, oneshot};
 use ipnetwork::{Ipv4Network, Ipv6Network};
@@ -5,9 +6,10 @@ use log::{debug, error, info};
 use nym_connection_monitor::ConnectionMonitorTask;
 use nym_gateway_directory::{DescribedGatewayWithLocation, GatewayClient, IpPacketRouterAddress};
 use nym_sdk::mixnet::{
-    MixnetClient, MixnetClientBuilder, MixnetClientSender, MixnetMessageSender, NodeIdentity,
-    Recipient, StoragePaths,
+    InputMessage, MixnetClient, MixnetClientBuilder, MixnetClientSender, MixnetMessageSender,
+    NodeIdentity, Recipient, StoragePaths,
 };
+use nym_task::connections::TransmissionLane;
 use nym_task::TaskManager;
 use nym_vpn_lib::gateway_directory::{Config as GatewayConfig, EntryPoint, ExitPoint};
 use nym_vpn_lib::nym_config::defaults::var_names::{EXPLORER_API, NYM_API};
@@ -15,6 +17,7 @@ use nym_vpn_lib::nym_config::OptionalSet;
 use nym_vpn_lib::wg_gateway_client::WgConfig as WgGatewayConfig;
 use nym_vpn_lib::{error::*, IpPair};
 use nym_vpn_lib::{nym_bin_common::bin_info_local_vergen, wg_gateway_client::WgConfig};
+use pnet_packet::Packet;
 use std::net::IpAddr;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
@@ -118,7 +121,7 @@ async fn run_vpn() -> Result<()> {
     info!("Connecting to exit gateway: {exit_gateway}");
     let enable_two_hop = false;
     let our_ips = nym_vpn_lib::mixnet_connect::connect_to_ip_packet_router(
-        shared_mixnet_client,
+        shared_mixnet_client.clone(),
         &exit_router_address,
         None,
         enable_two_hop,
@@ -129,7 +132,59 @@ async fn run_vpn() -> Result<()> {
 
     // Perform ICMP connectivity check for exit gateway
 
+    // Create ICMP/IPv4 echo request packet
+    let sequence_number = 1;
+    let identifier = icmp_identifier();
+    let icmp_echo_request = nym_connection_monitor::packet_helpers::create_icmpv4_echo_request(
+        sequence_number,
+        identifier,
+    )?;
+    let destination = "10.0.0.1".parse::<Ipv4Addr>()?;
+    let ipv4_packet = nym_connection_monitor::packet_helpers::wrap_icmp_in_ipv4(
+        icmp_echo_request,
+        our_ips.ipv4,
+        destination,
+    )?;
+
+    // Wrap the IPv4 packet in a MultiIpPacket
+    let bundled_packet = nym_ip_packet_requests::codec::MultiIpPacketCodec::bundle_one_packet(
+        ipv4_packet.packet().to_vec().into(),
+    );
+
+    // Wrap into a mixnet input message addressed to the IPR
+    let two_hop = true;
+    let mixnet_message = create_input_message(exit_router_address.0, bundled_packet, two_hop)?;
+
+    shared_mixnet_client.send(mixnet_message).await?;
+
+    // Listen for reply
+
     Ok(())
+}
+
+fn icmp_identifier() -> u16 {
+    8475
+}
+
+fn create_input_message(
+    recipient: Recipient,
+    bundled_packets: Bytes,
+    enable_two_hop: bool,
+) -> Result<InputMessage> {
+    let packet =
+        nym_ip_packet_requests::request::IpPacketRequest::new_data_request(bundled_packets)
+            .to_bytes()?;
+
+    let lane = TransmissionLane::General;
+    let packet_type = None;
+    let hops = enable_two_hop.then_some(0);
+    Ok(InputMessage::new_regular_with_custom_hops(
+        recipient,
+        packet,
+        lane,
+        packet_type,
+        hops,
+    ))
 }
 
 fn setup_logging() {
