71b0e3028e9bbecb54aad44856ca85523c2a0c6f
Jon Häggblad <jon.haggblad@gmail.com>
2024-04-15 08:40:39 +0200

    Switch to local socket for grpc, add status command (#288)
    * Initial work on switching to local socket
    
    * Tidy
    
    * Add status command
    
    * incoming stream
    
    * tidy in vpnc
    
    * Add optional http listener

--- nym-vpnd/src/command_interface/connection_handler.rs
+++ nym-vpnd/src/command_interface/connection_handler.rs
@@ -1,11 +1,8 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    sync::{mpsc::UnboundedSender, oneshot},
-};
-use tracing::{error, info, warn};
+use tokio::sync::{mpsc::UnboundedSender, oneshot};
+use tracing::{info, warn};
 
 use crate::service::{
     VpnServiceCommand, VpnServiceConnectResult, VpnServiceDisconnectResult, VpnServiceStatusResult,
@@ -58,7 +55,7 @@ impl CommandInterfaceConnectionHandler {
         };
     }
 
-    async fn handle_status(&self) -> VpnServiceStatusResult {
+    pub(crate) async fn handle_status(&self) -> VpnServiceStatusResult {
         let (tx, rx) = oneshot::channel();
         self.vpn_command_tx
             .send(VpnServiceCommand::Status(tx))
@@ -68,35 +65,5 @@ impl CommandInterfaceConnectionHandler {
         let status = rx.await.unwrap();
         info!("VPN status: {:?}", status);
         status
-    }
-
-    pub(super) fn handle(self, mut socket: tokio::net::UnixStream) -> tokio::task::JoinHandle<()> {
-        tokio::spawn(async move {
-            let mut buffer = [0; 1024];
-            match socket.read(&mut buffer).await {
-                Ok(0) => info!("Received 0 bytes"),
-                Ok(n) => {
-                    let command = std::str::from_utf8(&buffer[..n]).unwrap().trim();
-                    info!("Command: Received command: {:?}", command);
-                    match command {
-                        "connect" => {
-                            self.handle_connect().await;
-                        }
-                        "disconnect" => {
-                            self.handle_disconnect().await;
-                        }
-                        "status" => {
-                            let status = self.handle_status().await;
-                            socket
-                                .write_all(format!("{:?}", status).as_bytes())
-                                .await
-                                .unwrap();
-                        }
-                        command => info!("Unknown command: {}", command),
-                    }
-                }
-                Err(e) => error!("Failed to read from socket; err = {:?}", e),
-            }
-        })
     }
 }
--- nym-vpnd/src/command_interface/listener.rs
+++ nym-vpnd/src/command_interface/listener.rs
@@ -3,20 +3,75 @@
 
 use std::{
     fs,
+    net::SocketAddr,
     path::{Path, PathBuf},
 };
 
+use nym_vpn_proto::{
+    nym_vpnd_server::NymVpnd, ConnectRequest, ConnectResponse, ConnectionStatus, DisconnectRequest,
+    DisconnectResponse, StatusRequest, StatusResponse,
+};
 use tokio::sync::mpsc::UnboundedSender;
 use tracing::{error, info};
 
-use crate::service::VpnServiceCommand;
-
-use nym_vpn_proto::{
-    nym_vpnd_server::NymVpnd, ConnectRequest, ConnectResponse, DisconnectRequest,
-    DisconnectResponse,
-};
-
 use super::connection_handler::CommandInterfaceConnectionHandler;
+use crate::service::{VpnServiceCommand, VpnServiceStatusResult};
+
+enum ListenerType {
+    Path(PathBuf),
+    Uri(#[allow(unused)] SocketAddr),
+}
+
+pub(super) struct CommandInterface {
+    vpn_command_tx: UnboundedSender<VpnServiceCommand>,
+    listener: ListenerType,
+}
+
+impl CommandInterface {
+    pub(super) fn new_with_path(
+        vpn_command_tx: UnboundedSender<VpnServiceCommand>,
+        socket_path: &Path,
+    ) -> Self {
+        Self {
+            vpn_command_tx,
+            listener: ListenerType::Path(socket_path.to_path_buf()),
+        }
+    }
+
+    pub(super) fn new_with_uri(
+        vpn_command_tx: UnboundedSender<VpnServiceCommand>,
+        uri: SocketAddr,
+    ) -> Self {
+        Self {
+            vpn_command_tx,
+            listener: ListenerType::Uri(uri),
+        }
+    }
+
+    fn remove_previous_socket_file(&self) {
+        if let ListenerType::Path(ref socket_path) = self.listener {
+            match fs::remove_file(socket_path) {
+                Ok(_) => info!(
+                    "Removed previous command interface socket: {:?}",
+                    socket_path
+                ),
+                Err(err) if err.kind() == std::io::ErrorKind::NotFound => {}
+                Err(err) => {
+                    error!(
+                        "Failed to remove previous command interface socket: {:?}",
+                        err
+                    );
+                }
+            }
+        }
+    }
+}
+
+impl Drop for CommandInterface {
+    fn drop(&mut self) {
+        self.remove_previous_socket_file();
+    }
+}
 
 #[tonic::async_trait]
 impl NymVpnd for CommandInterface {
@@ -47,55 +102,31 @@ impl NymVpnd for CommandInterface {
         info!("Returning disconnect response");
         Ok(tonic::Response::new(DisconnectResponse { success: true }))
     }
-}
 
-pub(super) struct CommandInterface {
-    vpn_command_tx: UnboundedSender<VpnServiceCommand>,
-    socket_path: PathBuf,
-}
+    async fn vpn_status(
+        &self,
+        request: tonic::Request<StatusRequest>,
+    ) -> Result<tonic::Response<StatusResponse>, tonic::Status> {
+        info!("Got status request: {:?}", request);
 
-impl CommandInterface {
-    pub(super) fn new(
-        vpn_command_tx: UnboundedSender<VpnServiceCommand>,
-        socket_path: &Path,
-    ) -> Self {
-        Self {
-            vpn_command_tx,
-            socket_path: socket_path.to_path_buf(),
-        }
-    }
+        let status = CommandInterfaceConnectionHandler::new(self.vpn_command_tx.clone())
+            .handle_status()
+            .await;
 
-    fn remove_previous_socket_file(&self) {
-        match fs::remove_file(&self.socket_path) {
-            Ok(_) => info!(
-                "Removed previous command interface socket: {:?}",
-                self.socket_path
-            ),
-            Err(err) if err.kind() == std::io::ErrorKind::NotFound => {}
-            Err(err) => {
-                error!(
-                    "Failed to remove previous command interface socket: {:?}",
-                    err
-                );
-            }
-        }
-    }
-
-    #[allow(unused)]
-    pub(super) async fn listen(self) {
-        self.remove_previous_socket_file();
-        let listener = tokio::net::UnixListener::bind(&self.socket_path).unwrap();
-        info!("Command interface listening on {:?}", self.socket_path);
-
-        loop {
-            let (socket, _) = listener.accept().await.unwrap();
-            CommandInterfaceConnectionHandler::new(self.vpn_command_tx.clone()).handle(socket);
-        }
+        info!("Returning status response");
+        Ok(tonic::Response::new(StatusResponse {
+            status: ConnectionStatus::from(status) as i32,
+        }))
     }
 }
 
-impl Drop for CommandInterface {
-    fn drop(&mut self) {
-        self.remove_previous_socket_file();
+impl From<VpnServiceStatusResult> for ConnectionStatus {
+    fn from(status: VpnServiceStatusResult) -> Self {
+        match status {
+            VpnServiceStatusResult::NotConnected => ConnectionStatus::NotConnected,
+            VpnServiceStatusResult::Connecting => ConnectionStatus::Connecting,
+            VpnServiceStatusResult::Connected => ConnectionStatus::Connected,
+            VpnServiceStatusResult::Disconnecting => ConnectionStatus::Disconnecting,
+        }
     }
 }
