f546e71d79293b57d6173c91b9625bff3bf851c9
Jon Häggblad <jon.haggblad@gmail.com>
2024-04-19 11:17:04 +0200

    Remove testing code

--- crates/nym-harbour-master-client/src/main.rs
+++
@@ -1,8 +0,0 @@
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let gateways = nym_harbour_master_client::get_gateways().await?;
-    for gateway in gateways.items {
-        println!("{:?}", gateway);
-    }
-    Ok(())
-}
