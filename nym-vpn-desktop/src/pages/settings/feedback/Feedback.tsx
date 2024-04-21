1d3af905f2b280b11b741354e91f31dead3d94e4
pierre <dommerc.pierre@gmail.com>
2024-04-22 01:03:59 +0200

    move monitoring option in higher menu

--- nym-vpn-desktop/src/pages/settings/feedback/Feedback.tsx
+++ nym-vpn-desktop/src/pages/settings/feedback/Feedback.tsx
@@ -1,6 +1,4 @@
 import { open } from '@tauri-apps/api/shell';
-import * as _ from 'lodash-es';
-import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
   DiscordInviteUrl,
@@ -8,56 +6,11 @@ import {
   GitHubIssuesUrl,
   MatrixRoomUrl,
 } from '../../../constants';
-import {
-  useMainDispatch,
-  useMainState,
-  useNotifications,
-} from '../../../contexts';
-import { kvSet } from '../../../kvStore';
-import { StateDispatch } from '../../../types';
-import { PageAnim, SettingsMenuCard, Switch } from '../../../ui';
+import { PageAnim, SettingsMenuCard } from '../../../ui';
 import { DiscordIcon, ElementIcon, GitHubIcon } from '../../../assets';
 
-const ThrottleDelay = 10000; // ms
-
 function Feedback() {
-  const state = useMainState();
-  const [monitoring, setMonitoring] = useState(state.monitoring);
-  const dispatch = useMainDispatch() as StateDispatch;
-  const { push } = useNotifications();
-
   const { t } = useTranslation('settings');
-
-  useEffect(() => {
-    setMonitoring(state.monitoring);
-  }, [state]);
-
-  // notify the user at most once per every 10s when he toggles monitoring
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const showSnackbar = useCallback(
-    _.throttle(
-      () => {
-        push({
-          text: t('feedback.monitoring-alert'),
-          position: 'bottom',
-          closeIcon: true,
-        });
-      },
-      ThrottleDelay,
-      {
-        leading: true,
-        trailing: false,
-      },
-    ),
-    [push],
-  );
-
-  const handleMonitoringChanged = async () => {
-    showSnackbar();
-    const isSelected = !state.monitoring;
-    dispatch({ type: 'set-monitoring', monitoring: isSelected });
-    kvSet('Monitoring', isSelected);
-  };
 
   return (
     <PageAnim className="h-full flex flex-col mt-2 gap-6">
@@ -90,15 +43,6 @@ function Feedback() {
           <DiscordIcon className="w-6 h-6 fill-baltic-sea dark:fill-mercury-pinkish" />
         }
         trailingIcon="arrow_right"
-      />
-      <SettingsMenuCard
-        title={t('feedback.error-monitoring.title')}
-        desc={t('feedback.error-monitoring.desc')}
-        leadingIcon="bug_report"
-        onClick={handleMonitoringChanged}
-        trailingComponent={
-          <Switch checked={monitoring} onChange={handleMonitoringChanged} />
-        }
       />
     </PageAnim>
   );
