eaf19d3530429c33bf8d2a574d3aa5b57c6a5ee0
pierre <dommerc.pierre@gmail.com>
2024-04-25 10:54:46 +0200

    add grpc request tracing

--- crates/nym-vpn-proto/build.rs
+++ crates/nym-vpn-proto/build.rs
@@ -5,10 +5,14 @@ use std::{env, path::PathBuf};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // needed for reflection
-    let descriptor_path = PathBuf::from(env::var("OUT_DIR").unwrap()).join("vpn_descriptor.bin");
+    let vpn_fd = PathBuf::from(env::var("OUT_DIR").unwrap()).join("vpn_descriptor.bin");
+    tonic_build::configure()
+        .file_descriptor_set_path(vpn_fd)
+        .compile(&["../../proto/nym/vpn.proto"], &["../../proto/nym/"])?;
 
     tonic_build::configure()
-        .file_descriptor_set_path(descriptor_path)
-        .compile(&["../../proto/nym/vpn.proto"], &["../../proto/nym/"])?;
+        // server implementation is handled by tonic-health crate
+        .build_server(false)
+        .compile(&["../../proto/grpc/health.proto"], &["../../proto/grpc/"])?;
     Ok(())
 }
