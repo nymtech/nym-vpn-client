b6c26a3d06a56fc493cec6023731a84637397546
pierre <dommerc.pierre@gmail.com>
2024-04-29 21:30:09 +0200

    refactor default selected countries

--- nym-vpn-desktop/src-tauri/src/fs/config.rs
+++ nym-vpn-desktop/src-tauri/src/fs/config.rs
@@ -6,8 +6,4 @@ use serde::{Deserialize, Serialize};
 pub struct AppConfig {
     /// Path pointing to an env configuration file describing the network
     pub env_config_file: Option<PathBuf>,
-    /// 2-letter country code for the default entry node location
-    pub default_entry_node_location_code: Option<String>,
-    /// 2-letter country code for the default exit node location
-    pub default_exit_node_location_code: Option<String>,
 }
--- nym-vpn-desktop/src-tauri/src/states/app.rs
+++ nym-vpn-desktop/src-tauri/src/states/app.rs
@@ -1,6 +1,5 @@
 use std::fmt;
 
-use anyhow::anyhow;
 use futures::channel::mpsc::UnboundedSender;
 use nym_vpn_lib::NymVpnCtrlMessage;
 use serde::{Deserialize, Serialize};
@@ -9,7 +8,7 @@ use tracing::trace;
 use ts_rs::TS;
 
 use crate::{
-    country::{Country, DEFAULT_COUNTRY_CODE},
+    country::{Country, DEFAULT_ENTRY_COUNTRY, DEFAULT_EXIT_COUNTRY},
     db::{Db, Key},
     fs::config::AppConfig,
 };
@@ -76,26 +75,6 @@ impl TryFrom<(&Db, &AppConfig)> for AppState {
     type Error = anyhow::Error;
 
     fn try_from(store: (&Db, &AppConfig)) -> Result<Self, Self::Error> {
-        // retrieve default entry and exit node locations set from
-        // the config file
-        let default_entry_node_location = Country::try_from(
-            store
-                .1
-                .default_entry_node_location_code
-                .as_deref()
-                .unwrap_or(DEFAULT_COUNTRY_CODE),
-        )
-        .map_err(|e| anyhow!("failed to retrieve default entry node location: {e}"))?;
-
-        let default_exit_node_location = Country::try_from(
-            store
-                .1
-                .default_exit_node_location_code
-                .as_deref()
-                .unwrap_or(DEFAULT_COUNTRY_CODE),
-        )
-        .map_err(|e| anyhow!("failed to retrieve default exit node location: {e}"))?;
-
         // retrieve the saved app data from the embedded db
         let entry_node_location = store.0.get_typed::<NodeLocation>(Key::EntryNodeLocation)?;
         let exit_node_location = store.0.get_typed::<NodeLocation>(Key::ExitNodeLocation)?;
@@ -105,9 +84,9 @@ impl TryFrom<(&Db, &AppConfig)> for AppState {
         // fallback to config file for locations if not present
         Ok(AppState {
             entry_node_location: entry_node_location
-                .unwrap_or(NodeLocation::Country(default_entry_node_location)),
+                .unwrap_or(NodeLocation::Country(DEFAULT_ENTRY_COUNTRY.clone())),
             exit_node_location: exit_node_location
-                .unwrap_or(NodeLocation::Country(default_exit_node_location)),
+                .unwrap_or(NodeLocation::Country(DEFAULT_EXIT_COUNTRY.clone())),
             vpn_mode: vpn_mode.unwrap_or_default(),
             ..Default::default()
         })
