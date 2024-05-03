8184777c79ba80e43c02079a90fe22d7d9aff639
pierre <dommerc.pierre@gmail.com>
2024-05-03 14:40:13 +0200

    fix connect request
    fix disconnect states

--- nym-vpn-x/src-tauri/src/commands/connection.rs
+++ nym-vpn-x/src-tauri/src/commands/connection.rs
@@ -198,6 +198,7 @@ pub async fn disconnect(
     app_state.state = ConnectionState::Disconnecting;
     drop(app_state);
     app.emit_disconnecting();
+    drop(app_state);
 
     let request = Request::new(DisconnectRequest {});
     let response = vpnd.vpn_disconnect(request).await.map_err(|e| {
