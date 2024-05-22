48205c9407d26c9cabcbcac179ef26c8b3cb132e
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2024-05-22 11:32:25 +0300

    Update lib version

--- nym-vpn-apple/MixnetLibrary/Package.swift
+++ nym-vpn-apple/MixnetLibrary/Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "NymVpnLib",
-            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nightly/nym-vpn-lib_0.1.4-dev_ios_universal.zip",
-            checksum: "e74e68ee31584cb0cd8dbd024a529a93125af3ba1cdb80d08e945e2233d2e11e"
+            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nym-vpn-cli-v0.1.3/nym-vpn-lib_0.1.3_ios_universal.zip",
+            checksum: "e0ee7b50e4c5a83d41630236a31d1755bca3bdf163fc4eb07bbf1b8f02ffbd3c"
         ),
         .testTarget(
             name: "MixnetLibraryTests",
