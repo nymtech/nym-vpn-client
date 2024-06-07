93820460a65da57a634c17c68610eb589428e148
Jon Häggblad <jon.haggblad@gmail.com>
2024-06-07 09:59:22 +0200

    Tweak naming

--- nym-vpn-core/nym-vpnd/src/command_interface/proto_status_update.rs
+++ nym-vpn-core/nym-vpnd/src/command_interface/proto_status_update.rs
@@ -9,12 +9,12 @@ pub(super) fn status_update_from_task_status(
 ) -> ConnectionStatusUpdate {
     match status {
         nym_vpn_lib::TaskStatus::Ready => ConnectionStatusUpdate {
-            kind: StatusType::TunnelConnectionEstablished as i32,
+            kind: StatusType::TunnelEndToEndConnectionEstablished as i32,
             message: status.to_string(),
             details: Default::default(),
         },
         nym_vpn_lib::TaskStatus::ReadyWithGateway(ref gateway) => ConnectionStatusUpdate {
-            kind: StatusType::TunnelConnectionEstablished as i32,
+            kind: StatusType::TunnelEndToEndConnectionEstablished as i32,
             message: status.to_string(),
             details: maplit::hashmap! {
                 "entry_gateway".to_string() => gateway.to_string(),
@@ -28,37 +28,37 @@ pub(super) fn status_update_from_monitor_status(
 ) -> ConnectionStatusUpdate {
     match status {
         ConnectionMonitorStatus::EntryGatewayDown => ConnectionStatusUpdate {
-            kind: StatusType::EntryGatewayDown as i32,
+            kind: StatusType::EntryGatewayNotRoutingMixnetMessages as i32,
             message: status.to_string(),
             details: Default::default(),
         },
         ConnectionMonitorStatus::ExitGatewayDownIpv4 => ConnectionStatusUpdate {
-            kind: StatusType::ExitGatewayDownIpv4 as i32,
+            kind: StatusType::ExitRouterNotRespondingToIpv4Ping as i32,
             message: status.to_string(),
             details: Default::default(),
         },
         ConnectionMonitorStatus::ExitGatewayDownIpv6 => ConnectionStatusUpdate {
-            kind: StatusType::ExitGatewayDownIpv6 as i32,
+            kind: StatusType::ExitRouterNotRespondingToIpv6Ping as i32,
             message: status.to_string(),
             details: Default::default(),
         },
         ConnectionMonitorStatus::ExitGatewayRoutingErrorIpv4 => ConnectionStatusUpdate {
-            kind: StatusType::ExitGatewayRoutingErrorIpv4 as i32,
+            kind: StatusType::ExitRouterNotRoutingIpv4Traffic as i32,
             message: status.to_string(),
             details: Default::default(),
         },
         ConnectionMonitorStatus::ExitGatewayRoutingErrorIpv6 => ConnectionStatusUpdate {
-            kind: StatusType::ExitGatewayRoutingErrorIpv6 as i32,
+            kind: StatusType::ExitRouterNotRoutingIpv6Traffic as i32,
             message: status.to_string(),
             details: Default::default(),
         },
         ConnectionMonitorStatus::ConnectedIpv4 => ConnectionStatusUpdate {
-            kind: StatusType::ConnectedIpv4 as i32,
+            kind: StatusType::ConnectionOkIpv4 as i32,
             message: status.to_string(),
             details: Default::default(),
         },
         ConnectionMonitorStatus::ConnectedIpv6 => ConnectionStatusUpdate {
-            kind: StatusType::ConnectedIpv6 as i32,
+            kind: StatusType::ConnectionOkIpv6 as i32,
             message: status.to_string(),
             details: Default::default(),
         },
