ab0b74f14db472e39df3874aff55eb415c44ca6b
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2024-06-13 18:03:09 +0300

    Enable ipv6 routing for wg tunnel

--- nym-vpn-core/nym-vpn-lib/src/config.rs
+++ nym-vpn-core/nym-vpn-lib/src/config.rs
@@ -55,7 +55,7 @@ impl WireguardConfig {
             #[cfg(target_os = "linux")]
             fwmark: Some(TUNNEL_FWMARK),
         };
-        let generic_options = GenericTunnelOptions { enable_ipv6: false };
+        let generic_options = GenericTunnelOptions { enable_ipv6: true };
         let wg_options = TunnelOptions {
             mtu: Some(mtu),
             ..Default::default()
