60674163662a6e3c81ec058bb1c9eeb785c06953
pierre <dommerc.pierre@gmail.com>
2024-06-18 12:49:20 +0200

    wip

--- .pkg/aur_update.sh
+++ .pkg/aur_update.sh
@@ -42,12 +42,20 @@ if [ -z "$SOURCES" ]; then
   exit 1
 fi
 
-# ⚠ pkgver does not allow dashes, replace any - by _
+# bump _pkgver
+sed -i "s/_pkgver=.*/_pkgver=$PKGVER/" "$PKGBUILD"
+echo " ✓ bump _pkgver to $PKGVER"
+
+# ⚠ Dashes are not allowed in package version, replace any - by _
 PKGVER=${PKGVER//-/_}
 
 # bump package version
 sed -i "s/pkgver=.*/pkgver=$PKGVER/" "$PKGBUILD"
 echo " ✓ bump package version to $PKGVER"
+
+# bump _release_tag
+sed -i "s/_release_tag=.*/_release_tag=$RELEASE_TAG/" "$PKGBUILD"
+echo " ✓ bump _release_tag to $RELEASE_TAG"
 
 if [ -n "$PKGREL" ]; then
   # ⚠ on new package version, pkgrel must be reset to 1
@@ -61,12 +69,11 @@ echo "sources: ${sources[*]}"
 sums=()
 
 for file in "${sources[@]}"; do
-  if [ -z "$file" ]; then
-    continue
+  if [ -n "$file" ]; then
+    sum=$(sha256sum "$file" | awk '{print $1}')
+    sums+=("\n    '$sum'")
+    echo "sha256sum for $file: $sum"
   fi
-  sum=$(sha256sum "$file" | awk '{print $1}')
-  sums+=("\n    '$sum'")
-  echo "sha256sum for $file: $sum"
 done
 
 sed -i "s/sha256sums=.*/sha256sums=(${sums[*]})/" "$PKGBUILD"
