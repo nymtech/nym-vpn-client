e80d1c081bc7bd9a739b4f9185d66220fdb96201
pierre <dommerc.pierre@gmail.com>
2024-07-02 17:11:26 +0200

    restore build info

--- nym-vpn-x/src-tauri/src/cli.rs
+++ nym-vpn-x/src-tauri/src/cli.rs
@@ -9,7 +9,7 @@ pub type ManagedCli = Arc<Cli>;
 // generate `crate::build_info` function that returns the data
 // collected during build time
 // see https://github.com/danielschemmel/build-info
-// build_info::build_info!(fn build_info);
+build_info::build_info!(fn build_info);
 
 #[derive(Parser, Serialize, Deserialize, Debug, Clone)]
 #[command(author, version, about, long_about = None)]
@@ -43,42 +43,41 @@ pub struct Cli {
     pub console: bool,
 }
 
-// TODO restore this
-pub fn print_build_info(_package_info: &PackageInfo) {
-    // let info = build_info();
+pub fn print_build_info(package_info: &PackageInfo) {
+    let info = build_info();
 
-    //     print!(
-    //         r"crate name:      {}
-    // version:         {}
-    // tauri version:   {}
-    // package name:    {}
-    // package version: {}
-    // target:          {}
-    // profile:         {}
-    // build date:      {}
-    // rustc version:   {}
-    // rustc channel:   {}
-    // ",
-    //         info.crate_info.name,
-    //         info.crate_info.version,
-    //         tauri::VERSION,
-    //         package_info.name,
-    //         package_info.version,
-    //         info.target.triple,
-    //         info.profile,
-    //         info.timestamp,
-    //         info.compiler.version,
-    //         info.compiler.channel,
-    //     );
-    //     if let Some(git) = info.version_control.as_ref().and_then(|vc| vc.git()) {
-    //         println!(
-    //             r"commit sha:      {}
-    // commit date:     {}
-    // git branch:      {}
-    // ",
-    //             git.commit_id,
-    //             git.commit_timestamp,
-    //             git.branch.as_ref().unwrap_or(&"".to_string())
-    //         );
-    //     }
+    print!(
+        r"crate name:      {}
+version:         {}
+tauri version:   {}
+package name:    {}
+package version: {}
+target:          {}
+profile:         {}
+build date:      {}
+rustc version:   {}
+rustc channel:   {}
+",
+        info.crate_info.name,
+        info.crate_info.version,
+        tauri::VERSION,
+        package_info.name,
+        package_info.version,
+        info.target.triple,
+        info.profile,
+        info.timestamp,
+        info.compiler.version,
+        info.compiler.channel,
+    );
+    if let Some(git) = info.version_control.as_ref().and_then(|vc| vc.git()) {
+        println!(
+            r"commit sha:      {}
+commit date:     {}
+git branch:      {}
+",
+            git.commit_id,
+            git.commit_timestamp,
+            git.branch.as_ref().unwrap_or(&"".to_string())
+        );
+    }
 }
