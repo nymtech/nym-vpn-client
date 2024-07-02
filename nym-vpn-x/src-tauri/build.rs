07fc0ddfbd145d011ddd6ae4e0ef43eb9fd85350
Pierre <dommerc.pierre@gmail.com>
2024-07-02 18:50:21 +0200

    chore(vpnx): restore build info (#598)

--- nym-vpn-x/src-tauri/build.rs
+++ nym-vpn-x/src-tauri/build.rs
@@ -1,6 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // TODO restore this
-    // build_info_build::build_script();
+    build_info_build::build_script();
 
     tauri_build::build();
     Ok(())
