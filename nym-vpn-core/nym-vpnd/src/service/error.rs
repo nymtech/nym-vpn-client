f58a2d8040b2ea35d2c270f93abd06b96b5037bf
Pierre <dommerc.pierre@gmail.com>
2024-07-03 18:17:54 +0200

    fix(vpnx): credential error key (#618)
    * fix credential error key
    
    * cleanup

--- nym-vpn-core/nym-vpnd/src/service/error.rs
+++ nym-vpn-core/nym-vpnd/src/service/error.rs
@@ -52,16 +52,10 @@ impl From<VpnLibImportCredentialError> for ImportCredentialError {
                         }
                     }
                     NymIdError::StorageError { source } => {
-                        // There was a recent change for the upstream crate that adds a new variant
-                        // to StorageError to capture duplicate entries. Until that change makes
-                        // its way to the vpn-lib, we just match on the string as a temporary
-                        // solution.
-                        if let Some(StorageError::InternalDatabaseError(db_error)) =
+                        if let Some(StorageError::ConstraintUnique) =
                             source.downcast_ref::<StorageError>()
                         {
-                            if db_error.to_string().contains("code: 2067") {
-                                return ImportCredentialError::CredentialAlreadyImported;
-                            }
+                            return ImportCredentialError::CredentialAlreadyImported;
                         }
                         ImportCredentialError::StorageError {
                             path: location,
