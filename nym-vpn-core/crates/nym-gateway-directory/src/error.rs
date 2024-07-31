df64f786530aeb77a47f9a6a358eff629055cacd
Jon Häggblad <jon.haggblad@gmail.com>
2024-07-31 17:22:04 +0200

    rustfmt

--- nym-vpn-core/crates/nym-gateway-directory/src/error.rs
+++ nym-vpn-core/crates/nym-gateway-directory/src/error.rs
@@ -63,7 +63,6 @@ pub enum Error {
     //     requested_location: String,
     //     available_countries: Vec<String>,
     // },
-
     #[error("no entry gateway available for location {requested_location}, available countries: {available_countries:?}")]
     NoMatchingEntryGatewayForLocation {
         requested_location: String,
