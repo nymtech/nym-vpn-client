74f3c08df6cf871691e401a23c47b658afc00fe7
pierre <dommerc.pierre@gmail.com>
2024-08-01 15:27:23 +0200

    wip

--- nym-vpn-x/src/i18n/config.ts
+++ nym-vpn-x/src/i18n/config.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 import { en, es, fr, it, ptBR, ru, tr, uk, zhHans } from './resources';
@@ -16,30 +17,33 @@ export const resources = {
   ['zh-Hans']: zhHans,
 } as const;
 
-i18n.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: ['en'],
-  debug: import.meta.env.DEV,
-  defaultNS,
-  resources,
-  ns: [
-    'addCredential',
-    'common',
-    'home',
-    'settings',
-    'nodeLocation',
-    'backendMessages',
-    'display',
-    'licenses',
-    'errors',
-    'welcome',
-    'glossary',
-    'notifications',
-  ],
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    supportedLngs: Object.keys(resources),
+    fallbackLng: ['en'],
+    debug: import.meta.env.DEV,
+    defaultNS,
+    resources,
+    ns: [
+      'addCredential',
+      'common',
+      'home',
+      'settings',
+      'nodeLocation',
+      'backendMessages',
+      'display',
+      'licenses',
+      'errors',
+      'welcome',
+      'glossary',
+      'notifications',
+    ],
 
-  interpolation: {
-    escapeValue: false, // not needed for react as it escapes by default
-  },
-});
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  });
 
 export default i18n;
