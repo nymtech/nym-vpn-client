5e13e962db9a0622b668dc53b16e64695fb66a03
Jon Häggblad <jon.haggblad@gmail.com>
2024-08-05 15:48:45 +0200

    wip

--- nym-vpn-core/crates/nym-vpn-store/src/mnemonic.rs
+++ nym-vpn-core/crates/nym-vpn-store/src/mnemonic.rs
@@ -1,15 +1,37 @@
-use std::path::Path;
+use std::{fs::File, path::Path};
 
-use bip39::Mnemonic;
-// use cosmrs::crypto::secp256k1::SigningKey;
+pub const COSMOS_DERIVATION_PATH: &str = "m/44'/118'/0'/0/0";
 
-pub async fn store_mnemonic<P: AsRef<Path> + Clone>(
-    path: P,
-    mnemonic_phrase: &str,
-) -> Result<(), String> {
-    let mnemonic = Mnemonic::from_str(mnemonic_phrase).map_err(|e| e.to_string())?;
-    let seed = bip39::Seed::new(&mnemonic, "");
-    let signing_key = SigningKey::new_from_seed(seed.as_bytes());
+pub fn store_mnemonic<P: AsRef<Path> + Clone>(storage_path: P, mnemonic_phrase: &str) {
+    let mnemonic = bip39::Mnemonic::parse(mnemonic_phrase).unwrap();
+    let seed = mnemonic.to_seed("");
+    // let prefix = "".to_string();
+    let hd_path: cosmrs::bip32::DerivationPath = COSMOS_DERIVATION_PATH.parse().unwrap();
+    let signing_key =
+        cosmrs::crypto::secp256k1::SigningKey::derive_from_path(seed, &hd_path).unwrap();
 
-    todo!();
+    use std::io::Write;
+    // let signing_key_hex = hex::encode(signing_key.to_bytes());
+
+    let mut file = File::create(storage_path).unwrap();
+
+    writeln!(file, "Mnemonic: {}", mnemonic_phrase).unwrap();
+    writeln!(file, "Derivation Path: {}", COSMOS_DERIVATION_PATH).unwrap();
+    // writeln!(file, "Signing Key (Hex): {}", signing_key_hex).unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::{fs, path::PathBuf};
+
+    #[test]
+    fn test_store_mnemonic() {
+        // dummy mnemonic
+        let mnemonic = bip39::Mnemonic::generate_in(bip39::Language::English, 12).unwrap();
+        println!("Mnemonic: {}", mnemonic);
+
+        let path: PathBuf = "/tmp/test.txt".parse().unwrap();
+        store_mnemonic(path, &mnemonic.to_string());
+    }
 }
