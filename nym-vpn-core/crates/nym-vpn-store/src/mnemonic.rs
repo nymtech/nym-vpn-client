521065edaafa149d89648d0629521988aa356dfd
Jon Häggblad <jon.haggblad@gmail.com>
2024-08-08 16:08:25 +0200

    wip

--- nym-vpn-core/crates/nym-vpn-store/src/mnemonic.rs
+++ nym-vpn-core/crates/nym-vpn-store/src/mnemonic.rs
@@ -1,26 +1,79 @@
-use std::{fs::File, path::Path};
+use serde::{Deserialize, Serialize};
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+};
+use tokio::sync::Mutex;
+use zeroize::{Zeroize, ZeroizeOnDrop};
 
-pub const COSMOS_DERIVATION_PATH: &str = "m/44'/118'/0'/0/0";
+#[derive(Serialize, Deserialize, Zeroize, ZeroizeOnDrop)]
+struct StoredMnemonic {
+    mnemonic: bip39::Mnemonic,
+}
+
+trait MnemonicStorage {
+    async fn store_mnemonic(&self, mnemonic: bip39::Mnemonic);
+    async fn read_mnemonic(&self) -> bip39::Mnemonic;
+}
+
+struct InMemoryMnemonicStorage {
+    mnemonic: Mutex<Option<StoredMnemonic>>,
+}
+
+impl MnemonicStorage for InMemoryMnemonicStorage {
+    async fn store_mnemonic(&self, mnemonic: bip39::Mnemonic) {
+        let stored_mnemonic = StoredMnemonic { mnemonic };
+        self.mnemonic.lock().await.replace(stored_mnemonic);
+    }
+
+    async fn read_mnemonic(&self) -> bip39::Mnemonic {
+        self.mnemonic
+            .lock()
+            .await
+            .as_ref()
+            .unwrap()
+            .mnemonic
+            .clone()
+    }
+}
+
+struct OnDiskMnemonicStorage {
+    path: PathBuf,
+}
+
+impl OnDiskMnemonicStorage {
+    fn new<P: AsRef<Path>>(path: P) -> Self {
+        Self {
+            path: path.try_into().unwrap(),
+        }
+    }
+}
+
+impl MnemonicStorage for OnDiskMnemonicStorage {
+    async fn store_mnemonic(&self, mnemonic: bip39::Mnemonic) {
+        let stored_mnemonic = StoredMnemonic { mnemonic };
+
+        let file = File::create(&self.path).unwrap();
+        serde_json::to_writer(file, &stored_mnemonic).unwrap();
+    }
+
+    async fn read_mnemonic(&self) -> bip39::Mnemonic {
+        let file = File::open(&self.path).unwrap();
+        let stored_mnemonic: StoredMnemonic = serde_json::from_reader(file).unwrap();
+        stored_mnemonic.mnemonic
+    }
+}
 
 pub fn store_mnemonic<P: AsRef<Path> + Clone>(storage_path: P, mnemonic_phrase: &str) {
     let mnemonic = bip39::Mnemonic::parse(mnemonic_phrase).unwrap();
-    let seed = mnemonic.to_seed("");
-    // let prefix = "".to_string();
-    let hd_path: cosmrs::bip32::DerivationPath = COSMOS_DERIVATION_PATH.parse().unwrap();
-    let signing_key =
-        cosmrs::crypto::secp256k1::SigningKey::derive_from_path(seed, &hd_path).unwrap();
 
-    use std::io::Write;
-    // let signing_key_hex = hex::encode(signing_key.to_bytes());
-
-    let mut file = File::create(storage_path).unwrap();
-
-    writeln!(file, "Mnemonic: {}", mnemonic_phrase).unwrap();
-    writeln!(file, "Derivation Path: {}", COSMOS_DERIVATION_PATH).unwrap();
-    // writeln!(file, "Signing Key (Hex): {}", signing_key_hex).unwrap();
-
+    // Check that we can create a keypair from it
     let prefix = "n";
-    let wallet = nym_validator_client::DirectSecp256k1HdWallet::builder(prefix).build(mnemonic);
+    let _secp256k1_keypair =
+        nym_validator_client::DirectSecp256k1HdWallet::from_mnemonic(prefix, mnemonic.clone());
+
+    let stored_mnemonic = StoredMnemonic { mnemonic };
+    let storage = OnDiskMnemonicStorage::new(&storage_path);
 }
 
 #[cfg(test)]
