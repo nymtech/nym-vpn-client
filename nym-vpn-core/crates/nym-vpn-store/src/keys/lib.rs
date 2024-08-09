9c412f31d6e9f35528b887cf36ed4ed1c393c01b
Jon Häggblad <jon.haggblad@gmail.com>
2024-08-09 09:50:04 +0200

    Move keystore to subdir

---
+++ nym-vpn-core/crates/nym-vpn-store/src/keys/lib.rs
@@ -0,0 +1,4 @@
+pub mod device_keys;
+pub mod helpers;
+pub mod key_store;
+pub mod persistence;
--- nym-vpn-core/crates/nym-vpn-store/src/lib.rs
+++ nym-vpn-core/crates/nym-vpn-store/src/lib.rs
@@ -1,18 +1,7 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-pub use device_keys::DeviceKeys;
-pub use helpers::*;
-pub use key_store::KeyStore;
-pub use persistence::{
-    ephemeral::InMemEphemeralKeys,
-    on_disk::{DeviceKeysPaths, OnDiskKeys, OnDiskKeysError},
-};
+pub mod keys;
+pub mod mnemonic;
 
-mod device_keys;
-mod helpers;
-mod key_store;
-mod mnemonic;
-mod persistence;
-
-pub trait VpnStorage: mnemonic::MnemonicStorage + key_store::KeyStore {}
+pub trait VpnStorage: mnemonic::MnemonicStorage + keys::KeyStore {}
