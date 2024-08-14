6efe3f0d099b1007fdca9e3a0951e5e0f0ef4f9a
Zane Schepke <zanecschepke@gmail.com>
2024-08-14 12:20:47 -0400

    bump version

--- nym-vpn-android/app/build.gradle.kts
+++ nym-vpn-android/app/build.gradle.kts
@@ -78,7 +78,7 @@ android {
 				getDefaultProguardFile("proguard-android-optimize.txt"),
 				"proguard-rules.pro",
 			)
-			signingConfig = signingConfigs.getByName("debug")
+			signingConfig = signingConfigs.getByName(Constants.RELEASE)
 		}
 		debug {
 			isMinifyEnabled = false
