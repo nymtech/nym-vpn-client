d17c9a026be04631dc2343bcf5e07a974235ddb0
Jon Häggblad <jon.haggblad@gmail.com>
2024-08-14 10:45:34 +0200

    Remove commented out

--- nym-vpn-core/crates/bandwidth-controller/src/lib.rs
+++ nym-vpn-core/crates/bandwidth-controller/src/lib.rs
@@ -6,17 +6,10 @@ use crate::utils::stored_credential_to_issued_bandwidth;
 use log::{debug, error, warn};
 use nym_credential_storage::storage::Storage;
 use nym_credentials::coconut::bandwidth::issued::BandwidthCredentialIssuedDataVariant;
-// use nym_credentials::coconut::bandwidth::CredentialSpendingData;
-// use nym_credentials::coconut::utils::obtain_aggregate_verification_key;
 use nym_credentials::IssuedBandwidthCredential;
-// use nym_credentials_interface::VerificationKey;
-// use nym_validator_client::coconut::all_coconut_api_clients;
-// use nym_validator_client::nym_api::EpochId;
-// use nym_validator_client::nyxd::contract_traits::DkgQueryClient;
 
 pub use event::BandwidthStatusMessage;
 
-// pub mod acquire;
 pub mod error;
 mod event;
 mod utils;
@@ -27,18 +20,6 @@ pub struct BandwidthController<C, St> {
     client: C,
 }
 
-// pub struct PreparedCredential {
-//     /// The cryptographic material required for spending the underlying credential.
-//     pub data: CredentialSpendingData,
-//
-//     /// The (DKG) epoch id under which the credential has been issued so that the verifier
-//     /// could use correct verification key for validation.
-//     pub epoch_id: EpochId,
-//
-//     /// The database id of the stored credential.
-//     pub credential_id: i64,
-// }
-//
 pub struct RetrievedCredential {
     pub credential: IssuedBandwidthCredential,
     pub credential_id: i64,
@@ -104,58 +85,6 @@ impl<C, St: Storage> BandwidthController<C, St> {
     pub fn storage(&self) -> &St {
         &self.storage
     }
-
-    // async fn get_aggregate_verification_key(
-    //     &self,
-    //     epoch_id: EpochId,
-    // ) -> Result<VerificationKey, BandwidthControllerError>
-    // where
-    //     C: DkgQueryClient + Sync + Send,
-    //     <St as Storage>::StorageError: Send + Sync + 'static,
-    // {
-    //     let coconut_api_clients = all_coconut_api_clients(&self.client, epoch_id).await?;
-    //     Ok(obtain_aggregate_verification_key(&coconut_api_clients)?)
-    // }
-
-    // pub async fn prepare_bandwidth_credential(
-    //     &self,
-    //     gateway_id: &str,
-    // ) -> Result<PreparedCredential, BandwidthControllerError>
-    // where
-    //     C: DkgQueryClient + Sync + Send,
-    //     <St as Storage>::StorageError: Send + Sync + 'static,
-    // {
-    //     let retrieved_credential = self.get_next_usable_credential(gateway_id).await?;
-    //
-    //     let epoch_id = retrieved_credential.credential.epoch_id();
-    //     let credential_id = retrieved_credential.credential_id;
-    //
-    //     let verification_key = self.get_aggregate_verification_key(epoch_id).await?;
-    //
-    //     let spend_request = retrieved_credential
-    //         .credential
-    //         .prepare_for_spending(&verification_key)?;
-    //
-    //     Ok(PreparedCredential {
-    //         data: spend_request,
-    //         epoch_id,
-    //         credential_id,
-    //     })
-    // }
-
-    // pub async fn consume_credential(
-    //     &self,
-    //     id: i64,
-    //     gateway_id: &str,
-    // ) -> Result<(), BandwidthControllerError>
-    // where
-    //     <St as Storage>::StorageError: Send + Sync + 'static,
-    // {
-    //     self.storage
-    //         .consume_coconut_credential(id, gateway_id)
-    //         .await
-    //         .map_err(|err| BandwidthControllerError::CredentialStorageError(Box::new(err)))
-    // }
 }
 
 impl<C, St> Clone for BandwidthController<C, St>
