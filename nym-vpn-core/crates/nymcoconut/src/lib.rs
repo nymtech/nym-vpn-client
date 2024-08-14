2b8e19ca38b3a51d7997022dc746834cf2be40db
Jon Häggblad <jon.haggblad@gmail.com>
2024-08-14 13:56:29 +0200

    remove

--- nym-vpn-core/crates/nymcoconut/src/lib.rs
+++ nym-vpn-core/crates/nymcoconut/src/lib.rs
@@ -4,19 +4,15 @@
 #![warn(clippy::expect_used)]
 #![warn(clippy::unwrap_used)]
 
-// pub use elgamal::PublicKey;
 pub use error::CoconutError;
-// pub use scheme::issuance::BlindSignRequest;
 pub use scheme::keygen::SecretKey;
 pub use scheme::keygen::VerificationKey;
-// pub use scheme::verification::VerifyCredentialRequest;
 pub use scheme::BlindedSignature;
 pub use scheme::Signature;
 pub use traits::Base58;
 pub use traits::Bytable;
 pub use utils::hash_to_scalar;
 
-// pub(crate) mod elgamal;
 mod error;
 mod impls;
 mod proofs;
