4dc124164442d1c6141cdeaa7940f5e9a1103efb
pierre <dommerc.pierre@gmail.com>
2024-08-19 16:29:53 +0200

    add new langs de, hi, ja

---
+++ nym-vpn-x/src/i18n/resources/de.ts
@@ -0,0 +1,27 @@
+import common from '../de/common.json';
+import home from '../de/home.json';
+import settings from '../de/settings.json';
+import nodeLocation from '../de/node-location.json';
+import backendMessages from '../de/backend-messages.json';
+import display from '../de/display.json';
+import addCredential from '../de/add-credential.json';
+import licenses from '../de/licenses.json';
+import errors from '../de/errors.json';
+import welcome from '../de/welcome.json';
+import glossary from '../de/glossary.json';
+import notifications from '../de/notifications.json';
+
+export const de = {
+  common,
+  home,
+  settings,
+  nodeLocation,
+  backendMessages,
+  display,
+  addCredential,
+  licenses,
+  errors,
+  welcome,
+  glossary,
+  notifications,
+} as const;
---
+++ nym-vpn-x/src/i18n/resources/ja.ts
@@ -0,0 +1,27 @@
+import common from '../ja/common.json';
+import home from '../ja/home.json';
+import settings from '../ja/settings.json';
+import nodeLocation from '../ja/node-location.json';
+import backendMessages from '../ja/backend-messages.json';
+import display from '../ja/display.json';
+import addCredential from '../ja/add-credential.json';
+import licenses from '../ja/licenses.json';
+import errors from '../ja/errors.json';
+import welcome from '../ja/welcome.json';
+import glossary from '../ja/glossary.json';
+import notifications from '../ja/notifications.json';
+
+export const ja = {
+  common,
+  home,
+  settings,
+  nodeLocation,
+  backendMessages,
+  display,
+  addCredential,
+  licenses,
+  errors,
+  welcome,
+  glossary,
+  notifications,
+} as const;
