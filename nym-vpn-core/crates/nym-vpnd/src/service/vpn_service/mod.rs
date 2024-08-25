67cb22830edcf0e7847f6b72715e6b636ac25b05
Jon Häggblad <jon.haggblad@gmail.com>
2024-08-25 08:51:27 +0200

    Rename to nym_vpn_service

--- nym-vpn-core/crates/nym-vpnd/src/service/vpn_service/mod.rs
+++ nym-vpn-core/crates/nym-vpnd/src/service/vpn_service/mod.rs
@@ -2,18 +2,18 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 mod command;
+mod nym_vpn_service;
 mod response;
-mod service;
 mod shared_state;
 mod state;
 
 pub(crate) use {
     command::{ConnectArgs, ConnectOptions, VpnServiceCommand},
+    nym_vpn_service::NymVpnService,
     response::{
         VpnServiceConnectResult, VpnServiceDisconnectResult, VpnServiceInfoResult,
         VpnServiceStatusResult,
     },
-    service::NymVpnService,
     shared_state::{SharedVpnState, VpnServiceStateChange},
     state::{
         ConnectedResultDetails, ConnectedStateDetails, MixConnectedStateDetails,
