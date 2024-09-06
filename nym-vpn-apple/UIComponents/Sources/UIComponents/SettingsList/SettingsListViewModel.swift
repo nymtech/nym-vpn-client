8b7af7e6e271b7d96759396236d4f39d4df0f19d
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2024-09-06 11:46:36 +0300

    Add environment selector

--- nym-vpn-apple/UIComponents/Sources/UIComponents/SettingsList/SettingsListViewModel.swift
+++ nym-vpn-apple/UIComponents/Sources/UIComponents/SettingsList/SettingsListViewModel.swift
@@ -1,17 +1,36 @@
 import Foundation
+import ConfigurationManager
 import Theme
 
 public struct SettingsListViewModel {
     private let appVersion: String
+    private let configurationManager: ConfigurationManager
 
     let sections: [SettingsSection]
 
     var versionTitle: String {
-        "version".localizedString + " \(appVersion)"
+        "\("version".localizedString) \(appVersion)"
     }
 
-    public init(sections: [SettingsSection], appVersion: String) {
+    var isTestFlight: Bool {
+        configurationManager.isTestFlight
+    }
+
+    var envs: [Env] {
+        Env.allCases
+    }
+
+    public init(
+        sections: [SettingsSection],
+        appVersion: String,
+        configurationManager: ConfigurationManager
+    ) {
         self.sections = sections
         self.appVersion = appVersion
+        self.configurationManager = configurationManager
+    }
+
+    func changeEnvironment(to env: Env) {
+        configurationManager.updateEnv(to: env)
     }
 }
