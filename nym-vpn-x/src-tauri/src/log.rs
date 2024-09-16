d25e96655990521bf6b1878ee6abf2cf63f2086e
Pierre <dommerc.pierre@gmail.com>
2024-09-16 13:01:26 +0200

    chore: logs tracing setup (#1142)

--- nym-vpn-x/src-tauri/src/log.rs
+++ nym-vpn-x/src-tauri/src/log.rs
@@ -3,6 +3,7 @@ use std::{fs, path::PathBuf};
 use crate::envi;
 use crate::fs::path::APP_LOG_DIR;
 use anyhow::{anyhow, Result};
+use tracing::{debug, info};
 use tracing_appender::{non_blocking::WorkerGuard, rolling};
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::fmt::writer::MakeWriterExt;
@@ -12,19 +13,19 @@ const ENV_LOG_FILE: &str = "LOG_FILE";
 const LOG_FILE: &str = "app.log";
 const LOG_FILE_OLD: &str = "app.old.log";
 
-fn rotate_log_file(log_dir: PathBuf) -> Result<()> {
+fn rotate_log_file(log_dir: PathBuf) -> Result<Option<PathBuf>> {
     let log_file = log_dir.join(LOG_FILE);
     if log_file.is_file() {
         let old_file = log_dir.join(LOG_FILE_OLD);
-        println!("rotate log file to: {}", old_file.display());
         fs::rename(&log_file, &old_file).inspect_err(|e| {
             eprintln!(
                 "failed to rename log file during log rotation {}: {e}",
                 log_file.display()
             )
         })?;
+        return Ok(Some(old_file));
     }
-    Ok(())
+    Ok(None)
 }
 
 pub async fn setup_tracing(log_file: bool) -> Result<Option<WorkerGuard>> {
@@ -38,11 +39,9 @@ pub async fn setup_tracing(log_file: bool) -> Result<Option<WorkerGuard>> {
         let log_dir = APP_LOG_DIR
             .clone()
             .ok_or(anyhow!("failed to get log dir"))?;
-        let log_file = log_dir.join(LOG_FILE);
-        println!("logging to file: {}", log_file.display());
-        rotate_log_file(log_dir.clone()).ok();
+        let old = rotate_log_file(log_dir.clone()).ok().flatten();
 
-        let appender = rolling::never(log_dir, LOG_FILE);
+        let appender = rolling::never(log_dir.clone(), LOG_FILE);
         let (writer, guard) = tracing_appender::non_blocking(appender);
 
         tracing_subscriber::fmt()
@@ -51,6 +50,12 @@ pub async fn setup_tracing(log_file: bool) -> Result<Option<WorkerGuard>> {
             .with_ansi(false)
             .with_writer(std::io::stdout.and(writer))
             .init();
+
+        if let Some(old) = old {
+            debug!("rotated log file: {}", old.display());
+        }
+        let log_file = log_dir.join(LOG_FILE);
+        info!("logging to file: {}", log_file.display());
         Ok(Some(guard))
     } else {
         tracing_subscriber::fmt()
