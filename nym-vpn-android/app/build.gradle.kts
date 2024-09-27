cf6abd6496a1dfc9a6c84d8b904409ed047565b4
Zane Schepke <zanecschepke@gmail.com>
2024-09-27 09:55:00 -0400

    switch signing from local

--- nym-vpn-android/app/build.gradle.kts
+++ nym-vpn-android/app/build.gradle.kts
@@ -79,7 +79,7 @@ android {
 				"proguard-rules.pro",
 			)
 			resValue("string", "provider", "\"${Constants.APP_NAME}.provider\"")
-			signingConfig = signingConfigs.getByName("debug")
+			signingConfig = signingConfigs.getByName(Constants.RELEASE)
 		}
 		debug {
 			isMinifyEnabled = false
