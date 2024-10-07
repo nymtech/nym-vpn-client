b4a7e7fec26b697eda92d2f48f8b5e29f11d3f0d
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2024-10-07 18:34:17 +0300

    APPLE: update core v0.2.4 (#1260)

--- nym-vpn-apple/MixnetLibrary/Package.swift
+++ nym-vpn-apple/MixnetLibrary/Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "NymVpnLib",
-            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nym-vpn-core-v0.2.3/nym-vpn-core-v0.2.3_ios_universal.zip",
-            checksum: "f5fd531bfb62222bc15382d329f78a26737ea1eecc5adde4f56898a5a6156425"
+            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nym-vpn-core-v0.2.4/nym-vpn-core-v0.2.4_ios_universal.zip",
+            checksum: "e8e19f28bd5930b1481b033ddc9c8cfdf6a902591910eab2340cd0b757029b31"
         ),
         .testTarget(
             name: "MixnetLibraryTests",
--- nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
+++ nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum AppVersionProvider {
-    public static let libVersion = "0.2.3"
+    public static let libVersion = "0.2.4"
 
     public static var app: String {
         "nym-vpn-app"
