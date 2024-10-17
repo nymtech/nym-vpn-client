2cd0f10f4c659639da1583bb14d778bb8e7a279e
Jon Häggblad <jon.haggblad@gmail.com>
2024-10-17 13:57:33 +0200

    Split into functions

--- nym-vpn-core/crates/nym-vpn-account-controller/src/lib.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/lib.rs
@@ -7,9 +7,9 @@
 // 3. request ticketbooks and top up the local credential store
 
 mod controller;
+mod ecash_client;
 mod error;
 mod shared_state;
-mod ecash_client;
 
 pub use controller::{AccountCommand, AccountController};
 pub use shared_state::SharedAccountState;
