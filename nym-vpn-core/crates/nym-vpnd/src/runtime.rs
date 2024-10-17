c232aea9ccce1c69f59f36ff66c159ff9b001876
Andrej Mihajlov <andrej@nymtech.net>
2024-10-17 14:49:00 +0200

    Increase number of works to 10

--- nym-vpn-core/crates/nym-vpnd/src/runtime.rs
+++ nym-vpn-core/crates/nym-vpnd/src/runtime.rs
@@ -3,7 +3,7 @@ use tokio::runtime::{Builder, Runtime};
 pub fn new_runtime() -> Runtime {
     Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(4)
+        .worker_threads(10)
         .build()
         .unwrap()
 }
