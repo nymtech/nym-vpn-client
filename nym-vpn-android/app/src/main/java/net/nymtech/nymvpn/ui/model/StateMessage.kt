cb423cf3f9f99acd2a200e0a1d683754a91a52ba
Zane Schepke <zanecschepke@gmail.com>
2024-10-23 01:38:08 -0400

    android: initial state machine integration

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/model/StateMessage.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/model/StateMessage.kt
@@ -1,9 +1,10 @@
 package net.nymtech.nymvpn.ui.model
 
 import net.nymtech.nymvpn.util.StringValue
+import nym_vpn_lib.ErrorStateReason
 import nym_vpn_lib.VpnException
 
 sealed class StateMessage {
 	data class Status(val message: StringValue) : StateMessage()
-	data class Error(val exception: VpnException) : StateMessage()
+	data class Error(val reason: ErrorStateReason) : StateMessage()
 }
--- nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn/model/BackendMessage.kt
+++ nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn/model/BackendMessage.kt
@@ -1,10 +1,12 @@
 package net.nymtech.vpn.model
 
+import nym_vpn_lib.BandwidthEvent
 import nym_vpn_lib.BandwidthStatus
+import nym_vpn_lib.ErrorStateReason
 import nym_vpn_lib.VpnException
 
 sealed class BackendMessage {
-	data class Failure(val exception: VpnException) : BackendMessage()
-	data class BandwidthAlert(val status: BandwidthStatus) : BackendMessage()
+	data class Failure(val reason: ErrorStateReason) : BackendMessage()
+	data class BandwidthAlert(val status: BandwidthEvent) : BackendMessage()
 	data object None : BackendMessage()
 }
