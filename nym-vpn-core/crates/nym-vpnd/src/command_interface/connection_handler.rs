d56a9545513b63b078b307947a02343e8d0a0227
Jon Häggblad <jon.haggblad@gmail.com>
2024-10-23 13:48:20 +0200

    Update another set of names

--- nym-vpn-core/crates/nym-vpnd/src/command_interface/connection_handler.rs
+++ nym-vpn-core/crates/nym-vpnd/src/command_interface/connection_handler.rs
@@ -142,21 +142,22 @@ impl CommandInterfaceConnectionHandler {
     pub(crate) async fn handle_get_account_state(
         &self,
     ) -> Result<Result<AccountStateSummary, AccountError>, VpnCommandSendError> {
-        self.send_and_wait(VpnServiceCommand::GetLocalAccountState, ())
+        self.send_and_wait(VpnServiceCommand::GetAccountState, ())
             .await
     }
 
     pub(crate) async fn handle_fetch_raw_account_summary(
         &self,
     ) -> Result<Result<NymVpnAccountSummaryResponse, AccountError>, VpnCommandSendError> {
-        self.send_and_wait(VpnServiceCommand::GetAccountSummary, ())
+        self.send_and_wait(VpnServiceCommand::FetchRawAccountSummary, ())
             .await
     }
 
     pub(crate) async fn handle_fetch_raw_devices(
         &self,
     ) -> Result<Result<NymVpnDevicesResponse, AccountError>, VpnCommandSendError> {
-        self.send_and_wait(VpnServiceCommand::GetDevices, ()).await
+        self.send_and_wait(VpnServiceCommand::FetchRawDevices, ())
+            .await
     }
 
     pub(crate) async fn handle_register_device(
