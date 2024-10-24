875c6f5e038304418fef022087e5bfe2d031aadb
Zane Schepke <zanecschepke@gmail.com>
2024-10-23 23:25:02 -0400

    add path

--- nym-vpn-android/nym-vpn-client/src/main/scripts/build-libs.sh
+++ nym-vpn-android/nym-vpn-client/src/main/scripts/build-libs.sh
@@ -3,6 +3,12 @@
 echo "Building WireGuard dep"
 echo "Working dir: $PWD"
 echo "NDK_HOME: $1"
+
+# Ensure Go compiler is accessible
+export GOROOT="$HOME/go"
+export PATH="$PATH:$GOROOT/bin"
+source "$HOME/.cargo/env"
+
 #fix to work with different OS
 archDir=$(basename $1/toolchains/llvm/prebuilt/*/)
 echo "archdir: ${archDir}"
