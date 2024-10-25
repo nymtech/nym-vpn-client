3792208721b8201c4a70ad9c315f2a1ef0426809
Zane Schepke <zanecschepke@gmail.com>
2024-10-25 16:22:55 -0400

    fix script

--- nym-vpn-android/scripts/fdroid-prebuild.sh
+++ nym-vpn-android/scripts/fdroid-prebuild.sh
@@ -8,7 +8,8 @@ export NDK_TOOLCHAIN_DIR="$NDK_PATH/toolchains/llvm/prebuilt/$(basename $NDK_PAT
 bash ../wireguard/build-wireguard-go.sh
 bash ../wireguard/libwg/build-android.sh
 
-(cd ../nym-vpn-core/crates/nym-vpn-lib; cargo ndk -t arm64-v8a -o ../../../nym-vpn-android/nym-vpn-client/src/main/jniLibs build --release)
-(cd ../nym-vpn-core; cargo run --bin uniffi-bindgen generate --library target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n)
-cargo license -j --avoid-dev-deps --current-dir ../nym-vpn-core/crates/nym-vpn-lib --filter-platform aarch64-linux-android --avoid-build-deps > ./nym-vpn-client/src/main/assets/licenses_rust.json
-mv ../android/app/build/extraJni/arm64-v8a/libwg.so nym-vpn-client/src/main/jniLibs/arm64-v8a/
+(cd $PWD/../nym-vpn-core/crates/nym-vpn-lib; cargo ndk -t arm64-v8a -o ../../../nym-vpn-android/nym-vpn-client/src/main/jniLibs build --release)
+(cd $PWD/../nym-vpn-core; cargo run --bin uniffi-bindgen generate --library ./target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n)
+cargo license -j --avoid-dev-deps --current-dir ../../nym-vpn-core/crates/nym-vpn-lib --filter-platform aarch64-linux-android --avoid-build-deps > ./src/main/assets/licenses_rust.json
+
+mv $PWD/../../android/app/build/extraJni/arm64-v8a/libwg.so $PWD/src/main/jniLibs/arm64-v8a/
