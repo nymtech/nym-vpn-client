fef9224eaaff3194d8d77427d2b375df9a966da2
Zane Schepke <zanecschepke@gmail.com>
2024-10-25 07:33:28 -0400

    remove export

--- nym-vpn-android/scripts/fdroid-prebuild.sh
+++ nym-vpn-android/scripts/fdroid-prebuild.sh
@@ -29,9 +29,5 @@ echo "$GOLANG_HASH go.tgz" | sha256sum -c
 tar -xzvf go.tgz
 patch -p1 -f -N -r- -d "$HOME/go" < "$REPO_DIR/wireguard/libwg/goruntime-boottime-over-monotonic.diff"
 
-# Ensure Go compiler is accessible
-export GOROOT="$HOME/go"
-export PATH="$PATH:$GOROOT/bin"
-
 # install cargo dependencies
 cargo install cargo-ndk cargo-license --locked
