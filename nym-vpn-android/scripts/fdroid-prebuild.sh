e301c012c44d949188ecdcafc923606053389c20
Zane Schepke <zanecschepke@gmail.com>
2024-10-25 16:06:42 -0400

    Update fdroid-prebuild.sh

--- nym-vpn-android/scripts/fdroid-prebuild.sh
+++ nym-vpn-android/scripts/fdroid-prebuild.sh
@@ -9,6 +9,6 @@ bash ../wireguard/build-wireguard-go.sh
 bash ../wireguard/libwg/build-android.sh
 
 (cd ../nym-vpn-core/crates/nym-vpn-lib; cargo ndk -t arm64-v8a -o ../../../nym-vpn-android/nym-vpn-client/src/main/jniLibs build --release)
-(cd ../nym-vpn-core; cargo run --bin uniffi-bindgen generate --library ./target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n)
+(cd ../nym-vpn-core; cargo run --bin uniffi-bindgen generate --library target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n)
 cargo license -j --avoid-dev-deps --current-dir ../nym-vpn-core/crates/nym-vpn-lib --filter-platform aarch64-linux-android --avoid-build-deps > ./nym-vpn-client/src/main/assets/licenses_rust.json
 mv ../android/app/build/extraJni/arm64-v8a/libwg.so nym-vpn-client/src/main/jniLibs/arm64-v8a/
