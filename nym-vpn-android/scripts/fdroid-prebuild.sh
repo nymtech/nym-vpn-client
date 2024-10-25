b44f7455cc7b55ab0b0d58ff82e5818697294e48
Zane Schepke <zanecschepke@gmail.com>
2024-10-25 15:07:49 -0400

    remove rustflags, add prebuild script

--- nym-vpn-android/scripts/fdroid-prebuild.sh
+++ nym-vpn-android/scripts/fdroid-prebuild.sh
@@ -1,33 +1,14 @@
 #!/usr/bin/env bash
-
-set -eux
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-REPO_DIR="$SCRIPT_DIR/../../"
-
-# Install Rust
-curl -sf -L https://sh.rustup.rs > /tmp/rustup.sh
-chmod +x /tmp/rustup.sh
-/tmp/rustup.sh -y
-# shellcheck source=/dev/null
+echo "Building dependencies"
+echo "NDK_HOME: $NDK_PATH"
 source "$HOME/.cargo/env"
-rustup set profile minimal
-rustup target add \
-    i686-linux-android \
-    x86_64-linux-android \
-    aarch64-linux-android \
-    armv7-linux-androideabi
+export PATH="$PATH:$HOME/go/bin"
+export NDK_TOOLCHAIN_DIR="$NDK_PATH/toolchains/llvm/prebuilt/$(basename $NDK_PATH/toolchains/llvm/prebuilt/*/)/bin"
 
-# Install golang
-GOLANG_VERSION="1.21.3"
-# Checksum from: https://golang.org/dl/
-GOLANG_HASH="1241381b2843fae5a9707eec1f8fb2ef94d827990582c7c7c32f5bdfbfd420c8"
-cd "$HOME"
-curl -sf -L -o go.tgz https://go.dev/dl/go${GOLANG_VERSION}.linux-amd64.tar.gz
-echo "$GOLANG_HASH go.tgz" | sha256sum -c
-tar -xzvf go.tgz
-patch -p1 -f -N -r- -d "$HOME/go" < "$REPO_DIR/wireguard/libwg/goruntime-boottime-over-monotonic.diff"
+bash ../wireguard/build-wireguard-go.sh
+bash ../wireguard/libwg/build-android.sh
 
-# install cargo dependencies
-cargo install cargo-ndk cargo-license --locked
+(cd ../nym-vpn-core/crates/nym-vpn-lib; cargo ndk -t arm64-v8a -o ../../../nym-vpn-android/nym-vpn-client/src/main/jniLibs build --release)
+(cd ../nym-vpn-core; cargo run --bin uniffi-bindgen generate --library ./target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n)
+cargo license -j --avoid-dev-deps --current-dir ../nym-vpn-core/crates/nym-vpn-lib --filter-platform aarch64-linux-android --avoid-build-deps > ./nym-vpn-client/src/main/assets/licenses_rust.json
+mv ../android/app/build/extraJni/arm64-v8a/libwg.so nym-vpn-client/src/main/jniLibs/arm64-v8a/
