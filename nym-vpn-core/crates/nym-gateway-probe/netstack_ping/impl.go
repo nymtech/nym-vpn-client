0883f092b4cb492ae0bf827f98a0ea119b7f403e
durch <durch@users.noreply.github.com>
2024-11-10 21:22:46 +0100

    Fix struct name

--- nym-vpn-core/crates/nym-gateway-probe/netstack_ping/impl.go
+++ nym-vpn-core/crates/nym-gateway-probe/netstack_ping/impl.go
@@ -23,7 +23,7 @@ func init() {
 	NetstackCallImpl = Netstack{}
 }
 
-func (Netstack) ping(req NetstackRequest) NetstackResponse {
+func (Netstack) ping(req NetstackRequestGo) NetStackResponse {
 
 	fmt.Printf("Endpoint: %s\n", req.endpoint)
 	fmt.Printf("WireGuard IP: %s\n", req.wg_ip)
