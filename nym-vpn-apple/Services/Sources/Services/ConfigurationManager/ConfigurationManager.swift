7f8ccd0872012fdce36788a86935d6e26cc78e85
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2024-11-12 23:18:45 +0200

    Update configuration manager

--- nym-vpn-apple/Services/Sources/Services/ConfigurationManager/ConfigurationManager.swift
+++ nym-vpn-apple/Services/Sources/Services/ConfigurationManager/ConfigurationManager.swift
@@ -59,40 +59,26 @@ public final class ConfigurationManager {
 #endif
 
     public func setup() async throws {
-        let result: Bool = await Task(priority: .background) {
-            do {
 #if os(iOS)
-                try await setEnvVariables()
-                return true
+        try await setEnvVariables()
 #elseif os(macOS)
-                try setDaemonEnvironmentVariables()
-                return true
+        try setDaemonEnvironmentVariables()
 #endif
-            } catch {
-                return false
-            }
-        }.value
     }
 
     public func updateEnv(to env: Env) {
         Task(priority: .background) {
-            print("change to: \(env.rawValue)")
             guard isTestFlight || Device.isMacOS,
                   env != currentEnv
             else {
                 return
             }
-            Task { @MainActor in
-                currentEnv = env
-                print(currentEnv)
-            }
-
+            currentEnv = env
             do {
                 try await setup()
             } catch {
                 logger.error("Failed to set env to \(env.rawValue): \(error.localizedDescription)")
             }
-
             environmentDidChange?()
         }
     }
@@ -101,31 +87,9 @@ public final class ConfigurationManager {
 private extension ConfigurationManager {
 #if os(iOS)
     func setEnvVariables() async throws {
-        try initEnvironment(networkName: currentEnv.rawValue)
-        logger.info("🔥 Enabling env \(currentEnv.rawValue)")
-
-        let result = try await verifyEnvVariables()
-        if !result {
-            logger.error("Failed verifying env. Current env: \(currentEnv.rawValue)")
-            throw GeneralNymError.noEnv
-        }
+        try await initEnvironment(networkName: currentEnv.rawValue)
     }
 
-    func verifyEnvVariables(retryCount: Int = 0) async throws -> Bool {
-        guard retryCount < 6
-        else {
-            return false
-        }
-        let libEnv = try currentEnvironment()
-
-        guard libEnv.nymNetwork.networkName != currentEnv.rawValue
-        else {
-            try await Task.sleep(for: .seconds(1))
-            return try await verifyEnvVariables(retryCount: retryCount + 1)
-        }
-
-        return true
-    }
 #elseif os(macOS)
 
     func setDaemonEnvironmentVariables() throws {
