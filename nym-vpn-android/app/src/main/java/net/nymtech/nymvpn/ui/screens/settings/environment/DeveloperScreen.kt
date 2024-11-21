69e3415efc26878b0c7dd923beb835f5b2afcbfa
Zane Schepke <zanecschepke@gmail.com>
2024-11-21 09:46:04 -0500

    refactor credential mode selection

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/environment/DeveloperScreen.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/environment/DeveloperScreen.kt
@@ -13,12 +13,19 @@ import androidx.compose.material.icons.outlined.AdminPanelSettings
 import androidx.compose.material.icons.outlined.AirlineStops
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material.icons.outlined.Key
+import androidx.compose.material.icons.outlined.Place
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuAnchorType
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -34,7 +41,6 @@ import net.nymtech.nymvpn.ui.AppUiState
 import net.nymtech.nymvpn.ui.AppViewModel
 import net.nymtech.nymvpn.ui.Route
 import net.nymtech.nymvpn.ui.common.Modal
-import net.nymtech.nymvpn.ui.common.buttons.IconSurfaceButton
 import net.nymtech.nymvpn.ui.common.buttons.MainStyledButton
 import net.nymtech.nymvpn.ui.common.buttons.ScaledSwitch
 import net.nymtech.nymvpn.ui.common.buttons.surface.SelectionItem
@@ -48,6 +54,7 @@ import net.nymtech.nymvpn.util.extensions.scaledHeight
 import net.nymtech.nymvpn.util.extensions.scaledWidth
 import net.nymtech.vpn.backend.Tunnel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DeveloperScreen(appUiState: AppUiState, appViewModel: AppViewModel, viewModel: DeveloperViewModel = hiltViewModel()) {
 	val navController = LocalNavController.current
@@ -57,6 +64,18 @@ fun DeveloperScreen(appUiState: AppUiState, appViewModel: AppViewModel, viewMode
 	var showEntryModal by remember { mutableStateOf(false) }
 	var showExitModal by remember { mutableStateOf(false) }
 	var gatewayId by remember { mutableStateOf("") }
+	var environmentExpanded by remember { mutableStateOf(false) }
+	var credentialExpanded by remember { mutableStateOf(false) }
+
+	val credentialMode by remember {
+		derivedStateOf {
+			when (appUiState.settings.isCredentialMode) {
+				true -> CredentialMode.ON
+				false -> CredentialMode.OFF
+				null -> CredentialMode.DEFAULT
+			}
+		}
+	}
 
 	LaunchedEffect(Unit) {
 		appViewModel.onNavBarStateChange(
@@ -115,112 +134,156 @@ fun DeveloperScreen(appUiState: AppUiState, appViewModel: AppViewModel, viewMode
 			.padding(top = 24.dp.scaledHeight())
 			.padding(horizontal = 24.dp.scaledWidth()),
 	) {
-		enumValues<Tunnel.Environment>().forEach {
-			IconSurfaceButton(
-				title = it.name,
-				onClick = {
-					if (appUiState.settings.environment == it) return@IconSurfaceButton
-					appViewModel.logout()
-					viewModel.onEnvironmentChange(it)
-				},
-				selected = appUiState.settings.environment == it,
-			)
-		}
 		SurfaceSelectionGroupButton(
-			buildList {
-				addAll(
-					listOf(
-						SelectionItem(
-							Icons.Outlined.AdminPanelSettings,
-							{
-								ScaledSwitch(
-									appUiState.settings.isManualGatewayOverride,
-									onClick = { viewModel.onManualGatewayOverride(it) },
-								)
-							},
-							title = { Text("Manual gateways", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
-							description = {
-								Text(
-									"Override country selection",
-									style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
-								)
-							},
-						),
-						SelectionItem(
-							Icons.Outlined.AirlineStops,
-							{
-								val icon = Icons.Outlined.Edit
-								Icon(icon, icon.name)
-							},
-							title = { Text("Entry gateway id", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
-							description = appUiState.settings.entryGatewayId?.let {
-								{
-									Text(
-										it,
-										style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
-										modifier = Modifier.clickable { clipboardManager.setText(AnnotatedString(it)) },
+			listOf(
+				SelectionItem(
+					Icons.Outlined.Place,
+					title = {
+						ExposedDropdownMenuBox(
+							expanded = environmentExpanded,
+							onExpandedChange = { environmentExpanded = it },
+						) {
+							TextField(
+								value = appUiState.settings.environment.name,
+								onValueChange = {},
+								readOnly = true,
+								textStyle = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.onSurface),
+								trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = environmentExpanded) },
+								modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable, true),
+							)
+							ExposedDropdownMenu(
+								expanded = environmentExpanded,
+								onDismissRequest = { environmentExpanded = false },
+							) {
+								enumValues<Tunnel.Environment>().forEach { item ->
+									DropdownMenuItem(
+										text = { Text(text = item.name) },
+										onClick = {
+											if (appUiState.settings.environment == item) return@DropdownMenuItem
+											appViewModel.logout()
+											viewModel.onEnvironmentChange(item)
+											environmentExpanded = false
+										},
 									)
 								}
-							},
-							onClick = {
-								gatewayId = appUiState.settings.entryGatewayId ?: ""
-								showEntryModal = true
-							},
-						),
-						SelectionItem(
-							Icons.Outlined.AirlineStops,
-							{
-								val icon = Icons.Outlined.Edit
-								Icon(icon, icon.name)
-							},
-							title = { Text("Exit gateway id", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
-							description = appUiState.settings.exitGatewayId?.let {
-								{
-									Text(
-										it,
-										style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
-										modifier = Modifier.clickable { clipboardManager.setText(AnnotatedString(it)) },
+							}
+						}
+					},
+					description = {
+						Text(
+							"Environment",
+							style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
+						)
+					},
+					trailing = {},
+				),
+				SelectionItem(
+					Icons.Outlined.Key,
+					title = {
+						ExposedDropdownMenuBox(
+							expanded = credentialExpanded,
+							onExpandedChange = { credentialExpanded = it },
+						) {
+							TextField(
+								value = credentialMode.name,
+								onValueChange = {},
+								readOnly = true,
+								textStyle = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.onSurface),
+								trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = environmentExpanded) },
+								modifier = Modifier.menuAnchor(MenuAnchorType.PrimaryNotEditable, true),
+							)
+							ExposedDropdownMenu(
+								expanded = credentialExpanded,
+								onDismissRequest = { credentialExpanded = false },
+							) {
+								enumValues<CredentialMode>().forEach { item ->
+									DropdownMenuItem(
+										text = { Text(text = item.name) },
+										onClick = {
+											if (credentialMode == item) return@DropdownMenuItem
+											when (item) {
+												CredentialMode.DEFAULT -> viewModel.onCredentialOverride(null)
+												CredentialMode.ON -> viewModel.onCredentialOverride(true)
+												CredentialMode.OFF -> viewModel.onCredentialOverride(false)
+											}
+											credentialExpanded = false
+										},
 									)
 								}
-							},
-							onClick = {
-								gatewayId = appUiState.settings.exitGatewayId ?: ""
-								showExitModal = true
-							},
-						),
-						SelectionItem(
-							Icons.Outlined.AdminPanelSettings,
-							{
-								ScaledSwitch(
-									appUiState.settings.isCredentialMode != null,
-									onClick = { viewModel.onCredentialOverride(if (it) false else null) },
-								)
-							},
-							title = { Text("Credential override", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
-							description = {
-								Text(
-									"Override credential defaults",
-									style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
-								)
-							},
-						),
-					),
-				)
-				if (appUiState.settings.isCredentialMode != null) {
-					add(
-						SelectionItem(
-							Icons.Outlined.Key,
-							{
-								ScaledSwitch(
-									appUiState.settings.isCredentialMode == true,
-									onClick = { viewModel.onCredentialOverride(it) },
-								)
-							},
-							title = { Text("Credential mode enabled", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
-						),
-					)
-				}
-			},
+							}
+						}
+					},
+					description = {
+						Text(
+							"Credential mode",
+							style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
+						)
+					},
+					trailing = {},
+				),
+			),
+		)
+		SurfaceSelectionGroupButton(
+			listOf(
+				SelectionItem(
+					Icons.Outlined.AdminPanelSettings,
+					{
+						ScaledSwitch(
+							appUiState.settings.isManualGatewayOverride,
+							onClick = { viewModel.onManualGatewayOverride(it) },
+						)
+					},
+					title = { Text("Manual gateways", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
+					description = {
+						Text(
+							"Override country selection",
+							style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
+						)
+					},
+				),
+				SelectionItem(
+					Icons.Outlined.AirlineStops,
+					{
+						val icon = Icons.Outlined.Edit
+						Icon(icon, icon.name)
+					},
+					title = { Text("Entry gateway id", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
+					description = appUiState.settings.entryGatewayId?.let {
+						{
+							Text(
+								it,
+								style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
+								modifier = Modifier.clickable { clipboardManager.setText(AnnotatedString(it)) },
+							)
+						}
+					},
+					onClick = {
+						gatewayId = appUiState.settings.entryGatewayId ?: ""
+						showEntryModal = true
+					},
+				),
+				SelectionItem(
+					Icons.Outlined.AirlineStops,
+					{
+						val icon = Icons.Outlined.Edit
+						Icon(icon, icon.name)
+					},
+					title = { Text("Exit gateway id", style = MaterialTheme.typography.bodyLarge.copy(MaterialTheme.colorScheme.onSurface)) },
+					description = appUiState.settings.exitGatewayId?.let {
+						{
+							Text(
+								it,
+								style = MaterialTheme.typography.bodyMedium.copy(MaterialTheme.colorScheme.outline),
+								modifier = Modifier.clickable { clipboardManager.setText(AnnotatedString(it)) },
+							)
+						}
+					},
+					onClick = {
+						gatewayId = appUiState.settings.exitGatewayId ?: ""
+						showExitModal = true
+					},
+				),
+			),
 		)
 	}
 }
