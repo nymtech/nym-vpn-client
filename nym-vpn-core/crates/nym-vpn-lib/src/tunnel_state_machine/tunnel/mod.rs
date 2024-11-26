4b7396fb99056b26f6518cc8d0e53518fcd3c1ae
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2024-11-26 20:05:03 +0200

    Do it in state machine

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel/mod.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel/mod.rs
@@ -143,7 +143,7 @@ pub async fn connect_mixnet(
             // Always disable poisson process for outbound traffic in wireguard.
             mixnet_client_config.disable_poisson_rate = true;
             // Always disable background cover traffic in wireguard.
-            mixnet_client_config.disable_background_cover_traffic = true;
+            mixnet_client_config.disable_background_cover_traffic = false;
         }
     };
 
