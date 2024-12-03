a5a812b1e4b4e8be89fb4fc5626c3352f7e7a280
Jon Häggblad <jon.haggblad@gmail.com>
2024-12-03 13:52:05 +0100

    Warn for unwrap

--- nym-vpn-core/crates/nym-vpn-account-controller/src/lib.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/lib.rs
@@ -1,5 +1,6 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
+#![warn(clippy::panic, clippy::unwrap_used, clippy::expect_used)]
 
 // The account controller is responsible for
 // 1. checking if the account exists
