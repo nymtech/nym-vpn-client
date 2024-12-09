63c62aee8122c195c7a6978573c65c79ba754ae7
benedettadavico <benedetta.davico@gmail.com>
2024-12-09 21:15:58 +0100

    Add a fix to support multiple minor versions for v4

--- nym-vpn-core/crates/nym-authenticator-client/src/lib.rs
+++ nym-vpn-core/crates/nym-authenticator-client/src/lib.rs
@@ -800,7 +800,7 @@ impl From<semver::Version> for AuthenticatorVersion {
         if semver.minor == 1 && semver.patch >= 10 {
             return Self::V3;
         }
-        if semver.minor >= 1 && semver.patch >= 0 {
+        if semver.minor >= 1 {
             return Self::V4;
         }
         Self::UNKNOWN
