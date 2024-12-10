2e36c34307709961d9d5a8884aee2f3460be7e1f
Andrej Mihajlov <andrej@nymtech.net>
2024-12-10 15:26:27 +0100

    clippy: fix

--- nym-vpn-core/crates/nym-vpn-api-client/src/response.rs
+++ nym-vpn-core/crates/nym-vpn-api-client/src/response.rs
@@ -389,7 +389,7 @@ impl ProbeOutcome {
     pub fn is_fully_operational_exit(&self) -> bool {
         self.as_entry.can_connect
             && self.as_entry.can_route
-            && self.as_exit.as_ref().map_or(false, |exit| {
+            && self.as_exit.as_ref().is_some_and(|exit| {
                 exit.can_connect
                     && exit.can_route_ip_v4
                     && exit.can_route_ip_external_v4
