31f4c1649468babfb4ff82c60391ef26454033c8
Zane Schepke <zanecschepke@gmail.com>
2024-12-12 22:12:38 -0500

    bump version

--- nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
+++ nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Constants {
-	const val VERSION_NAME = "v1.1.5"
-    const val VERSION_CODE = 11500
+	const val VERSION_NAME = "v1.1.6"
+    const val VERSION_CODE = 11600
     const val TARGET_SDK = 35
     const val COMPILE_SDK = 35
     const val MIN_SDK = 24
