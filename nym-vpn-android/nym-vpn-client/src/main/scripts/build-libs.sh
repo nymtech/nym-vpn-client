b704e3161e765890f3701465781421a3a2ad879e
Andrej Mihajlov <andrej@nymtech.net>
2024-12-13 16:53:58 +0100

    Use pushd to scope calls

--- nym-vpn-android/nym-vpn-client/src/main/scripts/build-libs.sh
+++ nym-vpn-android/nym-vpn-client/src/main/scripts/build-libs.sh
@@ -16,13 +16,17 @@ echo "Building nym-vpn-lib dep"
 
 #fix emulators later
 #(cd $PWD/src/tools/nym-vpn-client/crates/nym-vpn-lib; cargo ndk -t armeabi-v7a -t arm64-v8a -t i686-linux-android -t x86_64-linux-android  -o ../../../main/jniLibs build --release)
-(cd $PWD/../../nym-vpn-core/crates/nym-vpn-lib; cargo ndk -t arm64-v8a -o ../../../nym-vpn-android/nym-vpn-client/src/main/jniLibs build --release)
+pushd $PWD/../../nym-vpn-core/crates/nym-vpn-lib
+cargo ndk -t arm64-v8a -o ../../../nym-vpn-android/nym-vpn-client/src/main/jniLibs build --release
+popd
 
-(cd $PWD/../../nym-vpn-core; cargo run --bin uniffi-bindgen generate --library ./target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n)
+pushd $PWD/../../nym-vpn-core
+cargo run --bin uniffi-bindgen generate --library ./target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n
+popd
+
 cargo license -j --avoid-dev-deps --current-dir ../../nym-vpn-core/crates/nym-vpn-lib --filter-platform aarch64-linux-android --avoid-build-deps > ./src/main/assets/licenses_rust.json
 
 mv $PWD/../../nym-vpn-android/app/build/extraJni/arm64-v8a/libwg.so $PWD/src/main/jniLibs/arm64-v8a/
 #mv $PWD/src/tools/nym-vpn-client/nym-vpn-android/app/build/extraJni/armeabi-v7a/libwg.so $PWD/src/main/jniLibs/armeabi-v7a/
 #mv $PWD/src/tools/nym-vpn-client/nym-vpn-android/app/build/extraJni/x86/libwg.so $PWD/src/main/jniLibs/x86/
 #mv $PWD/src/tools/nym-vpn-client/nym-vpn-android/app/build/extraJni/x86_64/libwg.so $PWD/src/main/jniLibs/x86_64/
-
