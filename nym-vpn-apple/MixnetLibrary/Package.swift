ffa32a1a25453ca52bc2ff51fd20bdf5d471a175
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2024-12-17 20:39:52 +0200

    APPLE: update core

--- nym-vpn-apple/MixnetLibrary/Package.swift
+++ nym-vpn-apple/MixnetLibrary/Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "NymVpnLib",
-            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nym-vpn-core-v1.1.0-beta.4/nym-vpn-core-v1.1.0-beta.4_ios_universal.zip",
-            checksum: "d16a0ad4e241721a17462917b3d7f609204179222cd308559cf23f6e9c12aba5"
+            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nym-vpn-core-v1.1.0-amneziawg/nym-vpn-core-v1.1.0_ios_universal.zip",
+            checksum: "05dc056761c94b0a26992faa978debfb1edf3487e24e3cdc67b3fab3c73183b9"
         ),
 //        .binaryTarget(
 //            name: "NymVpnLib",
