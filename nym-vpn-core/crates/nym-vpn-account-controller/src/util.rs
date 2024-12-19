0be5ccb98ebe0d253022b69ef7faba222dc2e1cb
Jon Häggblad <jon.haggblad@gmail.com>
2024-12-19 11:41:43 +0100

    Delete files explicitly instead of globbing

--- nym-vpn-core/crates/nym-vpn-account-controller/src/util.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/util.rs
@@ -1,7 +1,25 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{ffi::OsStr, fs, path::Path};
+use std::{fs, path::Path};
+
+use nym_client_core::config::disk_persistence::{
+    DEFAULT_ACK_KEY_FILENAME, DEFAULT_GATEWAYS_DETAILS_DB_FILENAME,
+    DEFAULT_PRIVATE_ENCRYPTION_KEY_FILENAME, DEFAULT_PRIVATE_IDENTITY_KEY_FILENAME,
+    DEFAULT_PUBLIC_ENCRYPTION_KEY_FILENAME, DEFAULT_PUBLIC_IDENTITY_KEY_FILENAME,
+    DEFAULT_REPLY_SURB_DB_FILENAME,
+};
+use nym_vpn_store::keys::persistence::{
+    DEFAULT_PRIVATE_DEVICE_KEY_FILENAME, DEFAULT_PUBLIC_DEVICE_KEY_FILENAME,
+};
+use nym_wg_gateway_client::{
+    DEFAULT_FREE_PRIVATE_ENTRY_WIREGUARD_KEY_FILENAME,
+    DEFAULT_FREE_PRIVATE_EXIT_WIREGUARD_KEY_FILENAME,
+    DEFAULT_FREE_PUBLIC_ENTRY_WIREGUARD_KEY_FILENAME,
+    DEFAULT_FREE_PUBLIC_EXIT_WIREGUARD_KEY_FILENAME, DEFAULT_PRIVATE_ENTRY_WIREGUARD_KEY_FILENAME,
+    DEFAULT_PRIVATE_EXIT_WIREGUARD_KEY_FILENAME, DEFAULT_PUBLIC_ENTRY_WIREGUARD_KEY_FILENAME,
+    DEFAULT_PUBLIC_EXIT_WIREGUARD_KEY_FILENAME,
+};
 
 use crate::Error;
 
@@ -9,25 +27,49 @@ use crate::Error;
 // protect us against the names drifting out of sync.
 
 pub fn remove_files_for_account(data_dir: &Path) -> Result<(), Error> {
-    // Delete all files in data_dir that matches any of these patterns:
-    // *.sqlite
-    // *.pem
+    let device_key = [
+        DEFAULT_PRIVATE_DEVICE_KEY_FILENAME,
+        DEFAULT_PUBLIC_DEVICE_KEY_FILENAME,
+    ];
 
-    for file in fs::read_dir(data_dir).map_err(Error::internal)? {
-        let file = match file {
-            Ok(file) => file,
-            Err(err) => {
-                tracing::error!("failed to read file in data directory: {:?}", err);
-                continue;
-            }
-        };
+    let mixnet_keys = [
+        DEFAULT_PRIVATE_IDENTITY_KEY_FILENAME,
+        DEFAULT_PUBLIC_IDENTITY_KEY_FILENAME,
+        DEFAULT_PRIVATE_ENCRYPTION_KEY_FILENAME,
+        DEFAULT_PUBLIC_ENCRYPTION_KEY_FILENAME,
+        DEFAULT_ACK_KEY_FILENAME,
+    ];
 
-        if Some(OsStr::new("sqlite")) == file.path().extension()
-            || Some(OsStr::new("pem")) == file.path().extension()
-        {
-            tracing::info!("removing file: {:?}", file.path());
-            fs::remove_file(file.path())
-                .inspect_err(|err| tracing::error!("failed to remove file: {:?}", err))
+    let wireguard_keys = [
+        DEFAULT_PRIVATE_ENTRY_WIREGUARD_KEY_FILENAME,
+        DEFAULT_PUBLIC_ENTRY_WIREGUARD_KEY_FILENAME,
+        DEFAULT_PRIVATE_EXIT_WIREGUARD_KEY_FILENAME,
+        DEFAULT_PUBLIC_EXIT_WIREGUARD_KEY_FILENAME,
+        DEFAULT_FREE_PRIVATE_ENTRY_WIREGUARD_KEY_FILENAME,
+        DEFAULT_FREE_PUBLIC_ENTRY_WIREGUARD_KEY_FILENAME,
+        DEFAULT_FREE_PRIVATE_EXIT_WIREGUARD_KEY_FILENAME,
+        DEFAULT_FREE_PUBLIC_EXIT_WIREGUARD_KEY_FILENAME,
+    ];
+
+    let mixnet_db = [
+        DEFAULT_REPLY_SURB_DB_FILENAME,
+        DEFAULT_GATEWAYS_DETAILS_DB_FILENAME,
+    ];
+
+    let files_to_remove = device_key
+        .iter()
+        .chain(mixnet_keys.iter())
+        .chain(wireguard_keys.iter())
+        .chain(mixnet_db.iter());
+
+    for file in files_to_remove {
+        let file_path = data_dir.join(file);
+        if file_path.exists() {
+            tracing::info!("removing file: {:?}", file);
+            fs::remove_file(file_path)
+                .inspect_err(|err| {
+                    tracing::error!("failed to remove file: {err:?}");
+                })
                 .ok();
         }
     }
