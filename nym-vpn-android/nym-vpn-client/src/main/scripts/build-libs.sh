86e836efa60d1273eb31bd4be7bb44bbc0e11385
Zane Schepke <zanecschepke@gmail.com>
2024-12-20 15:55:44 -0500

    fix: build script for local

--- nym-vpn-android/nym-vpn-client/src/main/scripts/build-libs.sh
+++ nym-vpn-android/nym-vpn-client/src/main/scripts/build-libs.sh
@@ -2,23 +2,38 @@
 
 set -euo pipefail
 
-# requires cargo, cargo-ndk and android NDK to be installed
-echo "Building WireGuard dep"
+# requires cargo, go, cargo-ndk and android NDK to be installed
 echo "Working dir: $PWD"
 echo "NDK_HOME: $1"
+
 #fix to work with different OS
 archDir=$(basename $1/toolchains/llvm/prebuilt/*/)
 echo "archdir: ${archDir}"
+
 export ANDROID_NDK_HOME="$1"
 export NDK_TOOLCHAIN_DIR="$1/toolchains/llvm/prebuilt/${archDir}/bin"
-bash $PWD/../../wireguard/build-wireguard-go.sh --android
-echo "Building nym-vpn-lib dep"
 
-PROJECT_ROOT="$(realpath $PWD/../..)"
+PROJECT_ROOT="$(realpath $(pwd)/../..)"
+
 # Rust compiler sysroot that typically points to ~/.rustup/toolchains/<toolchain>
 RUST_COMPILER_SYS_ROOT=$(rustc --print sysroot)
 # Rust flags used in reproducible builds to replace common paths with defaults
-IDEMPOTENT_RUSTFLAGS="-C link-args=-Wl,--build-id=none --remap-path-prefix ${HOME}=~ --remap-path-prefix ${PROJECT_ROOT}=/buildroot --remap-path-prefix ${RUST_COMPILER_SYS_ROOT}=/sysroot"
+
+# --build-id=none does not work on macOS and -no_uuid would not work with clang version
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    IDEMPOTENT_RUSTFLAGS="-C link-args=-Wl, --remap-path-prefix \
+                ${HOME}=~ --remap-path-prefix ${PROJECT_ROOT}=/buildroot --remap-path-prefix \
+                ${RUST_COMPILER_SYS_ROOT}=/sysroot"
+    echo "Setting macOS flags"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    IDEMPOTENT_RUSTFLAGS="-C link-args=-Wl, --build-id=none --remap-path-prefix \
+            ${HOME}=~ --remap-path-prefix ${PROJECT_ROOT}=/buildroot --remap-path-prefix \
+            ${RUST_COMPILER_SYS_ROOT}=/sysroot"
+    echo "Setting Linux flags"
+else
+    echo "Flags not properly set as OS is not supported"
+fi
+
 # Export rust flags enforcing reproducible builds
 export RUSTFLAGS=$IDEMPOTENT_RUSTFLAGS
 # Tell build tools to use unix timestamp of zero as a reference date
@@ -28,19 +43,22 @@ export VERGEN_IDEMPOTENT=1
 
 export VERGEN_GIT_BRANCH="VERGEN_IDEMPOTENT_OUTPUT"
 
-#fix emulators later
-#(cd $PWD/src/tools/nym-vpn-client/crates/nym-vpn-lib; cargo ndk -t armeabi-v7a -t arm64-v8a -t i686-linux-android -t x86_64-linux-android  -o ../../../main/jniLibs build --release)
-pushd $PWD/../../nym-vpn-core/crates/nym-vpn-lib
+cd ../..
+
+bash wireguard/build-wireguard-go.sh --android
+pushd nym-vpn-core/crates/nym-vpn-lib
+
+cargo install cargo-ndk cargo-license --locked
+
 cargo ndk -t arm64-v8a -o ../../../nym-vpn-android/nym-vpn-client/src/main/jniLibs build --release
 popd
 
-pushd $PWD/../../nym-vpn-core
+pushd nym-vpn-core
+
 cargo run --bin uniffi-bindgen generate --library ./target/aarch64-linux-android/release/libnym_vpn_lib.so  --language kotlin --out-dir ../nym-vpn-android/nym-vpn-client/src/main/java/net/nymtech/vpn -n
 popd
 
-cargo license -j --avoid-dev-deps --current-dir ../../nym-vpn-core/crates/nym-vpn-lib --filter-platform aarch64-linux-android --avoid-build-deps > ./src/main/assets/licenses_rust.json
+mv nym-vpn-android/app/build/extraJni/arm64-v8a/libwg.so nym-vpn-android/nym-vpn-client/src/main/jniLibs/arm64-v8a/
 
-mv $PWD/../../nym-vpn-android/app/build/extraJni/arm64-v8a/libwg.so $PWD/src/main/jniLibs/arm64-v8a/
-#mv $PWD/src/tools/nym-vpn-client/nym-vpn-android/app/build/extraJni/armeabi-v7a/libwg.so $PWD/src/main/jniLibs/armeabi-v7a/
-#mv $PWD/src/tools/nym-vpn-client/nym-vpn-android/app/build/extraJni/x86/libwg.so $PWD/src/main/jniLibs/x86/
-#mv $PWD/src/tools/nym-vpn-client/nym-vpn-android/app/build/extraJni/x86_64/libwg.so $PWD/src/main/jniLibs/x86_64/
+cargo license -j --avoid-dev-deps --current-dir nym-vpn-core/crates/nym-vpn-lib --filter-platform aarch64-linux-android --avoid-build-deps > nym-vpn-android/nym-vpn-client/src/main/assets/licenses_rust.json
+
