808f9c3697f7b667c24092f2dd38c91fa6d54ee2
pierre <dommerc.pierre@gmail.com>
2025-01-03 23:28:04 +0100

    update login screen

--- nym-vpn-app/src/screens/login/Login.tsx
+++ nym-vpn-app/src/screens/login/Login.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
 import { useNavigate } from 'react-router';
-import { NymDarkOutlineIcon, NymIcon } from '../../assets';
 import { useInAppNotify, useMainDispatch, useMainState } from '../../contexts';
 import { useI18nError } from '../../hooks';
 import { routes } from '../../router';
@@ -18,7 +17,7 @@ type AddError = {
 };
 
 function Login() {
-  const { uiTheme, daemonStatus, accountLinks } = useMainState();
+  const { daemonStatus, accountLinks } = useMainState();
   const [phrase, setPhrase] = useState('');
   const [error, setError] = useState<AddError | null>(null);
 
@@ -63,65 +62,63 @@ function Login() {
   };
 
   return (
-    <PageAnim className="h-full flex flex-col justify-end items-center gap-10 select-none cursor-default">
-      {uiTheme === 'Dark' ? (
-        <NymDarkOutlineIcon className="w-22 h-22" />
-      ) : (
-        <NymIcon className="w-24 h-24 fill-ghost" />
-      )}
+    <PageAnim className="h-full flex flex-col justify-end items-center gap-6 select-none cursor-default">
+      <div className="grow w-full" />
       <div className="flex flex-col items-center gap-4 px-4">
         <h1 className="text-2xl dark:text-white">{t('welcome')}</h1>
         <h2 className="text-center text-dim-gray dark:text-mercury-mist w-11/12">
           {t('description')}
         </h2>
       </div>
-      <div className="w-full">
-        <TextArea
-          value={phrase}
-          onChange={onChange}
-          spellCheck={false}
-          resize="none"
-          rows={5}
-          label={t('input-label')}
-          placeholder={t('input-placeholder')}
-          className="sentry-ignore"
-        />
-        {error ? (
-          <motion.div
-            initial={{ opacity: 0, x: -10 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={{ duration: 0.15, ease: 'easeInOut' }}
-            className={clsx([
-              'text-teaberry overflow-y-scroll max-h-16 mt-3 break-words',
-              'select-text',
-            ])}
-          >
-            {error.error}
-            {error.details && `: ${error.details}`}
-          </motion.div>
-        ) : (
-          <div className="h-4"></div>
-        )}
-      </div>
-      <div className="w-full flex flex-col justify-center items-center gap-6 mb-2">
-        <Button
-          onClick={handleClick}
-          disabled={daemonStatus === 'NotOk'}
-          className={clsx(
-            daemonStatus === 'NotOk' &&
-              'opacity-50 disabled:opacity-50 hover:opacity-50',
+      <div className="w-full grow flex flex-col justify-end gap-3">
+        <div className="w-full">
+          <TextArea
+            value={phrase}
+            onChange={onChange}
+            spellCheck={false}
+            resize="none"
+            rows={6}
+            label={t('input-label')}
+            placeholder={t('input-placeholder')}
+            className="sentry-ignore"
+          />
+          {error ? (
+            <motion.div
+              initial={{ opacity: 0, x: -10 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.15, ease: 'easeInOut' }}
+              className={clsx([
+                'text-teaberry overflow-y-scroll max-h-16 mt-3 mb-3 break-words',
+                'select-text',
+              ])}
+            >
+              {error.error}
+              {error.details && `: ${error.details}`}
+            </motion.div>
+          ) : (
+            <div className="h-4"></div>
           )}
-        >
-          {t('login-button')}
-        </Button>
-        {signUpUrl && (
-          <div className="flex flex-row justify-center items-center gap-2">
-            <span className="dark:text-mercury-pinkish truncate">
-              {t('create-account.text')}
-            </span>
-            <Link text={t('create-account.link')} url={signUpUrl} />
-          </div>
-        )}
+        </div>
+        <div className="w-full flex flex-col justify-center items-center gap-6 mb-2">
+          <Button
+            onClick={handleClick}
+            disabled={daemonStatus === 'NotOk'}
+            className={clsx(
+              daemonStatus === 'NotOk' &&
+                'opacity-50 disabled:opacity-50 hover:opacity-50',
+            )}
+          >
+            {t('login-button')}
+          </Button>
+          {signUpUrl && (
+            <div className="flex flex-row justify-center items-center gap-2">
+              <span className="dark:text-mercury-pinkish truncate">
+                {t('create-account.text')}
+              </span>
+              <Link text={t('create-account.link')} url={signUpUrl} />
+            </div>
+          )}
+        </div>
       </div>
     </PageAnim>
   );
