6f54f78969ecac90a016e69a7d71d80676a41547
Andrej Mihajlov <andrej@nymtech.net>
2025-01-03 14:33:44 +0100

    Fix offline monitoring for macos

--- nym-vpn-core/crates/nym-offline/src/lib.rs
+++ nym-vpn-core/crates/nym-offline/src/lib.rs
@@ -2,13 +2,15 @@
 // Copyright 2025 Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use std::sync::LazyLock;
+
+use futures::channel::mpsc::UnboundedSender;
+use nym_common::ErrorExt;
+#[cfg(not(target_os = "android"))]
+use nym_routing::RouteManagerHandle;
+
 #[cfg(target_os = "android")]
 use crate::connectivity_listener::ConnectivityListener;
-use futures::channel::mpsc::UnboundedSender;
-use std::sync::LazyLock;
-#[cfg(not(target_os = "android"))]
-use talpid_routing::RouteManagerHandle;
-use talpid_types::{net::Connectivity, ErrorExt};
 
 #[cfg(target_os = "macos")]
 #[path = "macos.rs"]
@@ -28,7 +30,7 @@ mod imp;
 
 /// Disables offline monitor
 static FORCE_DISABLE_OFFLINE_MONITOR: LazyLock<bool> = LazyLock::new(|| {
-    std::env::var("TALPID_DISABLE_OFFLINE_MONITOR")
+    std::env::var("NYM_DISABLE_OFFLINE_MONITOR")
         .map(|v| v != "0")
         .unwrap_or(false)
 });
@@ -64,7 +66,7 @@ pub async fn spawn_monitor(
         )
         .await
         .inspect_err(|error| {
-            log::warn!(
+            tracing::warn!(
                 "{}",
                 error.display_chain_with_msg("Failed to spawn offline monitor")
             );
@@ -73,4 +75,70 @@ pub async fn spawn_monitor(
     };
 
     MonitorHandle(monitor)
+}
+
+/// Details about the hosts's connectivity.
+///
+/// Information about the host's connectivity, such as the preesence of
+/// configured IPv4 and/or IPv6.
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Connectivity {
+    #[cfg(not(target_os = "android"))]
+    Status {
+        /// Whether IPv4 connectivity seems to be available on the host.
+        ipv4: bool,
+        /// Whether IPv6 connectivity seems to be available on the host.
+        ipv6: bool,
+    },
+    #[cfg(any(target_os = "ios", target_os = "android"))]
+    Status {
+        /// Whether _any_ connectivity seems to be available on the host.
+        connected: bool,
+    },
+    /// On/offline status could not be verified, but we have no particular
+    /// reason to believe that the host is offline.
+    PresumeOnline,
+}
+
+impl Connectivity {
+    /// Inverse of [`Connectivity::is_offline`].
+    pub fn is_online(&self) -> bool {
+        !self.is_offline()
+    }
+
+    /// If no IP4 nor IPv6 routes exist, we have no way of reaching the internet
+    /// so we consider ourselves offline.
+    #[cfg(not(target_os = "android"))]
+    pub fn is_offline(&self) -> bool {
+        matches!(
+            self,
+            Connectivity::Status {
+                ipv4: false,
+                ipv6: false
+            }
+        )
+    }
+
+    /// Whether IPv6 connectivity seems to be available on the host.
+    ///
+    /// If IPv6 status is unknown, `false` is returned.
+    #[cfg(not(target_os = "android"))]
+    pub fn has_ipv6(&self) -> bool {
+        matches!(self, Connectivity::Status { ipv6: true, .. })
+    }
+
+    /// Whether IPv6 connectivity seems to be available on the host.
+    ///
+    /// If IPv6 status is unknown, `false` is returned.
+    #[cfg(target_os = "android")]
+    pub fn has_ipv6(&self) -> bool {
+        self.is_online()
+    }
+
+    /// If the host does not have configured IPv6 routes, we have no way of
+    /// reaching the internet so we consider ourselves offline.
+    #[cfg(target_os = "android")]
+    pub fn is_offline(&self) -> bool {
+        matches!(self, Connectivity::Status { connected: false })
+    }
 }
