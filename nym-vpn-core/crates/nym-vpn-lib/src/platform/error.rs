19a3a8879c6706997940b1785ae3f3fabaac69bf
Zane Schepke <zanecschepke@gmail.com>
2025-01-09 01:35:26 -0500

    fmt

--- nym-vpn-core/crates/nym-vpn-lib/src/platform/error.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/platform/error.rs
@@ -176,7 +176,9 @@ impl From<nym_vpn_account_controller::AccountCommandError> for VpnError {
             AccountCommandError::InitDeviceKeys(e) => VpnError::InternalError { details: e },
             AccountCommandError::General(err) => VpnError::InternalError { details: err },
             AccountCommandError::Internal(err) => VpnError::InternalError { details: err },
-            AccountCommandError::RemoveDeviceApiClientFailure(err) => { VpnError::InternalError { details: err } },
+            AccountCommandError::RemoveDeviceApiClientFailure(err) => {
+                VpnError::InternalError { details: err }
+            }
         }
     }
 }
