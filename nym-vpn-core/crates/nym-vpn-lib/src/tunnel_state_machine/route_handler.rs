19c0d9008d770aa83ade4919c21df781b92fa3c9
Andrej Mihajlov <andrej@nymtech.net>
2025-01-09 10:32:18 +0100

    Add method to refresh routes on macos

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/route_handler.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/route_handler.rs
@@ -81,6 +81,13 @@ impl RouteHandler {
         }
     }
 
+    #[cfg(target_os = "macos")]
+    pub async fn refresh_routes(&mut self) {
+        if let Err(e) = self.route_manager.refresh_routes() {
+            tracing::error!("Failed to refresh routes: {}", e);
+        }
+    }
+
     #[cfg(windows)]
     pub async fn add_default_route_listener(
         &mut self,
