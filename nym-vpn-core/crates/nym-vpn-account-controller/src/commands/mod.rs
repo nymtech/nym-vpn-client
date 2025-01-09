2d8821377794f31ab2ef0fd7f618a3941e29af49
Zane Schepke <zanecschepke@gmail.com>
2025-01-09 01:19:24 -0500

    improve error mapping

--- nym-vpn-core/crates/nym-vpn-account-controller/src/commands/mod.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/commands/mod.rs
@@ -79,8 +79,8 @@ pub enum AccountCommandError {
     #[error("failed to remove account: {0}")]
     RemoveAccount(String),
 
-    #[error("failed to remove device from nym vpn api")]
-    RemoveDeviceApiClientFailure,
+    #[error("failed to remove device from nym vpn api: {0}")]
+    RemoveDeviceApiClientFailure(String),
 
     #[error("failed to remove device identity: {0}")]
     RemoveDeviceIdentity(String),
