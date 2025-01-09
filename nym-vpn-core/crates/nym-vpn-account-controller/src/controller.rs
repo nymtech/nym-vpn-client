7ea8b987c1590b4c2dc7002575aca4be3613e9a6
Zane Schepke <zanecschepke@gmail.com>
2025-01-09 00:54:57 -0500

    change error mapping

--- nym-vpn-core/crates/nym-vpn-account-controller/src/controller.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/controller.rs
@@ -366,7 +366,7 @@ where
         self.vpn_api_client
             .update_device(&account, &device, DeviceStatus::DeleteMe)
             .await
-            .map_err(|_err| AccountCommandError::NoDeviceStored)
+            .map_err(|_err| AccountCommandError::RemoveDeviceApiClientFailure)
     }
 
     async fn handle_sync_account_state(&mut self, command: AccountCommand) {
