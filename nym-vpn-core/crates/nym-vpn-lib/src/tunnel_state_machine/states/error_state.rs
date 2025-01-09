52c6dbb28392d11f3d33bf0649e380ba74d250fd
Andrej Mihajlov <andrej@nymtech.net>
2025-01-09 11:36:52 +0100

    Consume connectivity events while disconnecting or in error state

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/error_state.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/error_state.rs
@@ -89,6 +89,10 @@ impl TunnelStateHandler for ErrorState {
                     }
                 }
             }
+            Some(_connectivity) = shared_state.offline_monitor.next() => {
+                // consume connectivity status but do nothing while in error state.
+                NextTunnelState::SameState(self)
+            }
             else => NextTunnelState::Finished
         }
     }
