7be460b0de56b4b4365013618b3c1707f1bf0610
Andrej Mihajlov <andrej@nymtech.net>
2025-01-17 14:24:43 +0100

    Add copyrights

--- nym-vpn-core/crates/nym-firewall/src/ios.rs
+++ nym-vpn-core/crates/nym-firewall/src/ios.rs
@@ -1,3 +1,6 @@
+// Copyright 2025 Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
 use super::{FirewallArguments, FirewallPolicy};
 
 /// Stub error type for Firewall errors on Android.
