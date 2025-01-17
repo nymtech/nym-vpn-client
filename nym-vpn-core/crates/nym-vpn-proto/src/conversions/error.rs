854a38fc026007a1b7b17483ec550be6f662f326
Andrej Mihajlov <andrej@nymtech.net>
2025-01-17 20:40:46 +0100

    Merge protobuf errors into ConversionError

--- nym-vpn-core/crates/nym-vpn-proto/src/conversions/error.rs
+++ nym-vpn-core/crates/nym-vpn-proto/src/conversions/error.rs
@@ -1,10 +1,26 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use std::net::AddrParseError;
+
+use prost::DecodeError;
+
 #[derive(Debug, thiserror::Error)]
 pub enum ConversionError {
-    #[error("generic error: {0}")]
+    #[error("Generic error: {0}")]
     Generic(String),
+
+    #[error("No value set for {0}")]
+    NoValueSet(&'static str),
+
+    #[error("Failed to decode {0}: {1}")]
+    Decode(&'static str, #[source] DecodeError),
+
+    #[error("Failed to convert time {0}: {1}")]
+    ConvertTime(&'static str, #[source] time::Error),
+
+    #[error("Failed to parse address {0}: {1}")]
+    ParseAddr(&'static str, #[source] AddrParseError),
 }
 
 impl ConversionError {
