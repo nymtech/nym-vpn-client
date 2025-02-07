1aa71d89d0bc352b63788a78b46b9c4ac6352fdf
Jon Häggblad <jon.haggblad@gmail.com>
2025-02-07 15:20:23 +0100

    Add directives to logging

--- nym-vpn-core/crates/nym-vpn-lib/src/platform/swift.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/platform/swift.rs
@@ -1,13 +1,32 @@
+// Copyright 2023-2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
 use std::{fs::OpenOptions, io::Write, path::PathBuf, str::FromStr};
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
-use tracing_subscriber::filter::LevelFilter;
+
 use tracing_oslog::OsLogger;
+use tracing_subscriber::{filter::LevelFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
 pub(crate) const DEFAULT_LOG_FILE: &str = "nym-vpn-lib.log";
 
 pub fn init_logs(level: String, path: Option<PathBuf>) {
     // Set log level
-    let log_level = LevelFilter::from_str(&level).unwrap_or(LevelFilter::INFO);
+    let filter = tracing_subscriber::EnvFilter::builder()
+        .with_default_directive(
+            LevelFilter::from_str(&level)
+                .unwrap_or(LevelFilter::INFO)
+                .into(),
+        )
+        .from_env()
+        .unwrap()
+        .add_directive("hyper::proto=warn".parse().unwrap())
+        .add_directive("tokio_reactor::proto=warn".parse().unwrap())
+        .add_directive("reqwest::proto=warn".parse().unwrap())
+        .add_directive("mio::proto=warn".parse().unwrap())
+        .add_directive("want::proto=warn".parse().unwrap())
+        .add_directive("tungstenite::proto=warn".parse().unwrap())
+        .add_directive("tokio_tungstenite::proto=warn".parse().unwrap())
+        .add_directive("handlebars::proto=warn".parse().unwrap())
+        .add_directive("sled::proto=warn".parse().unwrap());
 
     // Determine log file path
     let log_path = path.unwrap_or_else(|| PathBuf::from(DEFAULT_LOG_FILE));
@@ -22,30 +41,31 @@ pub fn init_logs(level: String, path: Option<PathBuf>) {
     }
 
     // Attempt to open the log file for writing
-    let file = OpenOptions::new()
-        .append(true)
-        .create(true)
-        .open(&log_path);
+    let file = OpenOptions::new().append(true).create(true).open(&log_path);
 
     match file {
         Ok(f) => {
             // Initialize the logger with file output
-            fmt()
-                .with_writer(f)
-                .with_max_level(log_level)
-                .init();
+            fmt().with_writer(f).with_env_filter(filter).init();
 
-            tracing::info!("Logger initialized: level = {}, path = {:?}", level, log_path);
+            tracing::info!(
+                "Logger initialized: level = {}, path = {:?}",
+                level,
+                log_path
+            );
         }
         Err(e) => {
-            eprintln!("Failed to open log file {:?}: {}. Falling back to os_log.", log_path, e);
+            eprintln!(
+                "Failed to open log file {:?}: {}. Falling back to os_log.",
+                log_path, e
+            );
 
             // Initialize fallback logging with `os_log` for macOS/iOS
             let oslogger_layer = OsLogger::new("net.nymtech.vpn.agent", "default");
 
             tracing_subscriber::registry()
                 .with(oslogger_layer)
-                .with(log_level)
+                .with(filter)
                 .init();
 
             tracing::info!("Logger initialized with os_log due to file creation failure.");
