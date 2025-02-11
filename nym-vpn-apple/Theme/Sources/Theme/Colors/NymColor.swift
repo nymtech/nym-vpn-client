19ef28eb65aed62deeda8234d49248247f2f7064
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-02-11 22:06:16 +0200

    APPLE: add new colors

--- nym-vpn-apple/Theme/Sources/Theme/Colors/NymColor.swift
+++ nym-vpn-apple/Theme/Sources/Theme/Colors/NymColor.swift
@@ -19,9 +19,6 @@ public struct NymColor {
     // MARK: - Connect -
     public static let connectTitle = Color(.connectTitle)
 
-    // MARK: - Background
-    public static let background = Color(.background)
-
     // MARK: - Sys -
     public static let sysOnPrimary = Color(.sysOnPrimary)
     public static let sysOnSecondary = Color(.sysOnSecondary)
@@ -64,4 +61,21 @@ public struct NymColor {
 
     // MARK: - No Internet -
     public static let noInternet = Color(.noInternetRed)
+
+    // MARK: - New colors 2025 -
+
+    public static let black = Color(.blackNym)
+    public static let white = Color(.whiteNym)
+    public static let primary = Color(.primary)
+    public static let background = Color(.background)
+    public static let backgroundHover = Color(.backgroundHover)
+    public static let elevation = Color(.elevation)
+    public static let elevationHover = Color(.elevationHover)
+    public static let accent = Color(.accent)
+    public static let action = Color(.action)
+    public static let info = Color(.info)
+    public static let warning = Color(.warning)
+    public static let error = Color(.error)
+    public static let gray1 = Color(.gray1)
+    public static let gray2 = Color(.gray2)
 }
