71c9aa2c069a189b24859ae552c2fb85f3de862c
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2025-02-12 11:34:13 +0200

    APPLE: add new core v1.3.0-beta.2 (#2144)

--- nym-vpn-apple/MixnetLibrary/Package.swift
+++ nym-vpn-apple/MixnetLibrary/Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "NymVpnLib",
-            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nym-vpn-core-v1.3.0-beta.1/nym-vpn-core-v1.3.0-beta.1_ios_universal.zip",
-            checksum: "13fe31524a1075da59a6e12cb5a551ef3445880eb3728744c66d04e0d8e63094"
+            url: "https://github.com/nymtech/nym-vpn-client/releases/download/nym-vpn-core-v1.3.0-beta.2/nym-vpn-core-v1.3.0-beta.2_ios_universal.zip",
+            checksum: "f96868a66d4c47eb6a883ee0f41e0fb8ba70f914ff907c728e16d1cd7eb7f5c0"
         ),
 //        .binaryTarget(
 //            name: "NymVpnLib",
