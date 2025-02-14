f2e234fbf6b2d6b0c2ca9aef351925678a4fe8e9
Eugene Balabanov <eugene@nymtech.net>
2025-02-13 17:30:24 -0800

    Added windows acl to on_disk

--- nym-vpn-core/crates/nym-vpn-store/src/mnemonic/on_disk.rs
+++ nym-vpn-core/crates/nym-vpn-store/src/mnemonic/on_disk.rs
@@ -1,14 +1,30 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use super::{MnemonicStorage, MnemonicStorageError, StoredMnemonic};
+use std::os::raw::c_void;
 #[cfg(unix)]
 use std::os::unix::fs::PermissionsExt;
+use std::path::Path;
 use std::{
     fs::{self, File},
+    mem,
     path::PathBuf,
+    ptr,
 };
-
-use super::{MnemonicStorage, MnemonicStorageError, StoredMnemonic};
+#[cfg(windows)]
+use widestring::U16CString;
+#[cfg(windows)]
+use winapi::um::winnt::SECURITY_MAX_SID_SIZE;
+#[cfg(windows)]
+use windows_sys::Win32::Foundation::*;
+use windows_sys::Win32::Security::Authorization::{SetNamedSecurityInfoW, SE_FILE_OBJECT};
+#[cfg(windows)]
+use windows_sys::Win32::Security::*;
+#[cfg(windows)]
+use windows_sys::Win32::Storage::FileSystem::*;
+#[cfg(windows)]
+use windows_sys::Win32::System::SystemServices::SECURITY_DESCRIPTOR_REVISION;
 
 #[derive(Debug, thiserror::Error)]
 pub enum OnDiskMnemonicStorageError {
@@ -101,7 +117,17 @@ impl MnemonicStorage for OnDiskMnemonicStorage {
                 })?;
             }
 
-            // TODO: same for windows
+            #[cfg(windows)]
+            {
+                // Set directory permissions to parent directory on Windows
+                tracing::trace!("Setting ACL for parent directory on Windows");
+                set_secure_permissions_windows(parent).map_err(|err| {
+                    OnDiskMnemonicStorageError::FileCreateError {
+                        path: parent.to_path_buf(),
+                        source: err,
+                    }
+                })?;
+            }
         }
 
         // Another layer of defense, only create the file if it doesn't already exist
@@ -130,7 +156,16 @@ impl MnemonicStorage for OnDiskMnemonicStorage {
             })?;
         }
 
-        // TODO: same for windows
+        #[cfg(windows)]
+        {
+            // Setting ACL for file on Windows
+            set_secure_permissions_windows(&self.path).map_err(|err| {
+                OnDiskMnemonicStorageError::FileCreateError {
+                    path: self.path.clone(),
+                    source: err,
+                }
+            })?;
+        }
 
         Ok(())
     }
@@ -158,6 +193,93 @@ impl MnemonicStorage for OnDiskMnemonicStorage {
         }
         std::fs::remove_file(&self.path).map_err(OnDiskMnemonicStorageError::RemoveError)
     }
+}
+
+#[cfg(windows)]
+fn set_secure_permissions_windows<P: AsRef<Path>>(path: P) -> Result<(), std::io::Error> {
+    let wide_path = U16CString::from_os_str(path.as_ref().as_os_str()).map_err(|e| {
+        std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            format!("Invalid UTF-16 conversion: {e}"),
+        )
+    })?;
+
+    let mut sid_size: u32 = SECURITY_MAX_SID_SIZE as u32;
+    let mut system_sid = vec![0u8; sid_size as usize];
+
+    unsafe {
+        if CreateWellKnownSid(
+            WinLocalSystemSid,
+            ptr::null_mut(),
+            system_sid.as_mut_ptr().cast(),
+            &mut sid_size,
+        ) == 0
+        {
+            return Err(std::io::Error::last_os_error());
+        }
+    }
+
+    let sid_ptr = system_sid.as_mut_ptr().cast();
+    let acl_size = mem::size_of::<ACL>() as u32
+        + mem::size_of::<ACCESS_ALLOWED_ACE>() as u32
+        + unsafe { GetLengthSid(sid_ptr) };
+    let mut acl_buffer = vec![0u8; acl_size as usize];
+    let acl = acl_buffer.as_mut_ptr() as *mut ACL;
+
+    unsafe {
+        if InitializeAcl(acl, acl_size, ACL_REVISION) == 0 {
+            return Err(std::io::Error::last_os_error());
+        }
+
+        if AddAccessAllowedAceEx(
+            acl,
+            ACL_REVISION,
+            0,
+            FILE_GENERIC_READ | FILE_GENERIC_WRITE,
+            sid_ptr.cast(),
+        ) == 0
+        {
+            return Err(std::io::Error::last_os_error());
+        }
+    }
+
+    let mut security_desc: SECURITY_DESCRIPTOR = unsafe { mem::zeroed() };
+
+    unsafe {
+        if InitializeSecurityDescriptor(
+            &mut security_desc as *mut _ as *mut _,
+            SECURITY_DESCRIPTOR_REVISION,
+        ) == 0
+        {
+            return Err(std::io::Error::last_os_error());
+        }
+
+        if SetSecurityDescriptorDacl(
+            &mut security_desc as *mut _ as *mut c_void,
+            TRUE,
+            acl,
+            FALSE,
+        ) == 0
+        {
+            return Err(std::io::Error::last_os_error());
+        }
+    }
+
+    unsafe {
+        if SetNamedSecurityInfoW(
+            wide_path.as_ptr() as *mut _,
+            SE_FILE_OBJECT,
+            DACL_SECURITY_INFORMATION,
+            ptr::null_mut(),
+            ptr::null_mut(),
+            acl,
+            ptr::null_mut(),
+        ) != ERROR_SUCCESS
+        {
+            return Err(std::io::Error::last_os_error());
+        }
+    }
+    Ok(())
 }
 
 #[cfg(test)]
