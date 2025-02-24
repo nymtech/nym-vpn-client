92bcc28e0c48454e62b35eb8205078b5b08b634d
Andrej Mihajlov <andrej@nymtech.net>
2025-02-24 15:24:54 +0100

    clippy: fix

--- nym-vpn-core/crates/nym-platform-metadata/src/windows.rs
+++ nym-vpn-core/crates/nym-platform-metadata/src/windows.rs
@@ -48,7 +48,7 @@ impl WindowsVersion {
     pub fn new() -> Result<WindowsVersion, windows::core::Error> {
         let ntdll = unsafe { GetModuleHandleW(w!("ntdll"))? };
         let function_address = unsafe { GetProcAddress(ntdll, s!("RtlGetVersion")) }
-            .ok_or_else(|| windows::core::Error::from_win32())?;
+            .ok_or_else(windows::core::Error::from_win32)?;
 
         let rtl_get_version: extern "stdcall" fn(*mut RTL_OSVERSIONINFOEXW) =
             unsafe { *(&function_address as *const _ as *const _) };
