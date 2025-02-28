b6c8cf2b1f24f60da5f9161e5c3e87169c3f5db9
Zane Schepke <zanecschepke@gmail.com>
2025-02-28 07:27:06 -0500

    fix: forget account if registration fails

--- nym-vpn-core/crates/nym-vpn-account-controller/src/commander.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/commander.rs
@@ -45,7 +45,14 @@ impl AccountControllerCommander {
         self.store_account(mnemonic).await?;
         self.ensure_update_account().await?;
         self.ensure_update_device().await?;
-        self.ensure_register_device().await?;
+        //if register fails, forget the account
+        match self.ensure_register_device().await {
+            Ok(()) => Ok(()),
+            Err(register_error) => {
+                let _ = self.forget_account().await;
+                Err(AccountCommandError::RegisterDevice(register_error))
+            }
+        }?;
         Ok(())
     }
 
