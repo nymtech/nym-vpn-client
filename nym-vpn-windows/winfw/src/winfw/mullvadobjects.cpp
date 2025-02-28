2984329c4f3c1fcb10ad2a931ce59e74f378d871
Andrej Mihajlov <andrej@nymtech.net>
2025-02-28 19:10:32 +0100

    Update object descriptions

--- nym-vpn-windows/winfw/src/winfw/mullvadobjects.cpp
+++ nym-vpn-windows/winfw/src/winfw/mullvadobjects.cpp
@@ -8,8 +8,8 @@ std::unique_ptr<wfp::ProviderBuilder> MullvadObjects::Provider()
 	auto builder = std::make_unique<wfp::ProviderBuilder>();
 
 	(*builder)
-		.name(L"Mullvad VPN")
-		.description(L"Mullvad VPN firewall integration")
+		.name(L"Nym VPN")
+		.description(L"Nym VPN firewall integration")
 		.key(MullvadGuids::Provider());
 
 	return builder;
@@ -21,7 +21,7 @@ std::unique_ptr<wfp::SublayerBuilder> MullvadObjects::SublayerBaseline()
 	auto builder = std::make_unique<wfp::SublayerBuilder>();
 
 	(*builder)
-		.name(L"Mullvad VPN baseline")
+		.name(L"Nym VPN baseline")
 		.description(L"Filters that enforce a good baseline")
 		.key(MullvadGuids::SublayerBaseline())
 		.provider(MullvadGuids::Provider())
@@ -36,7 +36,7 @@ std::unique_ptr<wfp::SublayerBuilder> MullvadObjects::SublayerDns()
 	auto builder = std::make_unique<wfp::SublayerBuilder>();
 
 	(*builder)
-		.name(L"Mullvad VPN DNS")
+		.name(L"Nym VPN DNS")
 		.description(L"Filters that restrict DNS traffic")
 		.key(MullvadGuids::SublayerDns())
 		.provider(MullvadGuids::Provider())
@@ -51,8 +51,8 @@ std::unique_ptr<wfp::ProviderBuilder> MullvadObjects::ProviderPersistent()
 	auto builder = std::make_unique<wfp::ProviderBuilder>();
 
 	(*builder)
-		.name(L"Mullvad VPN persistent")
-		.description(L"Mullvad VPN firewall integration")
+		.name(L"Nym VPN persistent")
+		.description(L"Nym VPN firewall integration")
 		.persistent()
 		.key(MullvadGuids::ProviderPersistent());
 
@@ -65,7 +65,7 @@ std::unique_ptr<wfp::SublayerBuilder> MullvadObjects::SublayerPersistent()
 	auto builder = std::make_unique<wfp::SublayerBuilder>();
 
 	(*builder)
-		.name(L"Mullvad VPN persistent")
+		.name(L"Nym VPN persistent")
 		.description(L"Filters that restrict traffic before WinFw is initialized")
 		.key(MullvadGuids::SublayerPersistent())
 		.provider(MullvadGuids::ProviderPersistent())
