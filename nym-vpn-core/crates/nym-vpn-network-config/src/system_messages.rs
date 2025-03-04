d8f1c61fc7c1dbf3e24c27bd6b8c704c5e63f219
Zane Schepke <zanecschepke@gmail.com>
2025-03-04 07:56:51 -0500

    fix another

--- nym-vpn-core/crates/nym-vpn-network-config/src/system_messages.rs
+++ nym-vpn-core/crates/nym-vpn-network-config/src/system_messages.rs
@@ -7,11 +7,12 @@ use anyhow::Context;
 use serde::{Deserialize, Serialize};
 use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 
-use crate::network_compatibility::NetworkCompatibility;
-use crate::system_configuration::{ScoreThresholds, SystemConfiguration};
 use nym_vpn_api_client::response::{
     NetworkCompatibilityResponse, SystemConfigurationResponse, SystemMessageResponse,
 };
+
+use crate::network_compatibility::NetworkCompatibility;
+use crate::system_configuration::{ScoreThresholds, SystemConfiguration};
 
 #[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Eq, Default)]
 pub struct SystemMessages {
