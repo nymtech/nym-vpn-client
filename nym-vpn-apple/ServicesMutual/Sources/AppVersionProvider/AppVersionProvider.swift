7499d7e08dc3ab1ed4977c8c2aa4d162b15c3607
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-03-12 09:13:19 +0200

    APPLE: bump core to v1.5.3

--- nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
+++ nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum AppVersionProvider {
-    public static let libVersion = "1.5.1"
+    public static let libVersion = "1.5.3"
 
     public static var app: String {
         "nym-vpn-app"
