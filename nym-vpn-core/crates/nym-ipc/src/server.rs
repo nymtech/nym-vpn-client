603eb4620a69e7cdd52de86e4a9d469ec8af1030
Andrej Mihajlov <andrej@nymtech.net>
2025-03-12 18:50:44 +0100

    Refactor a bit

--- nym-vpn-core/crates/nym-ipc/src/server.rs
+++ nym-vpn-core/crates/nym-ipc/src/server.rs
@@ -1,14 +1,15 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{io, path::PathBuf};
+use std::{io::Result, path::PathBuf};
+
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio_stream::Stream;
 use tonic::transport::server::Connected;
 
 pub fn create_incoming(
     socket_path: PathBuf,
-) -> io::Result<impl Stream<Item = io::Result<impl AsyncRead + AsyncWrite + Connected + 'static>>> {
+) -> Result<impl Stream<Item = Result<impl AsyncRead + AsyncWrite + Connected + 'static>>> {
     #[cfg(unix)]
     {
         crate::uds::incoming(socket_path)
