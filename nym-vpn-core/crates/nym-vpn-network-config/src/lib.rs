919157c97e29764b8f2a5179818a6c10defae82a
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2025-03-26 16:21:10 +0200

    Bump max file age to 24h

--- nym-vpn-core/crates/nym-vpn-network-config/src/lib.rs
+++ nym-vpn-core/crates/nym-vpn-network-config/src/lib.rs
@@ -38,8 +38,8 @@ use std::{fmt::Debug, path::Path, str::FromStr, time::Duration};
 const NETWORKS_SUBDIR: &str = "networks";
 
 // Refresh the discovery and network details files periodically
-//const MAX_FILE_AGE: Duration = Duration::from_secs(60 * 60 * 24);
-const MAX_FILE_AGE: Duration = Duration::from_secs(60);
+const MAX_FILE_AGE: Duration = Duration::from_secs(60 * 60 * 24);
+// const MAX_FILE_AGE: Duration = Duration::from_secs(60);
 
 #[derive(Clone, Debug)]
 pub struct Network {
