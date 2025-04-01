01965c022e70b67cbe8248750b19e269e88d89f3
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2025-04-01 14:23:56 +0300

    Do proper check for ticketbook before inserting

--- nym-vpn-core/crates/nym-vpn-account-controller/src/storage/credentials/mod.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/storage/credentials/mod.rs
@@ -125,6 +125,16 @@ impl VpnCredentialStorage {
             .map_err(Error::from)
     }
 
+    pub(crate) async fn contains_issued_ticketbook(
+        &self,
+        ticketbook: &IssuedTicketBook,
+    ) -> Result<bool, Error> {
+        self.credential_storage
+            .contains_issued_ticketbook(ticketbook)
+            .await
+            .map_err(Error::from)
+    }
+
     pub(crate) async fn insert_master_verification_key(
         &self,
         key: &EpochVerificationKey,
