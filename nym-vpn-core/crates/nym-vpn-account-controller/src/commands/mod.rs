20198de36f6fc749523061124b781fe1cad90204
Jon Häggblad <jon.haggblad@gmail.com>
2025-04-02 09:26:47 +0200

    Reduce visibility

--- nym-vpn-core/crates/nym-vpn-account-controller/src/commands/mod.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/commands/mod.rs
@@ -1,13 +1,12 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-pub(crate) mod command_handler;
-pub(crate) mod dispatch;
-pub(crate) mod running_commands;
 pub(crate) mod tasks;
 
-pub(crate) use command_handler::AccountCommandHandler;
-pub(crate) use dispatch::AccountCommandResult;
-pub(crate) use running_commands::{Command, RunningCommands};
+mod command_handler;
+mod dispatch;
+mod running_commands;
 
-pub use dispatch::{AccountCommand, ReturnSender};
+pub(crate) use command_handler::AccountCommandHandler;
+pub(crate) use dispatch::{AccountCommand, AccountCommandResult, ReturnSender};
+pub(crate) use running_commands::{Command, RunningCommands};
