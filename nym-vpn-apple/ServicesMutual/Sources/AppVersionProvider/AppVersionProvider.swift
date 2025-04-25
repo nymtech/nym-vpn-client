d22f6350c7cdbd150cd676b063857975323e8d6c
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2025-04-25 17:23:24 +0300

    APPLE: use core v1.8.0 (#2713)

--- nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
+++ nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum AppVersionProvider {
-    public static let libVersion = "1.8.0-beta"
+    public static let libVersion = "1.8.0"
 
     public static var app: String {
         "nym-vpn-app"
