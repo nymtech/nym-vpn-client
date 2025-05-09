5b4d824250952aae476b97605c4af0f3489dda54
Jack Wampler <jmwample@users.noreply.github.com>
2025-05-09 08:33:06 -0600

    Remove mix hops for registration in two hop mode (#2740)

--- nym-vpn-core/crates/nym-vpn-lib/src/mixnet/connect.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/mixnet/connect.rs
@@ -90,11 +90,16 @@ pub(crate) async fn setup_mixnet_client(
     #[cfg(unix)] connection_fd_callback: Arc<dyn Fn(RawFd) + Send + Sync>,
 ) -> Result<SharedMixnetClient, MixnetError> {
     let mut debug_config = nym_client_core::config::DebugConfig::default();
-    // for mobile platforms, in two hop mode, we do less frequent cover traffic,
-    // to preserve battery
-    if two_hop_mode && (cfg!(unix) || cfg!(target_os = "ios")) {
-        debug_config.cover_traffic.loop_cover_traffic_average_delay =
-            MOBILE_LOOP_COVER_STREAM_AVERAGE_DELAY;
+    if two_hop_mode {
+        // for mobile platforms, in two hop mode, we do less frequent cover traffic, to preserve
+        // battery
+        if cfg!(any(target_os = "android", target_os = "ios")) {
+            debug_config.cover_traffic.loop_cover_traffic_average_delay =
+                MOBILE_LOOP_COVER_STREAM_AVERAGE_DELAY;
+        }
+
+        // If operating in two hop mode, we disable mix hops for the mixnet connection.
+        debug_config.traffic.disable_mix_hops = true;
     }
     apply_mixnet_client_config(&mixnet_client_config, &mut debug_config);
 
