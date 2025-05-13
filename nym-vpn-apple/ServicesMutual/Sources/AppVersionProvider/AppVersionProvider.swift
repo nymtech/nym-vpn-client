4ab46dba0fe032ed5ab65eb2871eea77188e873d
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-05-13 15:02:59 +0300

    APPLE: add out of sync error

--- nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
+++ nym-vpn-apple/ServicesMutual/Sources/AppVersionProvider/AppVersionProvider.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public enum AppVersionProvider {
-    public static let libVersion = "1.8.0"
+    public static let libVersion = "1.9.0-beta"
 
     public static var app: String {
         "nym-vpn-app"
