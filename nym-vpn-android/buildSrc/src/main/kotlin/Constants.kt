9777ae4612652691c124e669c880659dd897d207
Andrej Mihajlov <andrej@nymtech.net>
2025-05-15 13:15:27 +0200

    Bump android version to v1.5.0 (#2786)

--- nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
+++ nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Constants {
-	const val VERSION_NAME = "v1.4.0"
-    const val VERSION_CODE = 14000
+	const val VERSION_NAME = "v1.5.0"
+    const val VERSION_CODE = 15000
     const val TARGET_SDK = 35
     const val COMPILE_SDK = 35
     const val MIN_SDK = 24
