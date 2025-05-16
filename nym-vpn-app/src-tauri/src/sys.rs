c5a5f9ac266ad33924f68c38c71f9b272d7dfbd8
pierre <dommerc.pierre@gmail.com>
2025-05-16 19:36:15 +0200

    refactor: sys and os info

---
+++ nym-vpn-app/src-tauri/src/sys.rs
@@ -0,0 +1,122 @@
+use std::{env, process::Command};
+
+use sysinfo::System;
+#[cfg(any(target_os = "linux", target_os = "openbsd"))]
+use tracing::{error, info, warn};
+
+#[cfg(any(target_os = "linux", target_os = "openbsd"))]
+#[derive(Debug, strum::AsRefStr)]
+pub enum GpuType {
+    #[strum(serialize = "NVIDIA")]
+    Nvidia,
+    #[strum(serialize = "AMD")]
+    Amd,
+    Intel,
+    Unknown,
+}
+
+#[cfg(any(target_os = "linux", target_os = "openbsd"))]
+#[derive(Debug, strum::AsRefStr)]
+pub enum DisplayServer {
+    X11,
+    Wayland,
+    Unknown,
+}
+
+#[cfg(any(target_os = "linux", target_os = "openbsd"))]
+fn get_display_server() -> DisplayServer {
+    match std::env::var("XDG_SESSION_TYPE")
+        .inspect_err(|e| warn!("XDG_SESSION_TYPE not set or not valid: {e}"))
+        .map(|s| s.to_lowercase())
+    {
+        Ok(s) if s == "x11" => DisplayServer::X11,
+        Ok(s) if s == "wayland" => DisplayServer::Wayland,
+        Ok(s) => {
+            warn!("unknown display server: {}", s);
+            DisplayServer::Unknown
+        }
+        _ => DisplayServer::Unknown,
+    }
+}
+
+pub struct OsInfo {
+    pub name: String,
+    pub kernel: Option<String>,
+    pub arch: String,
+    #[cfg(any(target_os = "linux", target_os = "openbsd"))]
+    pub display_server: DisplayServer,
+    #[cfg(any(target_os = "linux", target_os = "openbsd"))]
+    pub gpu: GpuType,
+}
+
+impl OsInfo {
+    pub fn new() -> Self {
+        Self {
+            name: System::long_os_version().unwrap_or_else(|| env::consts::OS.into()),
+            kernel: System::kernel_version(),
+            arch: env::consts::ARCH.to_string(),
+            #[cfg(any(target_os = "linux", target_os = "openbsd"))]
+            display_server: get_display_server(),
+            #[cfg(any(target_os = "linux", target_os = "openbsd"))]
+            gpu: gpu_info(),
+        }
+    }
+
+    #[cfg(any(target_os = "linux", target_os = "openbsd"))]
+    pub fn linux_check(&self) {
+        // under X11 with nvidia gpu, there is an upstream issue with webkit dmabuf renderer
+        // see https://github.com/tauri-apps/tauri/issues/9304
+        if matches!(self.display_server, DisplayServer::X11) && matches!(self.gpu, GpuType::Nvidia)
+        {
+            info!("NVIDIA gpu and X11 detected, disabling webkit dmabuf renderer");
+            unsafe {
+                std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+            }
+        }
+    }
+}
+
+#[cfg(any(target_os = "linux", target_os = "openbsd"))]
+pub fn gpu_info() -> GpuType {
+    use tracing::debug;
+
+    let Ok(output) = Command::new("lspci").arg("-nn").output().inspect_err(|e| {
+        error!("failed to run lspci: {}", e);
+    }) else {
+        return GpuType::Unknown;
+    };
+    if !output.status.success() {
+        error!("lspci failed: {}", String::from_utf8_lossy(&output.stderr));
+        return GpuType::Unknown;
+    }
+    let output = String::from_utf8_lossy(&output.stdout);
+    let Some(info) = output
+        .lines()
+        .find(|line| line.to_lowercase().contains("vga compatible controller"))
+    else {
+        warn!("no VGA device found in lspci output");
+        return GpuType::Unknown;
+    };
+    debug!("GPU info: {}", info);
+    if info.to_lowercase().contains("nvidia") {
+        return GpuType::Nvidia;
+    } else if info.to_lowercase().contains("amd") || info.contains("radeon") {
+        return GpuType::Amd;
+    } else if info.to_lowercase().contains("intel") {
+        return GpuType::Intel;
+    }
+    warn!("unknown GPU type: {}", info);
+    GpuType::Unknown
+}
+
+impl std::fmt::Display for OsInfo {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{} {} {}",
+            self.name,
+            self.kernel.as_deref().unwrap_or("unknown"),
+            self.arch
+        )
+    }
+}
