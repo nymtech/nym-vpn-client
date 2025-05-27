2ebe2f32b6a981d409f7bec641772ca8b9b303ad
pierre <dommerc.pierre@gmail.com>
2025-05-27 16:27:23 +0200

    fix wf

--- nym-vpn-app/src-tauri/src/grpc/tunnel_error.rs
+++ nym-vpn-app/src-tauri/src/grpc/tunnel_error.rs
@@ -43,7 +43,6 @@ impl From<ProtoTunnelError> for TunnelError {
             ErrorStateReason::SubscriptionExpired => TunnelError::SubscriptionExpired(error.detail),
             ErrorStateReason::Api => TunnelError::Api(error.detail),
             ErrorStateReason::DeviceTimeOutOfSync => TunnelError::DeviceTimeOutOfSync(error.detail),
-            // TODO not sure if this is the right way to handle this one
             ErrorStateReason::CreateMixnetStorage => TunnelError::Internal(error.detail),
         }
     }
