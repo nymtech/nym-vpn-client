668bf13a7b75a6495a9a52ac4e7017db8e92f99e
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2025-06-07 08:26:11 +0300

    APPLE: Use voavanga core (#2861)

--- nym-vpn-apple/MixnetLibrary/Package.swift
+++ nym-vpn-apple/MixnetLibrary/Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "NymVpnLib",
-            url: "https://builds.ci.nymte.ch/nym-vpn-client/nym-vpn-core/release/2025.8-tomato-umbu/202505201519/nym-vpn-core-v1.9.0_ios_universal.zip",
-            checksum: "c159bb38783b2bed1a3465ce61053efe3d3242ee79c5b53909a2aff3ca26fea7"
+            url: "https://builds.ci.nymte.ch/nym-vpn-client/nym-vpn-core/release/2025.9-voavanga/202505290832/nym-vpn-core-v1.10.0-beta.202505290831_ios_universal.zip",
+            checksum: "422741abbb1df8d2f4222df2963d98ecfa6ca80e02526ae5749122c44f7ff577"
         ),
 //        .binaryTarget(
 //            name: "NymVpnLib",
