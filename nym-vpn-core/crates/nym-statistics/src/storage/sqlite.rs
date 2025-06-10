6b89b94c298e2aaeb4eea098f96a8eaf92e7c235
Simon Wicky <simon@nymtech.net>
2025-06-10 14:49:53 +0200

    tweaks and fixes

--- nym-vpn-core/crates/nym-statistics/src/storage/sqlite.rs
+++ nym-vpn-core/crates/nym-statistics/src/storage/sqlite.rs
@@ -2,20 +2,21 @@
 // SPDX-License-Identifier: GPL-3.0-only
 
 use super::error::StatsStorageError;
+use sqlx_pool_guard::SqlitePoolGuard;
 
 #[derive(Clone, Debug)]
 pub struct SqliteStatsStorageManager {
-    connection_pool: sqlx::SqlitePool,
+    connection_pool: SqlitePoolGuard,
 }
 
 impl SqliteStatsStorageManager {
-    pub fn new(connection_pool: sqlx::SqlitePool) -> Self {
+    pub fn new(connection_pool: SqlitePoolGuard) -> Self {
         Self { connection_pool }
     }
 
     pub async fn load_seed(&self) -> Result<Option<String>, StatsStorageError> {
         Ok(sqlx::query!("SELECT seed FROM seed")
-            .fetch_optional(&self.connection_pool)
+            .fetch_optional(&*self.connection_pool)
             .await?
             .map(|r| r.seed))
     }
@@ -23,14 +24,14 @@ impl SqliteStatsStorageManager {
     pub async fn set_seed(&self, seed: String) -> Result<(), StatsStorageError> {
         self.remove_seed().await?;
         sqlx::query!("INSERT INTO seed VALUES (?)", seed)
-            .execute(&self.connection_pool)
+            .execute(&*self.connection_pool)
             .await?;
         Ok(())
     }
 
     pub async fn remove_seed(&self) -> Result<(), StatsStorageError> {
         sqlx::query!("DELETE FROM seed")
-            .execute(&self.connection_pool)
+            .execute(&*self.connection_pool)
             .await?;
         Ok(())
     }
