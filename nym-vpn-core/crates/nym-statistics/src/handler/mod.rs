7a58abaa9b77d5c209e645d3ad75ba0526ea63a5
Simon Wicky <simon@nymtech.net>
2025-06-10 14:22:56 +0200

    add stats crates

---
+++ nym-vpn-core/crates/nym-statistics/src/handler/mod.rs
@@ -0,0 +1,76 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
+use nym_statistics_common::{
+    generate_vpn_client_stats_id, report::vpn_client::VpnClientStatsReport,
+};
+use static_information::StaticInformationHandler;
+use usage::UsageHandler;
+
+use crate::{
+    config::StatisticsControllerConfig,
+    error::Error,
+    events::{ControllerEvent, StatisticsEvent},
+    storage::StatsStorage,
+};
+
+mod static_information;
+mod usage;
+
+pub(crate) struct StatisticsHandler {
+    storage: StatsStorage,
+    config: StatisticsControllerConfig,
+
+    static_information_handler: StaticInformationHandler,
+    usage_handler: UsageHandler,
+    //SW TODO investigate using trait like Andrew did in Nym-nodes
+}
+
+impl StatisticsHandler {
+    pub fn new(storage: StatsStorage, config: StatisticsControllerConfig) -> Self {
+        StatisticsHandler {
+            storage: storage.clone(),
+            config,
+            static_information_handler: StaticInformationHandler::new(),
+            usage_handler: UsageHandler::new(storage),
+        }
+    }
+
+    pub async fn handle_event(&mut self, event: StatisticsEvent) {
+        match event {
+            StatisticsEvent::Usage(e) => self.usage_handler.handle_event(e),
+            StatisticsEvent::Controller(e) => self.handle_controller_event(e).await,
+        }
+    }
+
+    pub async fn get_report(&mut self) -> Result<VpnClientStatsReport, Error> {
+        // Use seed override or storage one
+        let seed = self
+            .config
+            .stats_id_seed
+            .clone()
+            .unwrap_or(self.storage.maybe_init_and_load_seed().await?);
+        let identifier = generate_vpn_client_stats_id(seed);
+        Ok(
+            VpnClientStatsReport::new(identifier, self.static_information_handler.get_report())
+                .with_usage_report(self.usage_handler.get_report()),
+        )
+    }
+
+    async fn handle_controller_event(&mut self, event: ControllerEvent) {
+        match event {
+            ControllerEvent::RemoveSeed => self
+                .storage
+                .remove_seed()
+                .await
+                .inspect_err(|e| tracing::error!("Failed to remove stats seed : {e}"))
+                .ok(),
+            ControllerEvent::ResetSeed => self
+                .storage
+                .reset_seed()
+                .await
+                .inspect_err(|e| tracing::error!("Failed to reset stats seed : {e}"))
+                .ok(),
+        };
+    }
+}
---
+++ nym-vpn-core/crates/nym-statistics/src/handler/usage.rs
@@ -0,0 +1,58 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
+use std::time::{Duration, Instant};
+
+use nym_statistics_common::report::vpn_client::UsageReport;
+
+use crate::{events::UsageEvent, storage::StatsStorage};
+
+pub(crate) struct UsageHandler {
+    _storage: StatsStorage,
+    connection_time: Option<Duration>,
+    connecting: Option<Instant>,
+    two_hop: bool,
+}
+
+impl UsageHandler {
+    pub(crate) fn new(storage: StatsStorage) -> Self {
+        UsageHandler {
+            _storage: storage,
+            connection_time: None,
+            connecting: None,
+            two_hop: false,
+        }
+    }
+
+    pub(crate) fn get_report(&mut self) -> UsageReport {
+        UsageReport {
+            connection_time_ms: self
+                .connection_time
+                .take()
+                .map(|d| d.as_millis().try_into().unwrap_or(i32::MAX)), //SW We should never hit the or clause, but it's better than to panic (it's ~24 days)
+            two_hop: self.two_hop,
+        }
+    }
+
+    pub(crate) fn handle_event(&mut self, event: UsageEvent) {
+        match event {
+            UsageEvent::Connecting {
+                instant,
+                enable_two_hop,
+            } => {
+                self.connecting = Some(instant);
+                self.connection_time = None;
+                self.two_hop = enable_two_hop;
+            }
+            UsageEvent::Connected(instant) => {
+                if let Some(connecting_time) = self.connecting {
+                    self.connection_time = Some(instant.duration_since(connecting_time));
+                }
+            }
+            _ => {
+                self.connecting = None;
+                self.connection_time = None;
+            }
+        }
+    }
+}
