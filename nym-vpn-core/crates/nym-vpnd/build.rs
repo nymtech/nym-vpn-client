297fd21ce30031a6897bf9b050df8875e1741956
Andrej Mihajlov <andrej@nymtech.net>
2025-06-13 17:44:08 +0200

    Migrate to vergen-git

--- nym-vpn-core/crates/nym-vpnd/build.rs
+++ nym-vpn-core/crates/nym-vpnd/build.rs
@@ -1,15 +1,13 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use vergen::EmitBuilder;
+use vergen_gitcl::{BuildBuilder, CargoBuilder, Emitter, GitclBuilder, RustcBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    EmitBuilder::builder()
-        .all_build()
-        .all_git()
-        .all_rustc()
-        .all_cargo()
-        .emit()
-        .expect("failed to extract build metadata");
-    Ok(())
+    Ok(Emitter::default()
+        .add_instructions(&BuildBuilder::all_build()?)?
+        .add_instructions(&CargoBuilder::all_cargo()?)?
+        .add_instructions(&GitclBuilder::all_git()?)?
+        .add_instructions(&RustcBuilder::all_rustc()?)?
+        .emit()?)
 }
