7e0a43aed0e6221473666f230e82f7f243ef1973
Andrej Mihajlov <andrej@nymtech.net>
2025-06-16 17:33:52 +0200

    Migrate to vergen-git (#2939)

--- nym-vpn-core/crates/nym-gateway-probe/build.rs
+++ nym-vpn-core/crates/nym-gateway-probe/build.rs
@@ -1,19 +1,17 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use vergen::EmitBuilder;
+use vergen_gitcl::{BuildBuilder, CargoBuilder, Emitter, GitclBuilder, RustcBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     rust2go::Builder::new()
         .with_go_src("./netstack_ping")
         .build();
 
-    EmitBuilder::builder()
-        .all_build()
-        .all_git()
-        .all_rustc()
-        .all_cargo()
-        .emit()
-        .expect("failed to extract build metadata");
-    Ok(())
+    Ok(Emitter::default()
+        .add_instructions(&BuildBuilder::all_build()?)?
+        .add_instructions(&CargoBuilder::all_cargo()?)?
+        .add_instructions(&GitclBuilder::all_git()?)?
+        .add_instructions(&RustcBuilder::all_rustc()?)?
+        .emit()?)
 }
