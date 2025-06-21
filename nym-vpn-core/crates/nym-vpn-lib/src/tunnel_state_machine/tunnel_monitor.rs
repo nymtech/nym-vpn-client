888fd5e11521182a4fed044061b8c6ff57e1270c
Andrej Mihajlov <andrej@nymtech.net>
2025-06-21 07:08:57 +0200

    Remove unnecessary bypass

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel_monitor.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel_monitor.rs
@@ -440,17 +440,6 @@ impl TunnelMonitor {
         .await
         .map_err(Box::new)?;
 
-        // Route mixnet client outside the tunnel.
-        #[cfg(target_os = "android")]
-        match connected_mixnet.websocket_fd().await {
-            Some(fd) => {
-                self.tun_provider.bypass(fd);
-            }
-            None => {
-                tracing::error!("Failed to obtain websocket for bypass");
-            }
-        }
-
         let status_listener_handle = connected_mixnet
             .start_event_listener(
                 self.mixnet_event_sender.clone(),
