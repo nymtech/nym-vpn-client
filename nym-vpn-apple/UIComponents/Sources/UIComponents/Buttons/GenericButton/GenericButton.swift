e31d1dbf5e40b30677657d435527b302b1ab1fba
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-06-23 09:56:23 +0300

    APPLE: add create account no mnemonic view

--- nym-vpn-apple/UIComponents/Sources/UIComponents/Buttons/GenericButton/GenericButton.swift
+++ nym-vpn-apple/UIComponents/Sources/UIComponents/Buttons/GenericButton/GenericButton.swift
@@ -7,30 +7,56 @@ public struct GenericButton: View {
     private let mainColor: Color
     private let height: CGFloat
     private let isWidthExpanded: Bool
+    private let systemImageNamge: String?
+    private let isSystemImageFlipped: Bool
 
     @State private var isHovered = false
+    @Binding private var isLoading: Bool
 
     public init(
         title: String,
         borderOnly: Bool = false,
         mainColor: Color = NymColor.accent,
         height: CGFloat = 56,
-        isWidthExpanded: Bool = true
+        isLoading: Binding<Bool> = .constant(false),
+        isWidthExpanded: Bool = true,
+        systemImageNamge: String? = nil,
+        isSystemImageFlipped: Bool = false
     ) {
         self.title = title
         self.borderOnly = borderOnly
         self.mainColor = mainColor
         self.height = height
         self.isWidthExpanded = isWidthExpanded
+        self.systemImageNamge = systemImageNamge
+        self.isSystemImageFlipped = isSystemImageFlipped
+        _isLoading = isLoading
     }
 
     public var body: some View {
         HStack {
-            Text(title)
-                .foregroundStyle(borderOnly ? mainColor : NymColor.black)
-                .textStyle(.Headline.Small.regular)
-                .padding(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle(tint: NymColor.black))
+            } else {
+                if let systemImageNamge {
+                    Image(systemName: systemImageNamge)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 24, height: 24)
+                        .padding(.horizontal, 8)
+                        .scaleEffect(x: isSystemImageFlipped ? -1 : 1, y: 1)
+                        .foregroundStyle(borderOnly ? mainColor : NymColor.black)
+                }
+
+                Text(title)
+                    .foregroundStyle(borderOnly ? mainColor : NymColor.black)
+                    .textStyle(.Headline.Small.regular)
+            }
         }
+        .padding(EdgeInsets(top: 10, leading: 16, bottom: 10, trailing: 16))
+        .accessibilityLabel(title)
+        .accessibilityAddTraits([.isButton])
         .frame(maxWidth: isWidthExpanded ? .infinity : nil)
         .frame(height: height)
         .background {
