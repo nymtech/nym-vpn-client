727ed2d09076e26c5d5921216f5722f6c7c4d487
Simon Wicky <simon@nymtech.net>
2025-06-26 14:16:15 +0200

    add stats crates

---
+++ nym-vpn-core/crates/nym-statistics/src/error.rs
@@ -0,0 +1,11 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    StatsApiClient(#[from] nym_statistics_api_client::StatisticsApiClientError),
+
+    #[error("storage error : {0}")]
+    StatsStorage(#[from] crate::storage::error::StatsStorageError),
+}
---
+++ nym-vpn-core/crates/nym-statistics/src/lib.rs
@@ -0,0 +1,10 @@
+// Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
+mod api_client;
+pub mod config;
+pub mod controller;
+mod error;
+pub mod events;
+pub mod handler;
+mod storage;
