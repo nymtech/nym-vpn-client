274f5bd58562d50d730cd70cd340e96abebbe91d
Andrej Mihajlov <andrej@nymtech.net>
2025-07-07 13:36:08 +0200

    Scope proto under proto::

--- nym-vpn-core/crates/nym-vpn-proto/src/lib.rs
+++ nym-vpn-core/crates/nym-vpn-proto/src/lib.rs
@@ -1,13 +1,15 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-tonic::include_proto!("nym.vpn");
+pub mod proto {
+    // Re-export needed prost types
+    pub use prost_types::Timestamp;
+
+    tonic::include_proto!("nym.vpn");
+}
 
 #[cfg(feature = "conversions")]
 pub mod conversions;
-
-// Re-export needed prost types
-pub use prost_types::Timestamp;
 
 #[cfg(feature = "rpc_client")]
 pub mod rpc_client;
