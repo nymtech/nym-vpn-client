a31ac26285f92ab6b1e550eb498186547959a3fc
Andrej Mihajlov <andrej@nymtech.net>
2025-07-08 08:02:31 +0200

    wip

--- nym-vpn-core/crates/nym-vpnd-types/src/account_state.rs
+++ nym-vpn-core/crates/nym-vpnd-types/src/account_state.rs
@@ -3,14 +3,26 @@
 
 pub use nym_vpn_account_controller::shared_state::{
     AccountRegistered, AccountState, AccountSummary, DeviceState, DeviceSummary, FairUsage,
-    RegisterDeviceResult, RequestZkNymResult, SubscriptionState,
+    RegisterDeviceResult, SubscriptionState,
 };
+pub use nym_vpn_lib_types::{RequestZkNymErrorReason, RequestZkNymSuccess};
 
 /// Public representation of `nym_vpn_account_controller::shared_state::MnemonicState`
 #[derive(Debug)]
 pub enum MnemonicState {
     Stored,
     NotStored,
+}
+
+/// Public representation of `nym_vpn_account_controller::shared_state::RequestZkNymResult`
+#[derive(Debug)]
+pub enum RequestZkNymResult {
+    InProgress,
+    Done {
+        successes: Vec<RequestZkNymSuccess>,
+        failures: Vec<RequestZkNymErrorReason>,
+    },
+    Error(RequestZkNymErrorReason),
 }
 
 #[derive(Debug)]
@@ -38,7 +50,31 @@ impl From<nym_vpn_account_controller::shared_state::AccountStateSummary> for Acc
             account_summary: value.account_summary,
             device: value.device,
             register_device_result: value.register_device_result,
-            request_zk_nym_result: value.request_zk_nym_result,
+            request_zk_nym_result: value.request_zk_nym_result.map(RequestZkNymResult::from),
+        }
+    }
+}
+
+impl From<nym_vpn_account_controller::shared_state::RequestZkNymResult> for RequestZkNymResult {
+    fn from(value: nym_vpn_account_controller::shared_state::RequestZkNymResult) -> Self {
+        match value {
+            nym_vpn_account_controller::shared_state::RequestZkNymResult::InProgress => {
+                RequestZkNymResult::InProgress
+            }
+            nym_vpn_account_controller::shared_state::RequestZkNymResult::Done {
+                successes,
+                failures,
+            } => RequestZkNymResult::Done {
+                successes,
+                failures: failures
+                    .into_iter()
+                    .map(RequestZkNymErrorReason::from)
+                    .collect(),
+            },
+            nym_vpn_account_controller::shared_state::RequestZkNymResult::Error(error) => {
+                let error = RequestZkNymErrorReason::from(error);
+                RequestZkNymResult::Error(error)
+            }
         }
     }
 }
