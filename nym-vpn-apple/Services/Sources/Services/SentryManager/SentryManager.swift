4d848683ae306998601d8932272e0acf79e8a246
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-07-11 10:14:20 +0300

    Update Sentry

--- nym-vpn-apple/Services/Sources/Services/SentryManager/SentryManager.swift
+++ nym-vpn-apple/Services/Sources/Services/SentryManager/SentryManager.swift
@@ -19,26 +19,6 @@ public final class SentryManager {
     }
 }
 
-public extension SentryManager {
-    func submitFeedback(recommendation: String, message: String) {
-        let initialIsErrorReporingOnValue = appSettings.isErrorReportingOn
-        appSettings.isErrorReportingOn = true
-
-        Task {
-            try await Task.sleep(for: .seconds(3))
-            let eventId = SentrySDK.capture(message: "Feedback submission")
-            let userFeedback = UserFeedback(eventId: eventId)
-            userFeedback.comments = "1. \(recommendation) \n 2.\(message)"
-            SentrySDK.capture(userFeedback: userFeedback)
-
-            try await Task.sleep(for: .seconds(4))
-            Task { @MainActor in
-                appSettings.isErrorReportingOn = initialIsErrorReporingOnValue
-            }
-        }
-    }
-}
-
 extension SentryManager {
     func setupObservers() {
         appSettings.$isErrorReportingOnPublisher.sink { [weak self] _ in
@@ -53,12 +33,8 @@ extension SentryManager {
             if appSettings.isErrorReportingOn {
                 SentrySDK.start { options in
                     options.dsn = "https://f860c307259ffe7827fa4ecdfaa8834f@o967446.ingest.us.sentry.io/4507135758237696"
-                    options.debug = false // Enabled debug when first installing is always helpful
-                    options.enableTracing = true
-
-                    // Uncomment the following lines to add more data to your events
-                    // options.attachScreenshot = true // This adds a screenshot to the error events
-                    // options.attachViewHierarchy = true // This adds the view hierarchy to the error events
+                    options.debug = false
+                    options.tracesSampleRate = 1
                 }
             } else {
                 SentrySDK.close()
