4c3a6f99ca5a79bb61feebc27d1c1745fa3374be
Simon Wicky <simon@nymtech.net>
2025-07-14 11:21:51 +0200

    tweaks and fixes

--- nym-vpn-core/crates/nym-statistics-api-client/src/routes.rs
+++ nym-vpn-core/crates/nym-statistics-api-client/src/routes.rs
@@ -1,7 +1,4 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-// pub(crate) const PUBLIC: &str = "public";
-pub(crate) const V1: &str = "v1";
-pub(crate) const STATS: &str = "stats";
-pub(crate) const REPORT: &str = "report";
+pub(crate) const REPORT_ROUTE: &str = "v1/stats/report";
