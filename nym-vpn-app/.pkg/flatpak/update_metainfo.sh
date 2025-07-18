84049bd88edbb3eb8d088da5f146e8d63727e837
pierre <dommerc.pierre@gmail.com>
2025-07-18 13:50:03 +0200

    fix

--- nym-vpn-app/.pkg/flatpak/update_metainfo.sh
+++ nym-vpn-app/.pkg/flatpak/update_metainfo.sh
@@ -3,15 +3,15 @@
 # this script is called by GH workflow `publish-nym-vpn-app.yml`
 # it updates flatpak metainfo by inserting a new release block
 # it expects the following environment variables to be set:
-# - MATAINFO_XML, the path to the metainfo.xml file
+# - METAINFO_XML, the path to the metainfo.xml file
 # - VERSION, the app version eg. 1.2.3
 # - RELEASE_TAG, the release tag eg. nym-vpn-app-v1.2.3
 # - RELEASE_DATE, the date of the release in YYYY-MM-DD format (optional, defaults to today)
 
 set -e
 
-if [ -z "$MATAINFO_XML" ]; then
-    echo "error MATAINFO_XML env variable not set"
+if [ -z "$METAINFO_XML" ]; then
+    echo "error METAINFO_XML env variable not set"
     exit 1
 fi
 if [ -z "$VERSION" ]; then
@@ -37,8 +37,8 @@ xmlstarlet edit --inplace \
     --subnode '$new_node' --type elem -n 'url' -v "https://github.com/nymtech/nym-vpn-client/releases/tag/$RELEASE_TAG" \
     --var new_node '$prev' \
     --insert '$new_node' --type attr -n 'type' --value 'details' \
-    "$MATAINFO_XML"
-formatted_xml=$(xmlstarlet format --indent-spaces 4 "$MATAINFO_XML")
-echo "$formatted_xml" > "$MATAINFO_XML"
+    "$METAINFO_XML"
+formatted_xml=$(xmlstarlet format --indent-spaces 4 "$METAINFO_XML")
+echo "$formatted_xml" > "$METAINFO_XML"
 
 echo "✓ done"
