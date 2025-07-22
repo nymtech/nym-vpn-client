a7859c09b3a07df7a84201b9b601e172347af81d
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2025-07-22 15:02:41 +0300

    APPLE: cherrypicks from release (#3090)
    * APPLE: use latest xurian yuzu core (#3088)
    
    * APPLE: fix empty products (#3087)
    
    * APPLE: fix macOS account link (#3092)

--- nym-vpn-apple/MixnetLibrary/Package.swift
+++ nym-vpn-apple/MixnetLibrary/Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "NymVpnLib",
-            url: "https://builds.ci.nymte.ch/nym-vpn-client/nym-vpn-core/release/2025.11-xurian-yuzu/202507171134/nym-vpn-core-v1.12.0-beta.202507171133_ios_universal.zip",
-            checksum: "11a257cb0083e856351889b0633af76c45d516123b09fcf2d2e0f46ff678cc42"
+            url: "https://builds.ci.nymte.ch/nym-vpn-client/nym-vpn-core/release/2025.11-xurian-yuzu/202507181107/nym-vpn-core-v1.12.0_ios_universal.zip",
+            checksum: "91dbac98dbdaa994c597dabf1cc0b04313f6f9af1ea6df93f009f73b4331a1cb"
         ),
 //        .binaryTarget(
 //            name: "NymVpnLib",
