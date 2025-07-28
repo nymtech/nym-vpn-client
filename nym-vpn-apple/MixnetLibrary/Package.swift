eff5e2019759b395c1255f223856fa7811a86efc
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-07-28 20:45:20 +0300

    APPLE: update core, bump version

--- nym-vpn-apple/MixnetLibrary/Package.swift
+++ nym-vpn-apple/MixnetLibrary/Package.swift
@@ -23,8 +23,8 @@ let package = Package(
         ),
         .binaryTarget(
             name: "NymVpnLib",
-            url: "https://builds.ci.nymte.ch/nym-vpn-client/nym-vpn-core/release/2025.12-zucchini/202507251049/nym-vpn-core-v1.13.0-beta.202507251048_ios_universal.zip",
-            checksum: "3b90234c8168f050e59ecf9b37bf73a0a9d284d47eee1db3c13963b77bcb912f"
+            url: "https://builds.ci.nymte.ch/nym-vpn-client/nym-vpn-core/release/2025.12-zucchini/202507281517/nym-vpn-core-v1.13.0-beta.202507281516_ios_universal.zip",
+            checksum: "5c5867020b69f137b9944dceccd3fbec82cb748d7efef5d032bc24d36d3bcbea"
         ),
 //        .binaryTarget(
 //            name: "NymVpnLib",
