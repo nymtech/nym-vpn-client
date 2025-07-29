4cf7f1df662d98c17d5abe183127cc8cd85e0857
Andrej Mihajlov <andrej@nymtech.net>
2025-07-29 13:02:58 +0200

    Make sure that cancellation during initialization does not hang forever

--- nym-vpn-core/crates/nym-vpnd/src/service/vpn_service.rs
+++ nym-vpn-core/crates/nym-vpnd/src/service/vpn_service.rs
@@ -395,7 +395,8 @@ impl NymVpnService {
         // todo: we must not block service initialization
         shutdown_token
             .run_until_cancelled(gateway_directory_client.refresh_all())
-            .await;
+            .await
+            .ok_or(Error::CancelledInitialization)?;
 
         let validator_client = nym_validator_client::NymApiClient::new_with_user_agent(
             api_url,
