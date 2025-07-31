3cd546883823373870e947144eeb51ecc5dadcc0
Simon Wicky <simon@nymtech.net>
2025-07-31 14:10:26 +0200

    small tweak

--- nym-vpn-core/crates/nym-vpnc/src/main.rs
+++ nym-vpn-core/crates/nym-vpnc/src/main.rs
@@ -289,7 +289,7 @@ async fn get_account_links(
 
 async fn get_account_state(mut rpc_client: RpcClient) -> Result<()> {
     let response = rpc_client.get_account_state().await?;
-    println!("{response:#?}");
+    println!("{response}");
     Ok(())
 }
 
