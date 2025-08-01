876b66cfc275e415d3a7d598bc44adec886f072f
Floriane TUERNAL SABOTINOV <ftuertinov@proton.me>
2025-08-01 10:18:16 +0200

    tunnel event serialization to separate backenderror object

--- nym-vpn-app/src/layers/EventNotification.tsx
+++ nym-vpn-app/src/layers/EventNotification.tsx
@@ -10,6 +10,7 @@ import {
   isTunnelError,
   isTunnelOffline,
 } from '../types';
+import { data } from 'react-router';
 
 export default function EventNotification({
   children,
@@ -22,6 +23,15 @@ export default function EventNotification({
 
   const registerStateListener = useCallback(() => {
     return listen<TunnelStateEventPayload>(TunnelStateEvent, async (event) => {
+      const serializablePayload = {
+        ...event.payload,
+        error: event.payload.error ? {
+          message: event.payload.error.message || 'Unknown error',
+          key: event.payload.error.key,
+          data: event.payload.error.data || null,
+        } : null
+      };
+      
       if (event.payload.state === 'disconnected') {
         await notify(t('vpn-tunnel-state.disconnected'), {
           locationPath: routes.root,
