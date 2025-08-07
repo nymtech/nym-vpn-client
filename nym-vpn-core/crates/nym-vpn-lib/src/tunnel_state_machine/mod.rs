d251569090ed827bf00d87ffeb90ae8c1f700d80
Andrej Mihajlov <andrej@nymtech.net>
2025-08-07 14:06:32 +0200

    Fix dns on macOS 15.x

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/mod.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/mod.rs
@@ -19,6 +19,8 @@ mod tunnel_monitor;
 #[cfg(windows)]
 mod wintun;
 
+#[cfg(target_os = "macos")]
+use std::net::{SocketAddr, SocketAddrV4};
 #[cfg(any(target_os = "ios", target_os = "android"))]
 use std::sync::Arc;
 use std::{
@@ -430,6 +432,8 @@ pub struct TunnelStateMachine {
     dns_handler_task: JoinHandle<()>,
     #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
     dns_handler_shutdown_token: CancellationToken,
+    #[cfg(target_os = "macos")]
+    filtering_resolver_handle: JoinHandle<()>,
     shutdown_token: CancellationToken,
 }
 
@@ -451,13 +455,17 @@ impl TunnelStateMachine {
         #[cfg(target_os = "android")] tun_provider: Arc<dyn AndroidTunProvider>,
         shutdown_token: CancellationToken,
     ) -> Result<JoinHandle<()>> {
-        #[cfg(target_os = "macos")]
-        let filtering_resolver = resolver::start_resolver()
-            .await
-            .map_err(Error::StartLocalDnsResolver)?;
-
         #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
         let dns_handler_shutdown_token = CancellationToken::new();
+
+        #[cfg(target_os = "macos")]
+        let (filtering_resolver, filtering_resolver_handle) = resolver::LocalResolver::spawn(
+            SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 0)),
+            dns_handler_shutdown_token.child_token(),
+        )
+        .await
+        .map_err(Error::StartLocalDnsResolver)?;
+
         #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
         let (dns_handler, dns_handler_task) = DnsHandlerHandle::spawn(
             #[cfg(target_os = "linux")]
@@ -521,6 +529,8 @@ impl TunnelStateMachine {
             dns_handler_task,
             #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
             dns_handler_shutdown_token,
+            #[cfg(target_os = "macos")]
+            filtering_resolver_handle,
             shutdown_token,
         };
 
@@ -575,6 +585,13 @@ impl TunnelStateMachine {
             }
 
             self.shared_state.route_handler.stop().await;
+        }
+
+        #[cfg(target_os = "macos")]
+        {
+            if let Err(e) = self.filtering_resolver_handle.await {
+                tracing::error!("Failed to join on filtering resolver task: {}", e)
+            }
         }
     }
 }
--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/offline_state.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/offline_state.rs
@@ -10,6 +10,8 @@ use nym_common::trace_err_chain;
 use nym_dns::DnsConfig;
 #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
 use nym_firewall::FirewallPolicy;
+#[cfg(target_os = "macos")]
+use nym_firewall::LOCAL_DNS_RESOLVER;
 
 #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
 use crate::tunnel_state_machine::{Error, Result};
@@ -61,7 +63,7 @@ impl OfflineState {
             allow_lan: true,
             allowed_endpoints: Vec::new(),
             #[cfg(target_os = "macos")]
-            dns_redirect_port: shared_state.filtering_resolver.listening_port(),
+            dns_redirect_port: shared_state.filtering_resolver.listen_addr().port(),
         };
 
         shared_state
@@ -88,8 +90,8 @@ impl OfflineState {
     async fn set_local_dns_resolver(shared_state: &mut SharedState) -> Result<()> {
         // Set system DNS to our local DNS resolver
         let system_dns = DnsConfig::default().resolve(
-            &[std::net::Ipv4Addr::LOCALHOST.into()],
-            shared_state.filtering_resolver.listening_port(),
+            &[shared_state.filtering_resolver.listen_addr().ip()],
+            shared_state.filtering_resolver.listen_addr().port(),
         );
         shared_state
             .dns_handler
@@ -138,12 +140,17 @@ impl TunnelStateHandler for OfflineState {
                 }
             }
             Some(connectivity) = shared_state.connectivity_handle.next() => {
-                // See: https://github.com/rust-lang/rust-clippy/issues/14799
-                #[allow(clippy::collapsible_else_if)]
                 if connectivity.is_offline() {
                     NextTunnelState::SameState(self)
                 } else {
-                    #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
+                    #[cfg(target_os = "macos")]
+                    if !*LOCAL_DNS_RESOLVER {
+                        // This is probably unnecessary, since DNS is already configured on the
+                        // primary interface.
+                        Self::reset_dns(shared_state).await;
+                    }
+
+                    #[cfg(any(target_os = "linux", target_os = "windows"))]
                     Self::reset_dns(shared_state).await;
 
                     if self.reconnect {
