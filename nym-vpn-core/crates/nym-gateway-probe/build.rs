bb78494b3feb0938233be1252456fced8e41e231
Andrej Mihajlov <andrej@nymtech.net>
2025-08-22 20:40:40 +0200

    Disable windows builds

--- nym-vpn-core/crates/nym-gateway-probe/build.rs
+++ nym-vpn-core/crates/nym-gateway-probe/build.rs
@@ -21,35 +21,15 @@ fn build_go() -> Result<(), Box<dyn std::error::Error>> {
 
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").expect("target arch is not set");
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("target os is not set");
-    let out_dir = if target_os == "windows" {
-        let manifest_path =
-            std::env::var_os("CARGO_MANIFEST_DIR").expect("manifest dir is not set");
-        let build_dir = PathBuf::from(manifest_path)
-            .join("../../../build/gateway-probe")
-            .join(&target_arch);
-        // canonicalize() will fail if the build directory does not exist
-        if !build_dir.exists() {
-            std::fs::create_dir_all(&build_dir).expect("failed to create build dir");
-        }
-        build_dir
-            .canonicalize()
-            .expect("failed to canonicalize build dir path")
-    } else {
-        PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR is not set"))
-    };
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR is not set"));
 
-    let lib_prefix = if target_os == "windows" { "lib" } else { "" };
-    let lib_ext = if target_os == "windows" { "dll" } else { "a" };
-
-    let go_build_mode = if target_os == "windows" {
-        "c-shared"
-    } else {
-        "c-archive"
-    };
+    if target_os == "windows" {
+        return Ok(());
+    }
 
     let go_target = match target_os.as_str() {
         "macos" => "darwin".to_owned(),
-        "linux" | "windows" => target_os.to_owned(),
+        "linux" => target_os.to_owned(),
         _ => panic!("unsupported target: {target_os}"),
     };
 
@@ -63,33 +43,14 @@ fn build_go() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("cargo::rerun-if-changed={}", src_dir.display());
 
-    let binary_out_path = out_dir
-        .join(format!("lib{LIB_NAME}"))
-        .with_extension(lib_ext);
+    let binary_out_path = out_dir.join(format!("lib{LIB_NAME}.a"));
 
     let mut command = Command::new("go");
 
-    match target_os.as_str() {
-        "macos" => {
-            let deployment_target =
-                std::env::var_os("MACOSX_DEPLOYMENT_TARGET").unwrap_or("10.13".into());
-            command.env("MACOSX_DEPLOYMENT_TARGET", deployment_target);
-        }
-        "windows" => {
-            let msys2_location = std::env::var_os("MSYS2_LOCATION")
-                .map(PathBuf::from)
-                .unwrap_or(PathBuf::from(r#"C:\msys64"#));
-
-            let cc = match target_arch.as_str() {
-                "aarch64" => Some(msys2_location.join(r#"clangarm64\bin\aarch64-w64-mingw32-cc"#)),
-                "x86_64" => Some(msys2_location.join(r#"mingw64\bin\x86_64-w64-mingw32-cc"#)),
-                _ => None,
-            };
-            if let Some(cc) = cc {
-                command.env("CC", cc);
-            }
-        }
-        _ => {}
+    if target_os == "macos" {
+        let deployment_target =
+            std::env::var_os("MACOSX_DEPLOYMENT_TARGET").unwrap_or("10.13".into());
+        command.env("MACOSX_DEPLOYMENT_TARGET", deployment_target);
     }
 
     let mut child = command
@@ -106,7 +67,7 @@ fn build_go() -> Result<(), Box<dyn std::error::Error>> {
         .arg("-o")
         .arg(binary_out_path)
         .arg("-buildmode")
-        .arg(go_build_mode)
+        .arg("c-archive")
         .arg("lib.go")
         .spawn()?;
     let status = child.wait()?;
@@ -117,13 +78,7 @@ fn build_go() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     println!("cargo::rustc-link-search={}", out_dir.display());
-
-    let link_type = match target_os.as_str() {
-        "linux" | "macos" => "=static",
-        "windows" => "=dylib",
-        _ => panic!("Unsupported platform: {target_os}"),
-    };
-    println!("cargo:rustc-link-lib{link_type}={lib_prefix}{LIB_NAME}");
+    println!("cargo:rustc-link-lib=static={LIB_NAME}");
 
     Ok(())
 }
