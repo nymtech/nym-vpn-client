fe490c7059bb6a5a6bc0c6e9a2fa2073b5d9392c
Andrej Mihajlov <andrej@nymtech.net>
2025-08-22 20:06:43 +0200

    fix windows build

--- nym-vpn-core/crates/nym-gateway-probe/build.rs
+++ nym-vpn-core/crates/nym-gateway-probe/build.rs
@@ -1,7 +1,7 @@
 // Copyright 2024 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::process::Command;
+use std::{path::PathBuf, process::Command};
 
 use vergen_gitcl::{BuildBuilder, CargoBuilder, Emitter, GitclBuilder, RustcBuilder};
 
@@ -21,19 +21,26 @@ fn build_go() -> Result<(), Box<dyn std::error::Error>> {
 
     let target_arch = std::env::var("CARGO_CFG_TARGET_ARCH").expect("target arch is not set");
     let target_os = std::env::var("CARGO_CFG_TARGET_OS").expect("target os is not set");
-    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR is not set");
-
-    let lib_prefix = if target_os.as_str() == "windows" {
-        "lib"
+    let out_dir = if target_os == "windows" {
+        let manifest_path =
+            std::env::var_os("CARGO_MANIFEST_DIR").expect("manifest dir is not set");
+        let build_dir = PathBuf::from(manifest_path)
+            .join("../../../build/gateway-probe")
+            .join(&target_arch);
+        // canonicalize() will fail if the build directory does not exist
+        if !build_dir.exists() {
+            std::fs::create_dir_all(&build_dir).expect("failed to create build dir");
+        }
+        let build_dir = build_dir
+            .canonicalize()
+            .expect("failed to canonicalize build dir path");
+        build_dir
     } else {
-        ""
+        PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR is not set"))
     };
 
-    let lib_ext = if target_os.as_str() == "windows" {
-        "dll"
-    } else {
-        "a"
-    };
+    let lib_prefix = if target_os == "windows" { "lib" } else { "" };
+    let lib_ext = if target_os == "windows" { "dll" } else { "a" };
 
     let go_build_mode = if target_os == "windows" {
         "c-shared"
@@ -53,19 +60,37 @@ fn build_go() -> Result<(), Box<dyn std::error::Error>> {
         _ => panic!("unsupported architecture: {target_arch}"),
     };
 
-    let src_dir = std::path::PathBuf::from("netstack_ping").canonicalize()?;
+    let src_dir = PathBuf::from("netstack_ping").canonicalize()?;
 
     println!("cargo::rerun-if-changed={}", src_dir.display());
 
-    let binary_out_path = std::path::PathBuf::from(out_dir.clone())
+    let binary_out_path = out_dir
         .join(format!("lib{LIB_NAME}"))
         .with_extension(lib_ext);
 
     let mut command = Command::new("go");
-    if target_os == "macos" {
-        let deployment_target =
-            std::env::var_os("MACOSX_DEPLOYMENT_TARGET").unwrap_or("10.13".into());
-        command.env("MACOSX_DEPLOYMENT_TARGET", deployment_target);
+
+    match target_os.as_str() {
+        "macos" => {
+            let deployment_target =
+                std::env::var_os("MACOSX_DEPLOYMENT_TARGET").unwrap_or("10.13".into());
+            command.env("MACOSX_DEPLOYMENT_TARGET", deployment_target);
+        }
+        "windows" => {
+            let msys2_location = std::env::var_os("MSYS2_LOCATION")
+                .map(PathBuf::from)
+                .unwrap_or(PathBuf::from(r#"C:\msys64"#));
+
+            let cc = match target_arch.as_str() {
+                "aarch64" => Some(msys2_location.join(r#"clangarm64\bin\aarch64-w64-mingw32-cc"#)),
+                "x86_64" => Some(msys2_location.join(r#"mingw64\bin\x86_64-w64-mingw32-cc"#)),
+                _ => None,
+            };
+            if let Some(cc) = cc {
+                command.env("CC", cc);
+            }
+        }
+        _ => {}
     }
 
     let mut child = command
@@ -87,12 +112,12 @@ fn build_go() -> Result<(), Box<dyn std::error::Error>> {
         .spawn()?;
     let status = child.wait()?;
     if !status.success() {
-        return Err(Box::new(std::io::Error::other(
-            "Failed to build {LIB_NAME}",
-        )));
+        return Err(Box::new(std::io::Error::other(format!(
+            "Failed to build {LIB_NAME}"
+        ))));
     }
 
-    println!("cargo::rustc-link-search={out_dir}");
+    println!("cargo::rustc-link-search={}", out_dir.display());
 
     let link_type = match target_os.as_str() {
         "linux" | "macos" => "=static",
