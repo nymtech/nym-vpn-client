94da7c01ea94e77b8b2abfc8a50fccaa078a8053
mrkiwi <sirakandrew@gmail.com>
2025-08-26 11:46:32 +0200

    ktlint

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/main/modal/NetworkStatsModal.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/main/modal/NetworkStatsModal.kt
@@ -67,7 +67,7 @@ fun NetworkStatsModal(showNetworkStatsDialog: Boolean, onDismiss: () -> Unit, on
 					Text(
 						stringResource(R.string.modal_network_stats_not_now_button),
 						fontFamily = FontFamily(Font(R.font.lab_grotesque_regular)),
-						color = MaterialTheme.colorScheme.onSurface
+						color = MaterialTheme.colorScheme.onSurface,
 					)
 				},
 				modifier = Modifier
