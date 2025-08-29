985558646a46e790fe8d5cb3e3050a43fedae2ee
Andy Duplain <trojanfoe@gmail.com>
2025-08-29 14:11:55 +0100

    Fixes during PR review.
    Unit test `test_service_config_migrate_gateway` is currently failing...

--- nym-vpn-core/crates/nym-vpnd/src/service/mod.rs
+++ nym-vpn-core/crates/nym-vpnd/src/service/mod.rs
@@ -7,8 +7,8 @@ mod vpn_service;
 
 pub use config::{
     ConfigSetupError, DEFAULT_GLOBAL_CONFIG_FILE_JSON, DEFAULT_GLOBAL_CONFIG_FILE_TOML,
-    DEFAULT_LOG_FILE, DEFAULT_OLD_LOG_FILE, config_dir, default_true, log_dir,
-    read_json_config_file, read_toml_config_file, write_json_config_file,
+    DEFAULT_LOG_FILE, DEFAULT_OLD_LOG_FILE, config_dir, log_dir, read_json_config_file,
+    read_toml_config_file, write_json_config_file,
 };
 pub use error::SetNetworkError;
 pub use vpn_service::{NymVpnService, NymVpnServiceParameters, VpnServiceCommand};
