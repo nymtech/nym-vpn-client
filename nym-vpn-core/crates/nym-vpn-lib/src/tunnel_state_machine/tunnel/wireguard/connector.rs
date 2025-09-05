0d1a91006e8f27f3a9d4804d6f2971028e0fdd14
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2025-09-05 12:48:44 +0300

    Use a feature flag to determine the gateway's metadata update version

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel/wireguard/connector.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel/wireguard/connector.rs
@@ -151,6 +151,7 @@ impl Connector {
                 &mut wg_exit_gateway_client,
                 entry_signal_rx,
                 exit_signal_rx,
+                network.get_feature_flag_gateway_metadata_update_version(),
                 shutdown,
                 cancel_token,
             )
@@ -168,6 +169,7 @@ impl Connector {
                 &mut wg_exit_gateway_client,
                 entry_signal_rx,
                 exit_signal_rx,
+                network.get_feature_flag_gateway_metadata_update_version(),
                 shutdown,
                 cancel_token,
             )
