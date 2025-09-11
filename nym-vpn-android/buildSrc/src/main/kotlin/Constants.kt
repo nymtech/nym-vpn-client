ff84c03a10f268fb774d96bca91b26eb39949dec
mrkiwi <sirakandrew@gmail.com>
2025-09-11 13:17:22 +0200

    update version

--- nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
+++ nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Constants {
-	const val VERSION_NAME = "v2.0.0"
-    const val VERSION_CODE = 20000
+	const val VERSION_NAME = "v2.0.1"
+    const val VERSION_CODE = 20100
     const val TARGET_SDK = 35
     const val COMPILE_SDK = 35
     const val MIN_SDK = 24
