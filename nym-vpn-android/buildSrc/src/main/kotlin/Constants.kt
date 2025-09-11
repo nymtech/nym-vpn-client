62c609a4cdd703e84a92546c065e4584573eecd0
mrkiwi <sirakandrew@gmail.com>
2025-09-11 13:57:25 +0200

    Fix billing dependency for fdroid

--- nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
+++ nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Constants {
-	const val VERSION_NAME = "v2.0.0"
-    const val VERSION_CODE = 20000
+	const val VERSION_NAME = "v2.0.2"
+    const val VERSION_CODE = 20200
     const val TARGET_SDK = 35
     const val COMPILE_SDK = 35
     const val MIN_SDK = 24
