66862d8d5869c5855b86352b34b56f5a21b7f3f8
Andrej Mihajlov <andrej@nymtech.net>
2025-09-12 14:02:15 +0200

    Add --release flag based on profile

--- nym-vpn-core/iOS.mk
+++ nym-vpn-core/iOS.mk
@@ -36,7 +36,7 @@ build: $(LIBWG_BUILD_DIR)/libwg.a
 
 swift-package: $(LIBWG_BUILD_DIR)/libwg.a
 	cd $(LIB_CRATE_DIR); \
-	$(ALL_IDEMPOTENT_FLAGS) cargo swift package --accept-all --platforms ios --name NymVPNLib --xcframework-name NymVPNLibUniffi --release ; \
+	$(ALL_IDEMPOTENT_FLAGS) cargo swift package --accept-all --platforms ios --name NymVPNLib --xcframework-name NymVPNLibUniffi $(RELEASE_FLAG) ; \
 	sed -i '' -e '/.macOS(.v10_15)/d' "NymVPNLib/Package.swift"
 
 libwg: $(LIBWG_BUILD_DIR)/libwg.a
