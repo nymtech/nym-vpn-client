53ffb075613b71716bc7dac86aad4a0b47d0c13b
pierre <dommerc.pierre@gmail.com>
2025-09-16 16:35:49 +0200

    cleanup

--- nym-vpn-app/src/screens/settings/anti-censorship/AntiCensorship.tsx
+++ nym-vpn-app/src/screens/settings/anti-censorship/AntiCensorship.tsx
@@ -3,9 +3,10 @@ import { CardSwitch, Link, PageAnim, SettingsMenuCardBig } from '../../../ui';
 import { useMainDispatch, useMainState } from '../../../contexts';
 import { StateDispatch } from '../../../types';
 import { kvSet } from '../../../kvStore';
+import { DomainFrontingUrl, QuicUrl } from '../../../constants';
 
 function AntiCensorship() {
-  const { quic, domainFronting } = useMainState();
+  const { quic, domainFronting, backendFlags } = useMainState();
 
   const dispatch = useMainDispatch() as StateDispatch;
 
@@ -29,61 +30,72 @@ function AntiCensorship() {
     } catch {}
   };
 
+  if (!backendFlags?.quic && !backendFlags?.domainFronting) {
+    return (
+      <PageAnim className="xs:max-w-lg h-full flex flex-col mt-2 gap-6 select-none">
+        This feature is not available
+      </PageAnim>
+    );
+  }
+
   return (
     <PageAnim className="xs:max-w-lg h-full flex flex-col mt-2 gap-6 select-none">
       <div className="text-iron dark:text-bombay">
         {t('anti-censorship.intro')}
       </div>
-      <SettingsMenuCardBig
-        header={
-          <CardSwitch
-            header={t('anti-censorship.quic.label')}
-            subheader={t('anti-censorship.quic.warning')}
-            subheaderColor="king-nacho"
-            checked={quic}
-            onClick={onQuicChange}
-          />
-        }
-      >
-        <div className="flex flex-col gap-2">
-          <p className="text-sm text-iron dark:text-bombay">
-            {t('anti-censorship.quic.content')}
-          </p>
-          <Link
-            className="w-fit text-sm text-iron dark:text-bombay"
-            text={t('anti-censorship.quic.link')}
-            // TODO add url
-            url=""
-            color="primary"
-            icon
-          />
-        </div>
-      </SettingsMenuCardBig>
-      <SettingsMenuCardBig
-        header={
-          <CardSwitch
-            header={t('anti-censorship.stealth-api.label')}
-            subheader={t('anti-censorship.stealth-api.warning')}
-            subheaderColor="king-nacho"
-            checked={domainFronting}
-            onClick={onDomainFrontingChange}
-          />
-        }
-      >
-        <div className="flex flex-col gap-2">
-          <p className="text-sm text-iron dark:text-bombay">
-            {t('anti-censorship.stealth-api.content')}
-          </p>
-          <Link
-            className="w-fit text-sm text-iron dark:text-bombay"
-            text={t('anti-censorship.stealth-api.link')}
-            // TODO add url
-            url=""
-            color="primary"
-            icon
-          />
-        </div>
-      </SettingsMenuCardBig>
+      {backendFlags?.quic && (
+        <SettingsMenuCardBig
+          header={
+            <CardSwitch
+              header={t('anti-censorship.quic.label')}
+              subheader={t('anti-censorship.quic.warning')}
+              subheaderColor="king-nacho"
+              checked={quic}
+              onClick={onQuicChange}
+            />
+          }
+        >
+          <div className="flex flex-col gap-2">
+            <p className="text-sm text-iron dark:text-bombay whitespace-pre-line">
+              {t('anti-censorship.quic.content')}
+            </p>
+            <Link
+              className="w-fit text-sm"
+              text={t('anti-censorship.quic.link')}
+              url={QuicUrl}
+              color="primary"
+              icon
+            />
+          </div>
+        </SettingsMenuCardBig>
+      )}
+      {backendFlags?.domainFronting && (
+        <SettingsMenuCardBig
+          header={
+            <CardSwitch
+              header={t('anti-censorship.stealth-api.label')}
+              subheader={t('anti-censorship.stealth-api.warning')}
+              subheaderColor="king-nacho"
+              checked={domainFronting}
+              onClick={onDomainFrontingChange}
+            />
+          }
+        >
+          <div className="flex flex-col gap-2">
+            <p className="text-sm text-iron dark:text-bombay whitespace-pre-line">
+              {t('anti-censorship.stealth-api.content')}
+            </p>
+            <Link
+              className="w-fit text-sm"
+              text={t('anti-censorship.stealth-api.link')}
+              // TODO add url
+              url={DomainFrontingUrl}
+              color="primary"
+              icon
+            />
+          </div>
+        </SettingsMenuCardBig>
+      )}
     </PageAnim>
   );
 }
