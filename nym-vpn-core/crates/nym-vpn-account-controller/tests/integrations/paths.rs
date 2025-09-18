2633e9454a3fa0e3ca658fc3ae9fb1f67bbf03e3
durch <durch@users.noreply.github.com>
2025-09-18 18:46:39 +0200

    Unignore tests

--- nym-vpn-core/crates/nym-vpn-account-controller/tests/integrations/paths.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/tests/integrations/paths.rs
@@ -67,9 +67,7 @@ async fn offline_test() -> anyhow::Result<()> {
     Ok(())
 }
 
-// TODO: we need details here, taking them out until we rework the errors
 #[tokio::test]
-#[ignore]
 async fn api_error_reponse_test() -> anyhow::Result<()> {
     // Get the test_bench
     let mut test_bench = TestBench::new().await?;
@@ -97,7 +95,6 @@ async fn api_error_reponse_test() -> anyhow::Result<()> {
 
 // TODO: we need error details here, taking them out until we rework the errors
 #[tokio::test]
-#[ignore]
 async fn unregistered_account_test() -> anyhow::Result<()> {
     // Get the test_bench
     let mut test_bench = TestBench::new().await?;
