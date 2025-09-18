7d82cc03af209e1dd20fb4a4f2316826dac99769
jmwample <jmwample@users.noreply.github.com>
2025-09-18 15:01:53 -0600

    dependency management

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/mod.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/mod.rs
@@ -795,7 +795,6 @@ impl tunnel::Error {
             | Self::Wireguard(_)
             | Self::Cancelled
             | Self::Transport(_)
-            | Self::Io(_)
             | Self::MixnetClientDisposed => None,
             #[cfg(target_os = "ios")]
             Self::ResolveDns64(_) => None,
