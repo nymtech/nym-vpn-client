45dee8605355adf8e6b1f6d32b956b3bbfdbd30f
durch <durch@users.noreply.github.com>
2025-09-19 08:20:00 +0200

    fmt

--- nym-vpn-core/crates/nym-vpn-account-controller/src/commands/handler.rs
+++ nym-vpn-core/crates/nym-vpn-account-controller/src/commands/handler.rs
@@ -144,9 +144,7 @@ pub(crate) async fn handle_unregister_device<C: ConnectivityMonitor>(
         .await
     {
         let nym_error = NymErrorResponse::from(e);
-        if nym_error.code_reference_id
-            == Some(UNREGISTER_NON_EXISTENT_DEVICE_CODE_ID.to_string())
-        {
+        if nym_error.code_reference_id == Some(UNREGISTER_NON_EXISTENT_DEVICE_CODE_ID.to_string()) {
             // Device didn't exist in the first place so we're good
             Ok(())
         } else {
