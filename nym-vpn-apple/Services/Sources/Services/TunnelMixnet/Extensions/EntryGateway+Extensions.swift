74d6303b0591da29bcbab12465c0bea3c6809dba
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2025-09-24 01:13:53 +0300

    APPLE: fix  parse base branch (#3491)
    * APPLE: fix  parse base branch
    
    * Bump version

--- nym-vpn-apple/Services/Sources/Services/TunnelMixnet/Extensions/EntryGateway+Extensions.swift
+++ nym-vpn-apple/Services/Sources/Services/TunnelMixnet/Extensions/EntryGateway+Extensions.swift
@@ -7,9 +7,9 @@ extension EntryGateway {
     var entryPoint: EntryPoint {
         switch self {
         case let .country(country):
-            return .location(location: country.code)
+            return .country(twoLetterIsoCountryCode: country.code)
         case let .lowLatencyCountry(country):
-            return .location(location: country.code)
+            return .country(twoLetterIsoCountryCode: country.code)
         case let .gateway(gateway):
             return .gateway(identity: gateway.id)
         case .random:
