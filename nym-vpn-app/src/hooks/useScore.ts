e210c166036f3d1e2818d2e547fd90551f5a3b4e
pierre <dommerc.pierre@gmail.com>
2025-09-24 16:48:47 +0200

    feat(tauri): update server load score ui (#3510)
    * update server load score ui
    
    * bump

--- nym-vpn-app/src/hooks/useScore.ts
+++ nym-vpn-app/src/hooks/useScore.ts
@@ -5,39 +5,67 @@ import type { Score } from '../types';
 function useScore() {
   const { t } = useTranslation('nodeLocation');
 
-  const getProps = useCallback(
+  const getPerformance = useCallback(
     (score: Score) => {
       switch (score) {
         case 'offline':
           return {
             icon: 'signal_cellular_alt_1_bar',
             color: 'text-iron',
-            label: t('node-details.score.offline'),
+            label: t('node-details.perf-score.offline'),
           };
         case 'low':
           return {
             icon: 'signal_cellular_alt_1_bar',
             color: 'text-aphrodisiac',
-            label: t('node-details.score.low'),
+            label: t('node-details.perf-score.low'),
           };
         case 'medium':
           return {
             icon: 'signal_cellular_alt_2_bar',
             color: 'text-king-nacho',
-            label: t('node-details.score.medium'),
+            label: t('node-details.perf-score.medium'),
           };
         case 'high':
           return {
             icon: 'signal_cellular_alt',
             color: 'text-malachite',
-            label: t('node-details.score.high'),
+            label: t('node-details.perf-score.high'),
           };
       }
     },
     [t],
   );
 
-  return { style: getProps };
+  const getLoad = useCallback(
+    (score: Score) => {
+      switch (score) {
+        case 'offline':
+          return {
+            color: 'text-iron dark:text-bombay',
+            label: t('node-details.server-load-score.offline'),
+          };
+        case 'low':
+          return {
+            color: 'text-malachite-moss dark:text-malachite',
+            label: t('node-details.server-load-score.low'),
+          };
+        case 'medium':
+          return {
+            color: 'text-king-nacho',
+            label: t('node-details.server-load-score.medium'),
+          };
+        case 'high':
+          return {
+            color: 'text-aphrodisiac',
+            label: t('node-details.server-load-score.high'),
+          };
+      }
+    },
+    [t],
+  );
+
+  return { performance: getPerformance, serverLoad: getLoad };
 }
 
 export default useScore;
