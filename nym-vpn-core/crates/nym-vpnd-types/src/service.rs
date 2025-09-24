cce02cbf1b6c66ad4182ac2b00efd1c5621a033d
Andy Duplain <trojanfoe@gmail.com>
2025-09-24 11:10:12 +0100

    Remove the now (unused) `VpnServiceConfig` from `nym_vpnd_types` missed during merge.

--- nym-vpn-core/crates/nym-vpnd-types/src/service.rs
+++ nym-vpn-core/crates/nym-vpnd-types/src/service.rs
@@ -4,7 +4,7 @@
 use nym_gateway_directory::{EntryPoint, ExitPoint};
 use nym_sdk::UserAgent;
 use nym_vpn_network_config::{NymNetwork, NymVpnNetwork};
-use std::{fmt, net::IpAddr};
+use std::net::IpAddr;
 use time::OffsetDateTime;
 
 /// The target tunnel state.
@@ -46,65 +46,6 @@ pub struct ConnectOptions {
     pub disable_background_cover_traffic: bool,
     pub enable_credentials_mode: bool,
     pub user_agent: Option<UserAgent>,
-}
-
-#[derive(Clone, Debug, Eq, PartialEq)]
-pub struct VpnServiceConfig {
-    pub entry_point: EntryPoint,
-    pub exit_point: ExitPoint,
-    pub dns: Option<IpAddr>,
-    pub allow_lan: bool,
-    pub disable_ipv6: bool,
-    pub enable_two_hop: bool,
-    pub netstack: bool,
-    pub disable_poisson_rate: bool,
-    pub disable_background_cover_traffic: bool,
-    pub min_mixnode_performance: Option<u8>,
-    pub min_gateway_mixnet_performance: Option<u8>,
-    pub min_gateway_vpn_performance: Option<u8>,
-}
-
-impl fmt::Display for VpnServiceConfig {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "entry point: {}, exit point: {}, dns: {}, allow LAN: {}, disable IPv6: {}, two-hop: {}, netstack: {}, poisson: {}, background cover traffic: {}, min mixnode performance: {:?}, min gateway mixnet performance: {:?}, min gateway VPN performance: {:?}",
-            self.entry_point,
-            self.exit_point,
-            self.dns.map_or("None".to_string(), |ip| ip.to_string()),
-            self.allow_lan,
-            self.disable_ipv6,
-            self.enable_two_hop,
-            self.netstack,
-            self.disable_poisson_rate,
-            self.disable_background_cover_traffic,
-            self.min_mixnode_performance
-                .map_or("None".to_string(), |f| f.to_string()),
-            self.min_gateway_mixnet_performance
-                .map_or("None".to_string(), |f| f.to_string()),
-            self.min_gateway_vpn_performance
-                .map_or("None".to_string(), |f| f.to_string())
-        )
-    }
-}
-
-impl Default for VpnServiceConfig {
-    fn default() -> Self {
-        Self {
-            entry_point: EntryPoint::Random,
-            exit_point: ExitPoint::Random,
-            dns: None,
-            allow_lan: false,
-            disable_ipv6: false,
-            enable_two_hop: false,
-            netstack: false,
-            disable_poisson_rate: false,
-            disable_background_cover_traffic: false,
-            min_mixnode_performance: None,
-            min_gateway_mixnet_performance: None,
-            min_gateway_vpn_performance: None,
-        }
-    }
 }
 
 #[derive(Clone, Debug)]
