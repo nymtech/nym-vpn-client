32e7394a6b2699ebaa66debbe1c217c317651dff
Andrej Mihajlov <andrej@nymtech.net>
2025-09-25 10:41:03 +0200

    Fix naming

--- nym-vpn-core/crates/nym-gateway-directory/src/error.rs
+++ nym-vpn-core/crates/nym-gateway-directory/src/error.rs
@@ -106,7 +106,7 @@ pub enum Error {
 
 impl Error {
     /// Returns true when no gateways matching the search criteria could be found, except when the gateway is constrained to identity
-    pub fn is_no_matching_gateway_for_non_specific_gateway(&self) -> bool {
+    pub fn is_unmatched_non_specific_gateway(&self) -> bool {
         matches!(
             self,
             Error::NoMatchingEntryGatewayForLocation { .. }
