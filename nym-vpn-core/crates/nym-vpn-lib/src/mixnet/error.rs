863ef09a68af3099a2c72b5b92dff2dfe464ebba
Simon Wicky <simon@nymtech.net>
2025-09-29 11:50:04 +0200

    integration of new registration client

--- nym-vpn-core/crates/nym-vpn-lib/src/mixnet/error.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/mixnet/error.rs
@@ -3,32 +3,8 @@
 
 #[derive(thiserror::Error, Debug)]
 pub enum MixnetError {
-    #[error("failed to setup mixnet storage paths")]
-    SetupMixnetStoragePaths(#[source] Box<nym_sdk::Error>),
-
-    #[error("failed to create mixnet client with default storage")]
-    CreateMixnetClientWithDefaultStorage(#[source] Box<nym_sdk::Error>),
-
-    #[error("failed to build mixnet client")]
-    BuildMixnetClient(#[source] Box<nym_sdk::Error>),
-
-    #[error("failed to connect to mixnet")]
-    ConnectToMixnet(#[source] Box<nym_sdk::Error>),
-
-    #[error("failed to connect to mixnet entry gateway {gateway_id}")]
-    EntryGateway {
-        gateway_id: String,
-        source: Box<dyn std::error::Error + Send + Sync>,
-    },
-
-    #[error("invalid credential")]
-    InvalidCredential,
-
     #[error("failed to serialize message")]
     SerializeMessage(#[from] bincode::Error),
-
-    #[error(transparent)]
-    ConnectionMonitorError(#[from] nym_connection_monitor::Error),
 
     #[error("failed to bundle packet")]
     BundlePacket(#[source] nym_ip_packet_requests::codec::Error),
@@ -38,9 +14,6 @@ pub enum MixnetError {
 
     #[error("failed to send input message")]
     SendInputMessage(#[source] Box<nym_sdk::Error>),
-
-    #[error("mixnet client is already disposed")]
-    ClientAlreadyDisposed,
 
     #[error("failed to join on mixnet listener")]
     JoinMixnetListener(#[source] tokio::task::JoinError),
