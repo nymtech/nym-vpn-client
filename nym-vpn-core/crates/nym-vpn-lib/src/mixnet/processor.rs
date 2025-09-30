16653b2865dabb70e20d8c863e5ff5ebe4323037
Simon Wicky <simon@nymtech.net>
2025-09-30 14:31:47 +0200

    main integration

--- nym-vpn-core/crates/nym-vpn-lib/src/mixnet/processor.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/mixnet/processor.rs
@@ -13,16 +13,14 @@ use nym_ip_packet_requests::{
     v8::request::IpPacketRequest,
 };
 use nym_sdk::mixnet::{
-    InputMessage, MixnetClientSender, MixnetMessageSender, MixnetMessageSinkTranslator, Recipient,
+    InputMessage, MixnetClient, MixnetClientSender, MixnetMessageSender,
+    MixnetMessageSinkTranslator, Recipient, TransmissionLane,
 };
-#[allow(deprecated)]
-// We should not migrate this to use an SDK task management of any sort, VPN should handle this how they want, this is a leaky abstraction
-use nym_task::{TaskClient, TaskManager, connections::TransmissionLane};
 use tokio::task::JoinHandle;
 use tokio_util::{codec::Encoder, sync::CancellationToken};
 use tun::{AsyncDevice, Device};
 
-use super::{MixnetError, SharedMixnetClient, backpressure::MixnetBackpressureMonitor};
+use super::{MixnetError, backpressure::MixnetBackpressureMonitor};
 
 /// How much time to wait for ipr disconnect before proceeding to shutdown.
 const IPR_DISCONNECT_TIMEOUT: Duration = Duration::from_secs(5);
@@ -72,7 +70,7 @@ struct MixnetProcessor {
     device: AsyncDevice,
 
     // The mixnet client for sending and receiving messages from the mixnet
-    mixnet_client: SharedMixnetClient,
+    mixnet_client: MixnetClient,
 
     // The connection monitor for sending connection events
     connection_event_tx: mpsc::UnboundedSender<ConnectionStatusEvent>,
@@ -94,7 +92,7 @@ struct MixnetProcessor {
 impl MixnetProcessor {
     fn new(
         device: AsyncDevice,
-        mixnet_client: SharedMixnetClient,
+        mixnet_client: MixnetClient,
         connection_monitor: &ConnectionMonitorTask,
         ip_packet_router_address: IpPacketRouterAddress,
         our_ips: IpPair,
@@ -111,12 +109,7 @@ impl MixnetProcessor {
         }
     }
 
-    #[allow(deprecated)] // We should not migrate this to use an SDK task management of any sort, VPN should handle this how they want, this is a leaky abstraction
-    async fn run(
-        self,
-        mut task_client_mix_processor: TaskClient,
-        task_client_mix_listener: TaskClient,
-    ) -> Result<AsyncDevice, MixnetError> {
+    async fn run(self) -> Result<AsyncDevice, MixnetError> {
         tracing::info!(
             "Opened mixnet processor on tun device {}",
             self.device.get_ref().name().unwrap(),
@@ -127,36 +120,27 @@ impl MixnetProcessor {
 
         tracing::debug!("Split mixnet sender");
         let (mixnet_sender, lane_queue_lengths) = {
-            let mixnet_client_guard = self.mixnet_client.lock().await;
-
-            let shared_mixnet_client = mixnet_client_guard
-                .as_ref()
-                .ok_or(MixnetError::ClientAlreadyDisposed)?;
-
             (
-                shared_mixnet_client.split_sender(),
-                shared_mixnet_client.shared_lane_queue_lengths(),
+                self.mixnet_client.split_sender(),
+                self.mixnet_client.shared_lane_queue_lengths(),
             )
         };
+        let mixnet_client_shutdown_token = self.mixnet_client.cancellation_token().child_token();
 
         let message_creator = MessageCreator::new(self.ip_packet_router_address.into());
 
         // Starting the mixnet listener.
         tracing::debug!("Starting mixnet listener");
-        let mixnet_listener_shutdown_token = CancellationToken::new();
+        // We need a dedicated token, because the listener needs to run a bit more after global shutdown
+        let mixnet_listener_cancel_token = CancellationToken::new();
         let mut mixnet_listener_handle = super::mixnet_listener::MixnetListener::spawn(
-            self.mixnet_client.clone(),
-            task_client_mix_listener,
+            self.mixnet_client,
             tun_device_sink,
             self.icmp_beacon_identifier,
             self.our_ips,
             self.connection_event_tx.clone(),
-            mixnet_listener_shutdown_token.child_token(),
+            mixnet_listener_cancel_token.clone(),
         );
-
-        // Keep track of whether we've sent the disconnect message, so we don't send it multiple
-        // times
-        let mut has_sent_ipr_disconnect = false;
 
         // Keeps track of whether ipr disconnect timeout has been activated.
         let mut is_disconnect_timeout_active = false;
@@ -182,7 +166,7 @@ impl MixnetProcessor {
         let notify_backpressure_lifted = backpressure_monitor.get_notify_backpressure_lifted();
 
         tracing::info!("Mixnet processor is running");
-        while !task_client_mix_processor.is_shutdown() {
+        loop {
             // Disable the TUN read select branch if we are in backpressure
             let is_backpressure = backpressure_monitor.is_backpressure();
 
@@ -192,10 +176,15 @@ impl MixnetProcessor {
                     tracing::warn!("Timed out waiting for ipr disconnect");
                     break;
                 }
+                // Handle task manager shutdown
+                _ = mixnet_client_shutdown_token.cancelled() => {
+                    tracing::debug!("Mixnet client has shut down");
+                    break;
+                }
                 // When we get the cancel token, send a disconnect message to the IPR. We keep
                 // running until the mixnet listener receives the disconnect response, so we can
                 // make sure we've fully disconnected before we return.
-                _ = self.cancel_token.cancelled(), if !has_sent_ipr_disconnect => {
+                _ = self.cancel_token.cancelled() => {
                     // Start disconnect timeout upon receiving cancellation in the very first time.
                     if is_disconnect_timeout_active {
                         tracing::debug!("Re-sending disconnect message");
@@ -220,17 +209,6 @@ impl MixnetProcessor {
                     }
 
                     tracing::info!("Sent disconnect message");
-                    has_sent_ipr_disconnect = true;
-                }
-                // When the mixnet listener receives the disconnect response, it will notify us
-                // that it's done. This means we can now stop
-                _ = &mut mixnet_listener_handle => {
-                    tracing::debug!("Mixnet listener has finished");
-                    break;
-                }
-                // Handle task manager shutdown
-                _ = task_client_mix_processor.recv() => {
-                    tracing::debug!("Received shutdown");
                     break;
                 }
                 // The backpressure monitor will notify us when the backpressure is lifted, so we
@@ -246,17 +224,17 @@ impl MixnetProcessor {
                         let packet = IprPacket::from(tun_packet.into_bytes());
                         tokio::select! {
                             ret = handle_packet(packet, &mut packet_bundler, &input_message_creator, &mixnet_sender) => {
-                                if ret.is_err() && !task_client_mix_processor.is_shutdown_poll() {
+                                if ret.is_err() && !mixnet_client_shutdown_token.is_cancelled() {
                                     tracing::error!("Failed to send IP packet to the mixnet");
                                 }
                             }
-                            _ = task_client_mix_processor.recv() => {
-                                tracing::debug!("Received shutdown while sending");
+                            _ = mixnet_client_shutdown_token.cancelled() => {
+                                tracing::debug!("Mixnet client has shut down while sending");
                                 break;
                             }
                             _ = self.cancel_token.cancelled() => {
                                 tracing::debug!("Received cancellation while sending");
-                                break;
+                                continue; // go through disconnect procedure
                             }
                         }
                     }
@@ -285,17 +263,17 @@ impl MixnetProcessor {
 
                     tokio::select! {
                         ret = handle_packet(IprPacket::Flush, &mut packet_bundler, &input_message_creator, &mixnet_sender) => {
-                            if ret.is_err() && !task_client_mix_processor.is_shutdown_poll() {
+                            if ret.is_err() && !mixnet_client_shutdown_token.is_cancelled() {
                                 tracing::error!("Failed to flush the multi IP packet sink");
                             }
                         }
-                        _ = task_client_mix_processor.recv() => {
-                            tracing::debug!("Received shutdown while flushing");
+                        _ = mixnet_client_shutdown_token.cancelled() => {
+                            tracing::debug!("Mixnet client has shut down while flushing");
                             break;
                         }
                         _ = self.cancel_token.cancelled() => {
                             tracing::debug!("Received shutdown while flushing");
-                            break;
+                            continue; // go through disconnect procedure
                         }
                     }
                 }
@@ -306,10 +284,21 @@ impl MixnetProcessor {
         backpressure_monitor.stop().await;
 
         tracing::info!("Waiting for mixnet listener to finish");
-        mixnet_listener_shutdown_token.cancel();
-        let tun_device_sink = mixnet_listener_handle
-            .await
-            .map_err(MixnetError::JoinMixnetListener)?;
+
+        // Wait for the mixnet listener to finish, or force finish
+        let mix_listener_result = tokio::select! {
+            biased;
+            tun_device = &mut mixnet_listener_handle => {
+                tun_device
+            },
+            _ = ipr_disconnect_timeout => {
+                    tracing::warn!("Timed out waiting for mixnet listener to finish");
+                    mixnet_listener_cancel_token.cancel();
+                    mixnet_listener_handle.await
+            }
+        };
+
+        let tun_device_sink = mix_listener_result.map_err(MixnetError::JoinMixnetListener)?;
 
         tracing::debug!("Exiting");
         Ok(tun_device_sink
@@ -384,12 +373,10 @@ impl MixnetMessageSinkTranslator for ToIprDataRequest {
     }
 }
 
-#[allow(deprecated)] // We should not migrate this to use an SDK task management of any sort, VPN should handle this how they want, this is a leaky abstraction
 pub async fn start_processor(
     config: MixnetProcessorConfig,
     dev: AsyncDevice,
-    mixnet_client: SharedMixnetClient,
-    task_manager: &TaskManager,
+    mixnet_client: MixnetClient,
     connection_monitor: &ConnectionMonitorTask,
     cancel_token: CancellationToken,
 ) -> JoinHandle<Result<AsyncDevice, MixnetError>> {
@@ -403,15 +390,9 @@ pub async fn start_processor(
         cancel_token,
     );
 
-    let task_client_mix_processor = task_manager.subscribe_named("mixnet_processor");
-    let task_client_mix_listener = task_manager.subscribe_named("mixnet_listener");
-
     tokio::spawn(async move {
-        processor
-            .run(task_client_mix_processor, task_client_mix_listener)
-            .await
-            .inspect_err(|err| {
-                tracing::error!("Mixnet processor error: {err}");
-            })
+        processor.run().await.inspect_err(|err| {
+            tracing::error!("Mixnet processor error: {err}");
+        })
     })
 }
