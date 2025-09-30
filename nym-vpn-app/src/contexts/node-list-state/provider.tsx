a0475b10798575807b5310f1a2025e8e696becf8
pierre <dommerc.pierre@gmail.com>
2025-09-30 16:10:17 +0200

    cleanup

--- nym-vpn-app/src/contexts/node-list-state/provider.tsx
+++ nym-vpn-app/src/contexts/node-list-state/provider.tsx
@@ -72,6 +72,7 @@ function NodeListPrevStateProvider({ children }: NodeListPrevStateProps) {
         setExitExpanded([]);
         setExitFocused(null);
         setExitSearch(null);
+        break;
     }
   }, []);
 
