715cac2b0ebb03c8db7f9841fdc0f84073328ce1
Andrej Mihajlov <andrej@nymtech.net>
2025-09-30 06:37:26 +0200

    Fix uniffi type issues (#3569)

--- nym-vpn-core/crates/nym-vpnd-types-uniffi/src/gateway.rs
+++ nym-vpn-core/crates/nym-vpnd-types-uniffi/src/gateway.rs
@@ -1,6 +1,8 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+use std::net::{Ipv4Addr, Ipv6Addr};
+
 #[derive(uniffi::Record)]
 pub struct Gateway {
     pub identity_key: String,
@@ -8,6 +10,9 @@ pub struct Gateway {
     pub location: Option<GatewayLocation>,
     pub mixnet_score: Option<GatewayScore>,
     pub wg_performance: Option<GatewayPerformance>,
+    pub exit_ipv4s: Vec<Ipv4Addr>,
+    pub exit_ipv6s: Vec<Ipv6Addr>,
+    pub build_version: Option<String>,
 }
 
 #[derive(uniffi::Enum)]
@@ -27,16 +32,16 @@ pub struct GatewayPerformance {
 }
 
 #[derive(uniffi::Enum)]
-pub enum AsnKind {
+pub enum GatewayAsnKind {
     Residential,
     Other,
 }
 
 #[derive(uniffi::Record)]
-pub struct Asn {
+pub struct GatewayAsn {
     pub asn: String,
     pub name: String,
-    pub kind: AsnKind,
+    pub kind: GatewayAsnKind,
 }
 
 #[derive(uniffi::Record)]
@@ -48,7 +53,7 @@ pub struct GatewayLocation {
     pub city: String,
     pub region: String,
 
-    pub asn: Option<Asn>,
+    pub asn: Option<GatewayAsn>,
 }
 
 #[derive(uniffi::Record)]
@@ -56,18 +61,18 @@ pub struct GatewayCountry {
     pub iso_code: String,
 }
 
-impl From<nym_vpnd_types::gateway::AsnKind> for AsnKind {
+impl From<nym_vpnd_types::gateway::AsnKind> for GatewayAsnKind {
     fn from(value: nym_vpnd_types::gateway::AsnKind) -> Self {
         match value {
-            nym_vpnd_types::gateway::AsnKind::Residential => AsnKind::Residential,
-            nym_vpnd_types::gateway::AsnKind::Other => AsnKind::Other,
+            nym_vpnd_types::gateway::AsnKind::Residential => GatewayAsnKind::Residential,
+            nym_vpnd_types::gateway::AsnKind::Other => GatewayAsnKind::Other,
         }
     }
 }
 
-impl From<nym_vpnd_types::gateway::Asn> for Asn {
+impl From<nym_vpnd_types::gateway::Asn> for GatewayAsn {
     fn from(value: nym_vpnd_types::gateway::Asn) -> Self {
-        Asn {
+        GatewayAsn {
             asn: value.asn,
             name: value.name,
             kind: value.kind.into(),
@@ -118,6 +123,9 @@ impl From<nym_vpnd_types::gateway::Gateway> for Gateway {
             location: gateway.location.map(GatewayLocation::from),
             mixnet_score: gateway.mixnet_score.map(GatewayScore::from),
             wg_performance: gateway.wg_performance.map(GatewayPerformance::from),
+            exit_ipv4s: gateway.exit_ipv4s,
+            exit_ipv6s: gateway.exit_ipv6s,
+            build_version: gateway.build_version,
         }
     }
 }
