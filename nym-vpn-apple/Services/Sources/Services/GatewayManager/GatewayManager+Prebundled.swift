307556290b43458d1bfb9b28b99626e31c78f9db
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-10-13 17:11:11 +0300

    APPLE: core integration

--- nym-vpn-apple/Services/Sources/Services/GatewayManager/GatewayManager+Prebundled.swift
+++ nym-vpn-apple/Services/Sources/Services/GatewayManager/GatewayManager+Prebundled.swift
@@ -88,6 +88,7 @@ extension GatewayManager {
                 let performance = GatewayNodePerformance(
                     lastUpdated: perfV2?.lastUpdatedUTC,
                     score: perfV2.map { mapScore(from: $0.score) } ?? .noScore,
+                    mixnetScore: mapScore(from: node.mixnetScore),
                     load: perfV2.map { mapScore(from: $0.load) } ?? .noScore,
                     uptime: perfV2?.uptimePercentageLast24Hours ?? 0
                 )
@@ -119,7 +120,8 @@ extension GatewayManager {
                     location: gatewayNodeLocation,
                     performance: performance,
                     mixnetScore: perfV2.map { mapScore(from: $0.score) } ?? .noScore,
-                    moniker: node.name,
+                    name: node.name,
+                    description: node.description,
                     buildVersion: node.buildInformation?.buildVersion,
                     ipv4s: node.ipAddresses?.ipv4s ?? [],
                     ipv6s: node.ipAddresses?.ipv6s ?? []
@@ -171,6 +173,7 @@ private func mapASNType(from kind: Kind) -> GatewayNodeASNType {
 private struct NodeElement: Codable, Sendable {
     let identityKey: String
     let name: String
+    let description: String?
     let ipPacketRouter: Authenticator?
     let authenticator: Authenticator?
     let location: Location
@@ -181,11 +184,13 @@ private struct NodeElement: Codable, Sendable {
     let entry: Entry?
     let performance: String?
     let performanceV2: PerformanceV2?
+    let mixnetScore: Load?
     let buildInformation: BuildInformation?
 
     enum CodingKeys: String, CodingKey {
         case identityKey = "identity_key"
         case name
+        case description
         case ipPacketRouter = "ip_packet_router"
         case authenticator, location
         case lastProbe = "last_probe"
@@ -194,6 +199,7 @@ private struct NodeElement: Codable, Sendable {
         case role, entry, performance
         case performanceV2 = "performance_v2"
         case buildInformation = "build_information"
+        case mixnetScore = "mixnet_score"
     }
 }
 
