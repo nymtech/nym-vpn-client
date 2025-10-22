77b499addb8743483ad02e143db2aa6276f622e0
pierre <dommerc.pierre@gmail.com>
2025-10-22 11:14:06 +0200

    feat(tauri): add light theme color for quic tag (#3728)

--- nym-vpn-app/src/screens/node/QuicTag.tsx
+++ nym-vpn-app/src/screens/node/QuicTag.tsx
@@ -9,12 +9,16 @@ function QuicTag({ className }: QuicTagProps) {
   return (
     <div
       className={clsx(
-        'border rounded border-azur/50 text-azur select-none',
+        'border rounded text-ozone  dark:text-azur select-none',
+        'border-ozone/50 dark:border-azur/50',
         'flex items-center flex-nowrap gap-1 font-medium text-xs py-0 px-2',
         className,
       )}
     >
-      <MsIcon icon="package_2" className="text-base text-azur" />
+      <MsIcon
+        icon="package_2"
+        className="text-base text-ozone dark:text-azur"
+      />
       <p>QUIC</p>
     </div>
   );
