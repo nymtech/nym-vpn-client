a0d719cf2d73499ecfb9c68dc127bce7437a5fad
nighthawk24 <nighthawk24@users.noreply.github.com>
2025-10-28 13:34:55 +0000

    VPN-3965: Add streaming icon on home and Exit (#3774)
    * Add stream icon on Home, exit and show banner on Home.
    
    * Update CHANGELOG
    
    * Fix detekt issue.

--- nym-vpn-android/CHANGELOG.md
+++ nym-vpn-android/CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Android: On Entry/Exit screens, display city instead of server key ID (https://github.com/nymtech/nym-vpn-client/pull/3733)
 - In "Gateway details", use relative time in last update. (https://github.com/nymtech/nym-vpn-client/pull/3748)
 - Display server up-time. (https://github.com/nymtech/nym-vpn-client/pull/3749)
+- Add streaming icon on home and Exit + update tooltip (https://github.com/nymtech/nym-vpn-client/pull/3774)
 
 ## [2.1.0]
 
--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/main/components/LocationField.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/main/components/LocationField.kt
@@ -3,18 +3,23 @@ package net.nymtech.nymvpn.ui.screens.main.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -37,6 +42,7 @@ fun LocationField(
 	onClick: () -> Unit,
 	enabled: Boolean,
 	modifier: Modifier = Modifier,
+	showGatewayStreamIcon: Boolean = false,
 ) {
 	val context = LocalContext.current
 	val trailingIcon = ImageVector.vectorResource(R.drawable.link_arrow_right)
@@ -59,7 +65,29 @@ fun LocationField(
 					.size(iconSize),
 			)
 		},
-		trailing = { Icon(trailingIcon, stringResource(R.string.go), tint = MaterialTheme.colorScheme.onSurface) },
+		trailing = {
+			Row(
+				verticalAlignment = Alignment.CenterVertically,
+				modifier = Modifier.then(
+					if (showGatewayStreamIcon) {
+						Modifier.padding(horizontal = 12.dp)
+					} else {
+						Modifier
+					},
+				),
+			) {
+				if (showGatewayStreamIcon) {
+					Icon(
+						imageVector = ImageVector.vectorResource(R.drawable.smart_display),
+						contentDescription = stringResource(R.string.stream_display),
+						modifier = Modifier.size(iconSize),
+						tint = Color.Unspecified,
+					)
+					Spacer(modifier = Modifier.width(4.dp))
+				}
+				Icon(trailingIcon, stringResource(R.string.go), tint = MaterialTheme.colorScheme.onSurface)
+			}
+		},
 		singleLine = true,
 		subtitle = {
 			if (!gatewayLocation.isNullOrEmpty()) {
