03c37ba0b90e8667a6dbc1e41ee848c7998c53a3
Andrej Mihajlov <andrej@nymtech.net>
2025-11-04 07:44:01 +0100

    Implement TCP connection probe

--- nym-vpn-core/crates/nym-connection-monitor/src/lib.rs
+++ nym-vpn-core/crates/nym-connection-monitor/src/lib.rs
@@ -6,9 +6,11 @@ mod connection_probe;
 mod icmp_probe;
 #[cfg(test)]
 mod mock_probe;
+mod tcp_probe;
 
 pub use connection_monitor::{
     ConnectionEvent, ConnectionMonitor, ConnectionStatusEvent, Error, Phase, TimingConfig,
 };
 pub use connection_probe::{BoxedProbeError, ConnectionProbe, ProbeError};
 pub use icmp_probe::{IcmpProbe, IcmpProbeConfig, IcmpProbeError};
+pub use tcp_probe::{TcpProbe, TcpProbeConfig, TcpProbeError};
