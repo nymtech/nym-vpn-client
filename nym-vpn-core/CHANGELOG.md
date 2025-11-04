1ae77ff3718f1b9d7fcbd2381b57a1eda077b054
Andrej Mihajlov <andrej@nymtech.net>
2025-11-04 18:22:08 +0100

    Implement TCP connection probe (#3868)

--- nym-vpn-core/CHANGELOG.md
+++ nym-vpn-core/CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Added
+
+- Implement a TCP-based probe as a fallback for connection monitoring when ICMP is unavailable. (https://github.com/nymtech/nym-vpn-client/pull/3868)
+
 ### Changed
 
 - Rotate wireguard keys every 1-2 weeks, if disconnected (https://github.com/nymtech/nym-vpn-client/pull/3788)
--- nym-vpn-core/crates/nym-connection-monitor/src/lib.rs
+++ nym-vpn-core/crates/nym-connection-monitor/src/lib.rs
@@ -6,9 +6,24 @@ mod connection_probe;
 mod icmp_probe;
 #[cfg(test)]
 mod mock_probe;
+mod tcp_probe;
+
+use std::net::{Ipv4Addr, Ipv6Addr};
 
 pub use connection_monitor::{
     ConnectionEvent, ConnectionMonitor, ConnectionStatusEvent, Error, Phase, TimingConfig,
 };
 pub use connection_probe::{BoxedProbeError, ConnectionProbe, ProbeError};
 pub use icmp_probe::{IcmpProbe, IcmpProbeConfig, IcmpProbeError};
+pub use tcp_probe::{TcpProbe, TcpProbeConfig, TcpProbeError};
+
+/// Default host used for probing via IPv4
+/// The same hosts are used by ICMP and TCP probes.
+const DEFAULT_IPV4_PROBE_IP: Ipv4Addr = Ipv4Addr::new(1, 1, 1, 1);
+
+/// Default host used for probing via IPv6
+/// The same hosts are used by ICMP and TCP probes.
+const DEFAULT_IPV6_PROBE_IP: Ipv6Addr = Ipv6Addr::new(0x2606, 0x4700, 0x4700, 0, 0, 0, 0, 0x1111);
+
+/// Default port for probing the above hosts over TCP
+const DEFAULT_TCP_PROBE_PORT: u16 = 80;
