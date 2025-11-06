71467ea036828ab37e0a3b58aca112ecfe9ddf33
nighthawk24 <nighthawk24@users.noreply.github.com>
2025-11-06 13:09:18 +0000

    VPN-3942: Split by US state and allow to connect to US state (#3895)
    * Split by US state and allow to connect to US state.
    
    * Refactor HopViewModel and UI to take logic out of UI to vieModel.
    
    * Update CHANGELOG
    
    * Add PR link to CHANGELOG

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/hop/HopUiState.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/hop/HopUiState.kt
@@ -8,9 +8,15 @@ data class HopUiState(
 	val error: Boolean = false,
 	val isQuicFeatureFlagEnabled: Boolean = false,
 	val query: String = "",
-	val countries: List<Locale> = emptyList(),
-	val queriedGateways: List<NymGateway> = emptyList(),
+	val items: List<ItemType> = emptyList(),
 )
+
+sealed interface ItemType {
+	data class CountryItem(val locale: Locale, val gateways: List<NymGateway>, val regions: List<Region>? = null) : ItemType {
+		data class Region(val region: String, val gateways: List<NymGateway>)
+	}
+	data class GatewayItem(val gateway: NymGateway) : ItemType
+}
 
 internal fun NymGateway.serverLocation(countryName: String?): String {
 	val region = this.region.takeIf { countryCodesForRegionSupport.contains(this.twoLetterCountryISO) }
