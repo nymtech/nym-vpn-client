62bac01bf533f596af69551bc07c3b3aaee93aa2
Andy Duplain <trojanfoe@gmail.com>
2025-11-10 15:06:06 +0000

    Formatting.

--- nym-vpn-core/crates/nym-vpn-lib/src/bandwidth_controller.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/bandwidth_controller.rs
@@ -6,7 +6,7 @@ use std::{net::IpAddr, time::Duration};
 use nym_authenticator_client::AuthenticatorClient;
 use nym_bandwidth_controller::{BandwidthTicketProvider, DEFAULT_TICKETS_TO_SPEND};
 use nym_registration_common::GatewayData;
-use tokio_stream::{wrappers::IntervalStream, StreamExt};
+use tokio_stream::{StreamExt, wrappers::IntervalStream};
 use tokio_util::sync::CancellationToken;
 
 use nym_common::ErrorExt;
