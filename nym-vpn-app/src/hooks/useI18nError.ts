3b424a1b597599b594971c4a045dab0070d5731d
pierre <dommerc.pierre@gmail.com>
2025-11-12 16:24:20 +0100

    cleanup

--- nym-vpn-app/src/hooks/useI18nError.ts
+++ nym-vpn-app/src/hooks/useI18nError.ts
@@ -1,78 +1,17 @@
 import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ErrorKey, TunnelError, isInternalError } from '../types';
-
-// enforce that all errors are handled
-type Terror = (error: ErrorKey | TunnelError) => string;
-
-function fmtErr(msg: string, data?: string | null) {
-  if (data && data.length > 0) {
-    return `${msg} - ${data}`;
-  }
-  return msg;
-}
+import { ErrorKey } from '../types';
 
 /**
  * Hook to get the translation function for backend errors
  *
  * @returns The translation function
  */
-// TODO split this function to avoid key conflicts between BackendError and TunnelError
 function useI18nError() {
   const { t } = useTranslation('errors');
 
-  const translateError: Terror = useCallback(
-    (error: ErrorKey | TunnelError) => {
-      if (isInternalError(error)) {
-        return fmtErr(t('tunnel.internal'), error.internal);
-      }
-      // tunnel state errors
-      switch (error) {
-        case 'tun-device':
-          return t('tunnel.tun-device');
-        case 'tunnel-provider':
-          return t('tunnel.tunnel-provider');
-        case 'inactive-account':
-          return t('tunnel.inactive-account');
-        case 'device-logged-out':
-          return t('tunnel.device-logged-out');
-        case 'set-firewall-policy':
-          return t('tunnel.firewall');
-        case 'set-routing':
-          return t('tunnel.routing');
-        case 'set-dns':
-          return t('tunnel.dns');
-        case 'same-entry-and-exit-gw':
-          return t('tunnel.same-entry-exit-gw');
-        case 'invalid-entry-gw-country':
-          return t('tunnel.invalid-entry-gw-country');
-        case 'invalid-exit-gw-country':
-          return t('tunnel.invalid-exit-gw-country');
-        case 'invalid-entry-gw-id':
-          return t('tunnel.invalid-entry-gw-id');
-        case 'invalid-exit-gw-id':
-          return t('tunnel.invalid-exit-gw-id');
-        case 'max-devices-reached':
-          return t('tunnel.max-devices-reached');
-        case 'bandwidth-exceeded':
-          return t('tunnel.bandwidth-exceeded');
-        case 'inactive-subscription':
-          return t('tunnel.subscription-expired');
-        case 'device-time-out-of-sync':
-          return t('tunnel.device-time-out-of-sync');
-        case 'ipv6-unavailable':
-          return t('tunnel.ipv6-unavailable');
-        case 'credential-wasted-on-entry-gateway':
-          return t('tunnel.credential-wasted-entry-gw');
-        case 'credential-wasted-on-exit-gateway':
-          return t('tunnel.credential-wasted-exit-gw');
-        case 'performant-entry-gw-unavailable':
-          return t('tunnel.performant-entry-gw-unavailable');
-        case 'performant-exit-gw-unavailable':
-          return t('tunnel.performant-exit-gw-unavailable');
-      }
-
-      // not a tunnel error
+  const translate = useCallback(
+    (error: ErrorKey) => {
       switch (error) {
         // mixnet event errors
         case 'entry-gw-down':
@@ -117,6 +56,8 @@ function useI18nError() {
           return t('account.max-devices-reached');
         case 'device-time-desync':
           return t('account.device-time-out-of-sync');
+        case 'bandwidth-exceeded':
+          return t('account.bandwidth-exceeded');
       }
 
       console.warn('unhandled backend error', error);
@@ -125,7 +66,7 @@ function useI18nError() {
     [t],
   );
 
-  return { tE: translateError };
+  return { tE: translate };
 }
 
 export default useI18nError;
