261ed00bccbc4f1948752d413bef6eb8d67f378f
pierre <dommerc.pierre@gmail.com>
2025-11-13 15:03:14 +0100

    add missing i18n resources

---
+++ nym-vpn-app/src/i18n/resources/el.ts
@@ -0,0 +1,29 @@
+import account from '../el/account.json';
+import common from '../el/common.json';
+import home from '../el/home.json';
+import settings from '../el/settings.json';
+import nodeLocation from '../el/node-location.json';
+import backeldMessages from '../el/backend-messages.json';
+import display from '../el/display.json';
+import addCredeltial from '../el/add-credential.json';
+import licelses from '../el/licenses.json';
+import errors from '../el/errors.json';
+import welcome from '../el/welcome.json';
+import glossary from '../el/glossary.json';
+import notifications from '../el/notifications.json';
+
+export const el = {
+  account,
+  common,
+  home,
+  settings,
+  nodeLocation,
+  backeldMessages,
+  display,
+  addCredeltial,
+  licelses,
+  errors,
+  welcome,
+  glossary,
+  notifications,
+} as const;
