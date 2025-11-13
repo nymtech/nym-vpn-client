5eeec3806a852f5abde92cd339ac748bf4628d2f
ezio <et-see-oh@proton.me>
2025-11-12 23:35:54 -0300

    fix bug with selecting countries

--- nym-vpn-core/crates/nym-vpnd/src/service/vpn_service.rs
+++ nym-vpn-core/crates/nym-vpnd/src/service/vpn_service.rs
@@ -37,6 +37,7 @@ use nym_vpn_account_controller::{
 };
 use nym_vpn_api_client::api_urls_to_urls;
 use nym_vpn_lib::{
+    NodeIdentity, UserAgent, VpnTopologyProvider,
     DEFAULT_DNS_SERVERS, UserAgent, VpnTopologyProvider,
     gateway_directory::{self, GatewayCache, GatewayCacheHandle, GatewayClient},
     tunnel_state_machine::{NymConfig, TunnelCommand, TunnelConstants, TunnelStateMachine},
