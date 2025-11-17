47beb63c6cb3c9f71bdc68a7b5486b6866294640
mrkiwi <sirakandrew@gmail.com>
2025-11-17 11:08:50 +0100

    Revert version bump

--- nym-vpn-android/CHANGELOG.md
+++ nym-vpn-android/CHANGELOG.md
@@ -7,8 +7,6 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
-## [2.3.1]
-
 ### Added
 - Crowdin Translations (https://github.com/nymtech/nym-vpn-client/pull/2777)
 
--- nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
+++ nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Constants {
-	const val VERSION_NAME = "v2.3.1"
-    const val VERSION_CODE = 23100
+	const val VERSION_NAME = "v2.3.0"
+    const val VERSION_CODE = 23000
     const val TARGET_SDK = 36
     const val COMPILE_SDK = 36
     const val MIN_SDK = 24
