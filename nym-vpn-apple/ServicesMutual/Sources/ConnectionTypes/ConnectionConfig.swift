ac654faa09375d5323e9c746a33c971e4dbb57a8
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2025-11-19 09:03:25 +0200

    APPLE: fix wrong connection mode on first launch (#3991) (#3992)

--- nym-vpn-apple/ServicesMutual/Sources/ConnectionTypes/ConnectionConfig.swift
+++ nym-vpn-apple/ServicesMutual/Sources/ConnectionTypes/ConnectionConfig.swift
@@ -1,9 +1,10 @@
+import Foundation
 #if os(iOS)
 #elseif os(macOS)
 import NymVPNRpc
 #endif
 
-public final class ConnectionConfig {
+public final class ConnectionConfig: Codable {
     public var entry: EntryGateway
     public var exit: ExitRouter
     public var dns: String?
@@ -146,3 +147,34 @@ private extension ConnectionConfig {
     }
 }
 #endif
+
+extension ConnectionConfig: Equatable {
+    public static func == (lhs: ConnectionConfig, rhs: ConnectionConfig) -> Bool {
+        lhs.entry == rhs.entry &&
+        lhs.exit == rhs.exit &&
+        lhs.dns == rhs.dns &&
+        lhs.allowLan == rhs.allowLan &&
+        lhs.disableIpv6 == rhs.disableIpv6 &&
+        lhs.enableTwoHop == rhs.enableTwoHop &&
+        lhs.enableBridges == rhs.enableBridges &&
+        lhs.netstack == rhs.netstack &&
+        lhs.disablePoissonRate == rhs.disablePoissonRate &&
+        lhs.disableBackgroundCoverTraffic == rhs.disableBackgroundCoverTraffic &&
+        lhs.minMixnodePerformance == rhs.minMixnodePerformance &&
+        lhs.minGatewayMixnetPerformance == rhs.minGatewayMixnetPerformance &&
+        lhs.minGatewayVpnPerformance == rhs.minGatewayVpnPerformance &&
+        lhs.residentialExit == rhs.residentialExit
+    }
+}
+
+extension ConnectionConfig {
+    public func toJson() -> String? {
+        guard let jsonData = try? JSONEncoder().encode(self) else { return nil }
+        return String(data: jsonData, encoding: .utf8)
+    }
+
+    public static func from(jsonString: String) -> ConnectionConfig? {
+        guard let jsonData = jsonString.data(using: .utf8) else { return nil }
+        return try? JSONDecoder().decode(ConnectionConfig.self, from: jsonData)
+    }
+}
