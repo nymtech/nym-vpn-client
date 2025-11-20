2bf837b2d9b47e5d4a7cd346e8a69fcd37c77e76
Andrii <sirakandrew@gmail.com>
2025-11-20 13:03:00 +0100

    Android: Cherry-pick 2.4.0 version (#4004)
    * Bump version to 2.4.0
    
    * Fix translations (#3979)
    
    * Force IPv4 connections for ruby (#3996)
    
    * Force IPv4 connections for ruby
    
    * Update ruby version
    
    * Add with:
    
    * Downgrade Ruby to 3.3 (#3997)
    
    * Downgrade to 3.3
    
    CLIToolsDistributor failed in v3.4
    
    * Update publish play store workflow (#4002)
    
    * Fix locales and metadata (#4003)
    
    * Fix locales
    
    * Update vi locale
    
    * Update short descriptions
    
    ---------
    
    Co-authored-by: nighthawk24 <nighthawk24@users.noreply.github.com>

--- nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
+++ nym-vpn-android/buildSrc/src/main/kotlin/Constants.kt
@@ -1,8 +1,8 @@
 import org.gradle.api.JavaVersion
 
 object Constants {
-	const val VERSION_NAME = "v2.3.0"
-    const val VERSION_CODE = 23000
+	const val VERSION_NAME = "v2.4.0"
+    const val VERSION_CODE = 24000
     const val TARGET_SDK = 36
     const val COMPILE_SDK = 36
     const val MIN_SDK = 24
