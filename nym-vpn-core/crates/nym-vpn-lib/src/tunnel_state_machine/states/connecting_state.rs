defce03d561cfe1e2e1a867c7de68240f546b102
Andy Duplain <trojanfoe@gmail.com>
2025-11-26 20:02:18 +0000

    Force gateway re-selection after blacklisting the entry gateway.

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/connecting_state.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/connecting_state.rs
@@ -626,12 +626,12 @@ impl TunnelStateHandler for ConnectingState {
                     }
                     TunnelMonitorEvent::ConnectionFailed => {
                         // We have failed to connect repeatidly; let's blacklist the previously selected
-                        // entry gateways for a while (or until successfully connected) in order to avoid
-                        // connection attempt loops.
+                        // entry gateways for a while and force gateway re-selection.
                         if let Some(ref selected_gateways) = self.selected_gateways {
                             let entry_gateway_identifier = selected_gateways.entry_gateway().identity;
                             tracing::warn!("Blacklisting entry gateway {} due to repeated connection failure", entry_gateway_identifier.to_string());
                             shared_state.blacklisted_entry_gateways.add(entry_gateway_identifier);
+                            self.selected_gateways = None;
                         }
                         NextTunnelState::SameState(self)
                     }
