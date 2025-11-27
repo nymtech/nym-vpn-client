0896a152aee6b5581a6a493d4f6c4f94e65c8a86
mrkiwi <sirakandrew@gmail.com>
2025-11-27 16:48:19 +0100

    Update login button

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/components/LoginSection.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/components/LoginSection.kt
@@ -1,5 +1,6 @@
 package net.nymtech.nymvpn.ui.screens.settings.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
@@ -7,10 +8,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.nymtech.nymvpn.R
 import net.nymtech.nymvpn.ui.common.buttons.MainStyledButton
 import net.nymtech.nymvpn.ui.theme.CustomTypography
+import net.nymtech.nymvpn.ui.theme.NymVPNTheme
+import net.nymtech.nymvpn.ui.theme.Theme
 import net.nymtech.nymvpn.util.extensions.scaledHeight
 
 @Composable
@@ -19,12 +23,22 @@ fun LoginSection(isMnemonicStored: Boolean, onLoginClick: () -> Unit) {
 		MainStyledButton(
 			onClick = onLoginClick,
 			content = {
-				Text(stringResource(R.string.log_in).uppercase(), style = CustomTypography.buttonMain)
+				Text(
+					stringResource(R.string.log_in),
+					style = CustomTypography.buttonMain,
+				)
 			},
-			color = MaterialTheme.colorScheme.primary,
 			modifier = Modifier
 				.fillMaxWidth()
 				.height(56.dp.scaledHeight()),
 		)
+	}
+}
+
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+internal fun PreviewLoginSection() {
+	NymVPNTheme(Theme.default()) {
+		LoginSection( false) {}
 	}
 }
