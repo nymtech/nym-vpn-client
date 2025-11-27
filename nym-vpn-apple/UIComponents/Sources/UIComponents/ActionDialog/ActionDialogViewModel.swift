f6c2645b4d3af5a010fef1a170b396515ccd98ad
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2025-11-27 22:37:59 +0200

    APPLE: settings logout improvements

--- nym-vpn-apple/UIComponents/Sources/UIComponents/ActionDialog/ActionDialogViewModel.swift
+++ nym-vpn-apple/UIComponents/Sources/UIComponents/ActionDialog/ActionDialogViewModel.swift
@@ -6,13 +6,16 @@ import ImpactGenerator
     let configuration: ActionDialogConfiguration
 
     @Binding var isDisplayed: Bool
+    @Binding var isLoading: Bool
 
     public init(
         isDisplayed: Binding<Bool>,
         configuration: ActionDialogConfiguration,
-        impactGenerator: ImpactGenerator
+        impactGenerator: ImpactGenerator,
+        isLoading: Binding<Bool> = .constant(false)
     ) {
         _isDisplayed = isDisplayed
+        _isLoading = isLoading
         self.impactGenerator = impactGenerator
         self.configuration = configuration
     }
