256058a9183638d97dfe3b49ef1b5f7c49687ca7
Karol Sawicki <agentpietrucha@gmail.com>
2025-11-28 11:19:57 +0100

    Fix hop select button overflow (#4069)

--- nym-vpn-app/src/screens/home/HopSelect.tsx
+++ nym-vpn-app/src/screens/home/HopSelect.tsx
@@ -261,7 +261,7 @@ export default function HopSelect({
 
       <Button
         className={clsx([
-          'flex flex-1 pl-4 items-center justify-center h-full py-3 rounded-none rounded-l-lg',
+          'flex flex-1 pl-4 items-center justify-center h-full py-3 rounded-none rounded-l-lg overflow-hidden',
           !disabled && 'hover:text-white/80',
         ])}
         onClick={handleClick}
