a3b704e4b1dca88989fff8cc201823b6c686b011
mrkiwi <sirakandrew@gmail.com>
2025-12-01 10:28:45 +0100

    Scaled Switch styling

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/common/buttons/ScaledSwitch.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/common/buttons/ScaledSwitch.kt
@@ -1,10 +1,16 @@
 package net.nymtech.nymvpn.ui.common.buttons
 
+import android.content.res.Configuration
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import net.nymtech.nymvpn.ui.theme.NymVPNTheme
+import net.nymtech.nymvpn.ui.theme.Theme
 import net.nymtech.nymvpn.util.extensions.scaledHeight
 
 @Composable
@@ -13,5 +19,22 @@ fun ScaledSwitch(checked: Boolean, onClick: (checked: Boolean) -> Unit, enabled:
 		checked,
 		{ onClick(it) },
 		Modifier.scale((52.dp.scaledHeight() / 52.dp)),
+		colors = SwitchDefaults.colors(
+			checkedBorderColor = MaterialTheme.colorScheme.primary,
+			checkedThumbColor = MaterialTheme.colorScheme.surface,
+			checkedTrackColor = MaterialTheme.colorScheme.primary,
+			uncheckedBorderColor = MaterialTheme.colorScheme.outline,
+			uncheckedThumbColor = MaterialTheme.colorScheme.outline,
+		)
 	)
+}
+
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+internal fun PreviewScaledSwitch() {
+	NymVPNTheme(Theme.default()) {
+		ScaledSwitch(
+			true, {}
+		)
+	}
 }
