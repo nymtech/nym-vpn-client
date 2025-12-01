41038ab1f5705c8d0c5747d0086471fd22488e86
Simon Wicky <simon@nymtech.net>
2025-12-01 09:54:01 +0100

    cleanup

--- nym-vpn-core/crates/nym-statistics/src/lib.rs
+++ nym-vpn-core/crates/nym-statistics/src/lib.rs
@@ -1,15 +1,19 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-mod api_client;
-mod config;
+#![warn(clippy::expect_used)]
+#![warn(clippy::unwrap_used)]
+#![warn(clippy::todo)]
+#![warn(clippy::dbg_macro)]
+
+mod commands;
 mod controller;
 mod error;
-pub mod events;
+mod events;
 mod handler;
 mod storage;
 
-pub use config::StatisticsControllerConfig;
+pub use commands::StatisticsCommandsSender;
 pub use controller::StatisticsController;
-pub use error::Error;
+pub use error::Error as StatisticsControllerError;
 pub use events::StatisticsSender;
