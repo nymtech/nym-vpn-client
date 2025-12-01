3bd5efd8c037333cba09ff587ff1d7f2f10f4d8c
mrkiwi <sirakandrew@gmail.com>
2025-12-01 10:11:42 +0100

    Fix loading dialog for split tunneling

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/tunneling/components/LoadingDialog.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/tunneling/components/LoadingDialog.kt
@@ -1,5 +1,6 @@
 package net.nymtech.nymvpn.ui.screens.settings.tunneling.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,10 +17,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import net.nymtech.nymvpn.R
+import net.nymtech.nymvpn.ui.theme.NymVPNTheme
+import net.nymtech.nymvpn.ui.theme.Theme
 import net.nymtech.nymvpn.util.extensions.scaledHeight
 
 @Composable
@@ -51,8 +56,17 @@ fun LoadingDialog() {
 					text = stringResource(R.string.loading),
 					style = MaterialTheme.typography.bodyMedium,
 					color = MaterialTheme.colorScheme.onSurface,
+					textAlign = TextAlign.Center
 				)
 			}
 		}
+	}
+}
+
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, locale = "fr", widthDp = 120)
+internal fun PreviewLoadingDialog() {
+	NymVPNTheme(Theme.default()) {
+		LoadingDialog()
 	}
 }
