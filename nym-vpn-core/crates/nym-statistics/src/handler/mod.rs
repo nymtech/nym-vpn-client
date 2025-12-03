b3c322782b4a964ab608821fad3efcdd94c8bbf9
Simon Wicky <simon@nymtech.net>
2025-12-03 10:00:19 +0100

    core of collection v2

--- nym-vpn-core/crates/nym-statistics/src/handler/mod.rs
+++ nym-vpn-core/crates/nym-statistics/src/handler/mod.rs
@@ -1,37 +1,39 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use nym_statistics_common::{
-    generate_vpn_client_stats_id, report::vpn_client::VpnClientStatsReport,
-};
-use static_information::StaticInformationHandler;
+use nym_vpn_lib_types::NetworkStatisticsConfig;
 use usage::UsageHandler;
 
 use crate::{
-    config::StatisticsControllerConfig,
-    error::Error,
-    events::{ControllerEvent, StatisticsEvent},
-    storage::StatsStorage,
+    commands::ConfigCommand, events::StatisticsEvent,
+    handler::report_sending::ReportSendingHandler, storage::StatsStorage,
 };
+use nym_statistics_api_client::StatisticsApiClient;
 
-mod static_information;
+mod report_sending;
 mod usage;
 
 pub(crate) struct StatisticsHandler {
     storage: StatsStorage,
-    config: StatisticsControllerConfig,
 
-    static_information_handler: StaticInformationHandler,
+    // Handlers
+    report_sending_handler: ReportSendingHandler,
     usage_handler: UsageHandler,
-    //SW TODO investigate using trait like Andrew did in Nym-nodes
 }
 
 impl StatisticsHandler {
-    pub fn new(storage: StatsStorage, config: StatisticsControllerConfig) -> Self {
+    pub fn new(
+        storage: StatsStorage,
+        stats_api_client: StatisticsApiClient,
+        config: NetworkStatisticsConfig,
+    ) -> Self {
         StatisticsHandler {
             storage: storage.clone(),
-            config,
-            static_information_handler: StaticInformationHandler::new(),
+            report_sending_handler: ReportSendingHandler::new(
+                storage.clone(),
+                config,
+                stats_api_client,
+            ),
             usage_handler: UsageHandler::new(storage),
         }
     }
@@ -42,43 +44,18 @@ impl StatisticsHandler {
 
     pub async fn handle_event(&mut self, event: StatisticsEvent) {
         match event {
-            StatisticsEvent::Usage(e) => self.usage_handler.handle_event(e),
-            StatisticsEvent::Controller(e) => self.handle_controller_event(e).await,
+            StatisticsEvent::Usage(e) => self.usage_handler.handle_event(e).await,
+            StatisticsEvent::ReportSending(e) => self.report_sending_handler.handle_event(e),
         }
     }
 
-    pub async fn get_report(&mut self) -> Result<VpnClientStatsReport, Error> {
-        // Use seed override or storage one
-        let seed = self
-            .config
-            .stats_id_seed
-            .clone()
-            .unwrap_or(self.storage.maybe_init_and_load_seed().await?);
-        let identifier = generate_vpn_client_stats_id(seed);
-        Ok(
-            VpnClientStatsReport::new(identifier, self.static_information_handler.get_report())
-                .with_usage_report(self.usage_handler.get_report()),
-        )
+    pub async fn handle_command(&mut self, command: ConfigCommand) {
+        self.usage_handler.handle_command(command).await;
+        self.report_sending_handler.handle_command(command).await;
     }
 
-    async fn handle_controller_event(&mut self, event: ControllerEvent) {
-        match event {
-            ControllerEvent::RemoveSeed => self
-                .storage
-                .remove_seed()
-                .await
-                .inspect_err(|e| tracing::error!("Failed to remove stats seed : {e}"))
-                .ok(),
-            ControllerEvent::ResetSeed => self
-                .storage
-                .reset_seed()
-                .await
-                .inspect_err(|e| tracing::error!("Failed to reset stats seed : {e}"))
-                .ok(),
-        };
-    }
-
-    pub fn is_connected(&self) -> bool {
-        self.usage_handler.is_connected
+    pub async fn on_shutdown(&mut self) {
+        self.usage_handler.on_shutdown().await;
+        self.report_sending_handler.on_shutdown().await;
     }
 }
