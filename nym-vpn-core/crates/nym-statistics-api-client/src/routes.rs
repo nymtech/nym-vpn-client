db43029ed30aeb275841f78020b114c0e6273419
Simon Wicky <simon@nymtech.net>
2025-12-03 10:00:19 +0100

    api client support for report v2

--- nym-vpn-core/crates/nym-statistics-api-client/src/routes.rs
+++ nym-vpn-core/crates/nym-statistics-api-client/src/routes.rs
@@ -1,4 +1,4 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-pub(crate) const REPORT_ROUTE: &str = "v1/stats/report";
+pub(crate) const SESSION_REPORT_ROUTE: &str = "v1/stats/session";
