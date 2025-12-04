63e118e89da0eed02d3ce6468b297ee79cb0d7ee
Simon Wicky <simon@nymtech.net>
2025-12-04 11:09:58 +0100

    report v2 storage

---
+++ nym-vpn-core/crates/nym-statistics/src/storage/test.rs
@@ -0,0 +1,21 @@
+use sqlx_pool_guard::SqlitePoolGuard;
+
+use crate::storage::{StatsStorage, sqlite::SqliteStatsStorageManager};
+
+pub(crate) async fn mock_database() -> StatsStorage {
+    let connection_pool = SqlitePoolGuard::new(
+        sqlx::sqlite::SqlitePoolOptions::new()
+            .connect(":memory:")
+            .await
+            .expect("cannot connect to db"),
+    );
+
+    sqlx::migrate!("./migrations")
+        .run(&*connection_pool)
+        .await
+        .expect("Failed to run migrations");
+
+    StatsStorage {
+        storage_manager: SqliteStatsStorageManager::new(connection_pool),
+    }
+}
