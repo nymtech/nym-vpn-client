7a5c328ae9936808ecec956849fcbb5585d4c0c8
Simon Wicky <simon@nymtech.net>
2025-12-04 11:09:59 +0100

    core of collection v2

--- nym-vpn-core/crates/nym-statistics/src/error.rs
+++ nym-vpn-core/crates/nym-statistics/src/error.rs
@@ -8,4 +8,20 @@ pub enum Error {
 
     #[error("storage error : {0}")]
     StatsStorage(#[from] crate::storage::error::StatsStorageError),
+
+    #[error("failed to initialzed storage")]
+    NoStorage,
+
+    #[error("Some reports couldn't be sent")]
+    ReportSending,
+
+    // Internal error that should not happen, channel related
+    #[error("internal error: {0}")]
+    Internal(String),
+}
+
+impl Error {
+    pub fn internal(message: impl ToString) -> Self {
+        Error::Internal(message.to_string())
+    }
 }
