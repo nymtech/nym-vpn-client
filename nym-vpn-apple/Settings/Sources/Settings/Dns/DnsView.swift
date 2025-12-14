51a4742fcd6cbfba1983d46a63065ba703392a60
Matthew Watt <matt@twopoint.dev>
2025-12-14 13:50:28 -0600

    Custom dns layout and styling complete

--- nym-vpn-apple/Settings/Sources/Settings/Dns/DnsView.swift
+++ nym-vpn-apple/Settings/Sources/Settings/Dns/DnsView.swift
@@ -10,6 +10,11 @@ import UIComponents
 
 public struct DnsView: View {
     @StateObject private var viewModel: DnsViewModel
+    
+    @FocusState private var isIpAddressTextFieldFocused: Bool
+    @State private var isCustomDnsHovered = false
+    @State private var isCustomDnsTextFieldHovered = false
+    @State private var isCustomDnsAddButtonHovered = false
 
     public var body: some View {
         VStack(spacing: 0) {
@@ -126,7 +131,12 @@ private extension DnsView {
                 customDnsInstructionsAndList()
             }
         )
+        .onHover { newValue in
+            isCustomDnsHovered = newValue
+        }
     }
+
+    private static let dnsEntryHeight: CGFloat = 44
 
     func customDnsInstructionsAndList() -> some View {
         VStack {
@@ -136,8 +146,95 @@ private extension DnsView {
                     .foregroundStyle(NymColor.gray1)
                 Spacer()
             }
+            .padding(.horizontal, 16)
+
+            List {
+                ForEach(viewModel.ipAddresses, id: \.self) { ip in
+                    HStack {
+                        HStack {
+                            GenericImage(imageName: "dragIndicator")
+                                .frame(width: 20, height: 20)
+                                .foregroundStyle(NymColor.gray1)
+                            Text(ip)
+                        }
+                        Spacer()
+                        GenericImage(systemImageName: "trash")
+                            .frame(width: 20, height: 20)
+                            .foregroundStyle(NymColor.primary)
+                    }
+                    .frame(height: DnsView.dnsEntryHeight)
+                    .alignmentGuide(.listRowSeparatorLeading) { _ in 0 }
+                    .background(.clear)
+                }
+                .onMove { from, to in
+                    viewModel.ipAddresses.move(fromOffsets: from, toOffset: to)
+                }
+            }
+            .background(.clear)
+            .scrollContentBackground(.hidden)
+            .scrollDisabled(true)
+            .frame(maxWidth: .infinity)
+            .frame(height: DnsView.dnsEntryHeight * CGFloat(viewModel.ipAddresses.count))
+            dnsTextfield()
+        }
+        .padding(.bottom, 16)
+    }
+
+    @ViewBuilder
+    func dnsTextfield() -> some View {
+        HStack(spacing: 16) {
+            StrokeBorderView(
+                strokeTitle: "dns.textfield.title".localizedString,
+                strokeTitleLeftMargin: 60,
+                isHovered: $isCustomDnsTextFieldHovered,
+                strokeColor: NymColor.primary,
+                backgroundColor: NymColor.elevation,
+                backgroundColorHover: NymColor.elevation.opacity(0.7)
+            ) {
+                HStack {
+                    ZStack(alignment: .leading) {
+                        TextField("", text: $viewModel.ipAddressTextField)
+                            .foregroundStyle(NymColor.gray1)
+                            .textFieldStyle(PlainTextFieldStyle())
+                            .background(NymColor.elevation.opacity(isCustomDnsHovered ? 0.7 : 1))
+                            .textStyle(.Body.Large.regular)
+                            .focused($isIpAddressTextFieldFocused)
+                            .padding(.horizontal, 16)
+
+                        if viewModel.ipAddressTextField.isEmpty {
+                            Text("dns.textfield.placeholder".localizedString)
+                                .foregroundStyle(NymColor.gray1)
+                                .textStyle(.Body.Large.regular)
+                                .padding(.leading, 16)
+                        }
+                    }
+                    Spacer()
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            }
+            .onTapGesture {
+                isIpAddressTextFieldFocused = true
+            }
+
+            Text("dns.button.add".localizedString)
+                .frame(height: 56)
+                .padding(.horizontal, 16)
+                .foregroundStyle(NymColor.primary)
+                .textStyle(.Body.Large.regular)
+                .background(NymColor.elevation.opacity(isCustomDnsAddButtonHovered ? 0.7 : 1.0))
+                .cornerRadius(8)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 8)
+                        .inset(by: 0.5)
+                        .stroke(NymColor.primary.opacity(isCustomDnsAddButtonHovered ? 0.7 : 1), lineWidth: 1)
+                }
+                .onHover { newValue in
+                    isCustomDnsAddButtonHovered = newValue
+                }
+                .onTapGesture {
+                    print("Add!")
+                }
         }
         .padding(.horizontal, 16)
-        .padding(.bottom, 16)
     }
 }
--- nym-vpn-apple/Settings/Sources/Settings/Dns/DnsViewModel.swift
+++ nym-vpn-apple/Settings/Sources/Settings/Dns/DnsViewModel.swift
@@ -2,14 +2,21 @@ import SwiftUI
 import AppSettings
 
 @MainActor public final class DnsViewModel: ObservableObject {
-
     private let appSettings: AppSettings
+    let maxDnsEntries = 5
 
     @Binding private var path: NavigationPath
-    
+
     @Published var isDefaultDnsDisplayed = false
-    
     @Published var isCustomDnsEnabled = false
+    @Published var ipAddresses: [String] = [
+        "192.168.1.1",
+        "10.0.0.1",
+        "208.67.222.222",
+        "208.67.220.220",
+        "208.67.220.221"
+    ]
+    @Published var ipAddressTextField = ""
 
     @Published var isSnackbarDisplayed = false
     @Published var snackbarMessage: String?
