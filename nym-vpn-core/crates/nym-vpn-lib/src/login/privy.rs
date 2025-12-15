ba4fa7922e605811123facf5b9ac002e39f1e049
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2025-12-15 13:24:33 +0200

    Use HKDF with the same salt and info as web implementation

--- nym-vpn-core/crates/nym-vpn-lib/src/login/privy.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/login/privy.rs
@@ -1,13 +1,15 @@
 // Copyright 2025 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use nym_crypto::Digest;
+use nym_crypto::hkdf;
 use nym_validator_client::nyxd::bip32::secp256k1::sha2::Sha256;
 use nym_vpn_store::account::Mnemonic;
 
 use super::error::LoginError;
 
 pub const PRIVY_DERIVATION_MESSAGE: &str = "DeriveAccount:NymVPN";
+const HKDF_SALT: &str = "privy-bip44-derivation";
+const HKDF_INFO: &str = "cosmos-entropy";
 
 pub fn message_to_sign() -> String {
     hex::encode(PRIVY_DERIVATION_MESSAGE.as_bytes())
@@ -16,11 +18,15 @@ pub fn message_to_sign() -> String {
 pub fn hex_signature_to_mnemonic(hex_signature: &str) -> Result<Mnemonic, LoginError> {
     let bytes_signature = hex::decode(hex_signature)?;
 
-    let mut hasher = Sha256::new();
-    hasher.update(&bytes_signature);
-    let hashed_signature = hasher.finalize();
+    let entropy = hkdf::extract_then_expand::<Sha256>(
+        Some(HKDF_SALT.as_bytes()),
+        &bytes_signature,
+        Some(HKDF_INFO.as_bytes()),
+        32,
+    )
+    .map_err(|_| LoginError::HkdfInvalidLength)?;
 
-    let mnemonic = Mnemonic::from_entropy(&hashed_signature)?;
+    let mnemonic = Mnemonic::from_entropy(&entropy)?;
 
     Ok(mnemonic)
 }
