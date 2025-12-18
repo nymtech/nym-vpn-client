196e34e8fc9a227f42579bfde8128a4301e03105
Andrii <sirakandrew@gmail.com>
2025-12-18 14:53:22 +0100

    Merge settings screen

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/SettingsViewModel.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/settings/SettingsViewModel.kt
@@ -12,7 +12,6 @@ import kotlinx.coroutines.launch
 import net.nymtech.nymvpn.data.SettingsRepository
 import net.nymtech.nymvpn.di.qualifiers.ApplicationScope
 import net.nymtech.nymvpn.manager.backend.BackendManager
-import net.nymtech.nymvpn.manager.environment.EnvironmentManager
 import net.nymtech.vpn.backend.Tunnel
 import timber.log.Timber
 import javax.inject.Inject
