e1aaf6471eee2bbbc7e33bcde33e0124d4073152
mrkiwi <sirakandrew@gmail.com>
2025-12-19 12:05:42 +0100

    ktlint

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/util/extensions/UiExtensions.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/util/extensions/UiExtensions.kt
@@ -232,7 +232,7 @@ fun Score.colorPerformance(): Color {
 
 @Composable
 fun Score.displayText(): String {
-	return when(this) {
+	return when (this) {
 		Score.HIGH -> stringResource(R.string.score_text_high)
 		Score.MEDIUM -> stringResource(R.string.score_text_medium)
 		Score.LOW -> stringResource(R.string.score_text_low)
