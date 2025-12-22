ee605d16fa44de605de090c91913dfdf5de1d551
agentpietrucha <agentpietrucha@gmail.com>
2025-12-22 20:16:03 +0100

    Put PrivyProvider under privy feature flag

--- nym-vpn-app/src/PrivyProvider.tsx
+++ nym-vpn-app/src/PrivyProvider.tsx
@@ -3,31 +3,35 @@ import { PrivyProvider as PrivyProviderComponent } from '@privy-io/react-auth';
 import { useMainState } from './contexts';
 
 export const PrivyProvider = ({ children }: { children: React.ReactNode }) => {
-  const { uiTheme } = useMainState();
+  const { uiTheme, backendFlags } = useMainState();
 
-  return (
-    <PrivyProviderComponent
-      appId={import.meta.env.VITE_PRIVY_APP_ID}
-      config={{
-        loginMethods: ['google', 'twitter', 'github'],
-        embeddedWallets: {
-          ethereum: {
-            createOnLogin: 'all-users',
+  if (backendFlags.privy) {
+    return (
+      <PrivyProviderComponent
+        appId={import.meta.env.VITE_PRIVY_APP_ID}
+        config={{
+          loginMethods: ['google', 'twitter', 'github'],
+          embeddedWallets: {
+            ethereum: {
+              createOnLogin: 'all-users',
+            },
           },
-        },
-        appearance: {
-          theme: uiTheme,
-          logo: '/icon.svg',
-          accentColor: '#14e76f',
-          walletChainType: 'ethereum-only',
-        },
-        intl: {
-          defaultCountry: 'US',
-        },
-        supportedChains: [mainnet],
-      }}
-    >
-      {children}
-    </PrivyProviderComponent>
-  );
+          appearance: {
+            theme: uiTheme,
+            logo: '/icon.svg',
+            accentColor: '#14e76f',
+            walletChainType: 'ethereum-only',
+          },
+          intl: {
+            defaultCountry: 'US',
+          },
+          supportedChains: [mainnet],
+        }}
+      >
+        {children}
+      </PrivyProviderComponent>
+    );
+  }
+
+  return children;
 };
