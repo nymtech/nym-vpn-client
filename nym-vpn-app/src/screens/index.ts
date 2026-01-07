94d61ce9acaeabeb23761f0fcae8328e2ad27f8b
agentpietrucha <agentpietrucha@gmail.com>
2026-01-07 15:45:02 +0100

    Fix lint issues

--- nym-vpn-app/src/screens/index.ts
+++ nym-vpn-app/src/screens/index.ts
@@ -1,13 +1,12 @@
 export { default as IntroSplash } from './IntroSplash';
-// export { default as Onboarding } from './onboarding';
 export { default as IntroAnim } from './IntroAnim';
 export { default as MainLayout } from './MainLayout';
 export { default as Error } from './Error';
 export { default as StartupError } from './StartupError';
 export { default as Welcome } from './Welcome';
 
-export * from './account';
 export * from './onboarding';
+export * from './account';
 export * from './settings';
 export * from './node';
 export * from './login';
