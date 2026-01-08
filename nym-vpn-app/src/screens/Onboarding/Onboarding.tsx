c00a31249351911ba2c3adf8b08820a473b59574
agentpietrucha <agentpietrucha@gmail.com>
2026-01-08 18:05:34 +0100

    Update onboarding screen details

--- nym-vpn-app/src/screens/Onboarding/Onboarding.tsx
+++ nym-vpn-app/src/screens/Onboarding/Onboarding.tsx
@@ -1,8 +1,9 @@
 import clsx from 'clsx';
 import { useNavigate } from 'react-router';
 import { useTranslation } from 'react-i18next';
+import { Button as HuButton } from '@headlessui/react';
 import useEmblaCarousel from 'embla-carousel-react';
-import { Button, PageAnim } from '../../ui';
+import { Button, MsIcon, PageAnim } from '../../ui';
 import { NymVpnTextLogo } from '../../assets';
 import { routes } from '../../router';
 import { useMainDispatch } from '../../contexts/main';
@@ -12,6 +13,34 @@ import { Speed, Tracking, Welcome, ZeroKnowledge } from './slides';
 import { DotButton, useDotButton } from './CarouselDotButton';
 
 const slides = [Welcome, Speed, Tracking, ZeroKnowledge];
+
+const ArrowButton = ({
+  icon,
+  onClick,
+  disabled,
+}: {
+  icon: string;
+  onClick: () => void;
+  disabled: boolean;
+}) => {
+  return (
+    <HuButton
+      onClick={onClick}
+      className={clsx(
+        'h-11 w-11 my-2 mr-2 flex items-center justify-center rounded-full bg-mercury hover:bg-bombay dark:bg-mine-shaft dark:hover:bg-charcoal',
+      )}
+    >
+      <MsIcon
+        icon={icon}
+        className={clsx(
+          !disabled
+            ? 'text-baltic-sea dark:text-white leading-none'
+            : 'text-bombay',
+        )}
+      />
+    </HuButton>
+  );
+};
 
 function Onboarding() {
   const dispatch = useMainDispatch() as StateDispatch;
@@ -51,17 +80,29 @@ function Onboarding() {
               ))}
             </div>
           </div>
-          <div className="flex flex-row gap-2 bg-mercury dark:bg-mine-shaft px-3 py-2 rounded-2xl">
-            {scrollSnaps.map((_, index) => (
-              <DotButton
-                key={index}
-                onClick={() => onDotButtonClick(index)}
-                className={clsx(
-                  'flex items-center justify-center rounded-full cursor-pointer bg-bombay dark:bg-charcoal border-0 p-0 m-0 w-2 h-2 appearance-none tap-highlight-transparent no-underline touch-manipulation',
-                  index === selectedIndex ? ' bg-white dark:bg-white' : '',
-                )}
-              />
-            ))}
+          <div className="w-full flex flex-row justify-between items-center">
+            <ArrowButton
+              icon="arrow_left"
+              onClick={() => emblaApi?.scrollPrev()}
+              disabled={!emblaApi?.canScrollPrev()}
+            />
+            <div className="flex flex-row gap-2 bg-mercury dark:bg-mine-shaft px-3 py-2 rounded-2xl">
+              {scrollSnaps.map((_, index) => (
+                <DotButton
+                  key={index}
+                  onClick={() => onDotButtonClick(index)}
+                  className={clsx(
+                    'flex items-center justify-center rounded-full cursor-pointer bg-bombay dark:bg-charcoal border-0 p-0 m-0 w-2 h-2 appearance-none tap-highlight-transparent no-underline touch-manipulation',
+                    index === selectedIndex ? ' bg-white dark:bg-white' : '',
+                  )}
+                />
+              ))}
+            </div>
+            <ArrowButton
+              icon="arrow_right"
+              onClick={() => emblaApi?.scrollNext()}
+              disabled={!emblaApi?.canScrollNext()}
+            />
           </div>
         </div>
 
