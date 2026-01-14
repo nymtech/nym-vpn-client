159ce9c67e45053047a72d35a6ab0ebde740e2a8
Andy Duplain <trojanfoe@gmail.com>
2026-01-14 09:48:24 +0000

    VPN-4615: Replace "mnemonic" and "access code" by "passphrase". (#4376)
    * VPN-4615: Replace "mnemonic" and "access code" by "passphrase".

--- nym-vpn-app/src-tauri/src/vpnd/client.rs
+++ nym-vpn-app/src-tauri/src/vpnd/client.rs
@@ -431,7 +431,7 @@ impl VpndClient {
             },
             _ => {
                 return Err(VpndError::Response(BackendError::internal(
-                    "either mnemonic or signature must be provided",
+                    "either passphrase or signature must be provided",
                     None,
                 )));
             }
