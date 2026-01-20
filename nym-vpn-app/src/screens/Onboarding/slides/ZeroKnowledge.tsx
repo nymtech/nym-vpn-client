fcbf28743c7d854e100ee37433215ea338dfde39
agentpietrucha <agentpietrucha@gmail.com>
2026-01-20 16:29:48 +0100

    Decrease spacing and icons size

--- nym-vpn-app/src/screens/Onboarding/slides/ZeroKnowledge.tsx
+++ nym-vpn-app/src/screens/Onboarding/slides/ZeroKnowledge.tsx
@@ -5,7 +5,7 @@ function ZeroKnowledge() {
   const { t } = useTranslation('onboarding');
   return (
     <div className="flex flex-col items-center gap-4">
-      <ZeroKnowledgeAsset className="h-full max-h-72 w-fit" />
+      <ZeroKnowledgeAsset className="h-full max-h-64 w-fit" />
       <h1 className="text-2xl text-baltic-sea dark:text-white">
         {t('zero-knowledge.title')}
       </h1>
