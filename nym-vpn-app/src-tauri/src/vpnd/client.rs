746a5683c971de5d6e515b6d8c35d9e67e8842fd
agentpietrucha <agentpietrucha@gmail.com>
2026-01-22 16:22:56 +0100

    Run cargo fmt

--- nym-vpn-app/src-tauri/src/vpnd/client.rs
+++ nym-vpn-app/src-tauri/src/vpnd/client.rs
@@ -819,7 +819,7 @@ impl VpndClient {
     #[instrument(skip_all)]
     pub async fn set_enable_lewes_protocol(&self, enabled: bool) -> Result<(), VpndError> {
         let mut vpnd = self.vpnd().await?;
-        
+
         vpnd.set_enable_lewes_protocol(enabled)
             .or_else(async |e| self.handle_rpc_error("set_enable_lewes_protocol", e).await)
             .await
