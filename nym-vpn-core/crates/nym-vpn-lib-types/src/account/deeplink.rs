03316ba91c92bda0c949d3bb72eca58ff369e151
Andy Duplain <trojanfoe@gmail.com>
2026-01-22 11:04:57 +0000

    Fix broken `develop` branch. (#4448)
    * Fix broken `develop` branch.

--- nym-vpn-core/crates/nym-vpn-lib-types/src/account/deeplink.rs
+++ nym-vpn-core/crates/nym-vpn-lib-types/src/account/deeplink.rs
@@ -1,4 +1,9 @@
+// Copyright 2026 - Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
+#[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+
 use std::str::FromStr;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
