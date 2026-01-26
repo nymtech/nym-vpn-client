3ebec1f230873ab6c1a9e9b6d958f8be2765aaae
Andrii <sirakandrew@gmail.com>
2026-01-26 10:54:21 +0100

    [Android] Fix navigation padding on Select Plan (#4472)
    * Fix navigation padding on Select Plan
    
    * CHANGELOG.md

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/account/plan/SelectPlanScreen.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/account/plan/SelectPlanScreen.kt
@@ -39,10 +39,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -128,7 +130,8 @@ fun SelectPlanScreen(
 			.fillMaxSize()
 			.imePadding()
 			.background(MaterialTheme.colorScheme.background)
-			.padding(horizontal = 24.dp.scaledWidth()),
+			.padding(horizontal = 24.dp.scaledWidth())
+			.padding(WindowInsets.systemBars.asPaddingValues()),
 	) {
 		FilledProgressBar4(
 			modifier = Modifier.padding(top = 8.dp, bottom = 24.dp),
@@ -166,9 +169,24 @@ fun SelectPlanScreen(
 			)
 
 			Column(
-				verticalArrangement = Arrangement.spacedBy(24.dp, Alignment.CenterVertically),
+				verticalArrangement = Arrangement.spacedBy(18.dp, Alignment.CenterVertically),
 				modifier = Modifier.padding(horizontal = 52.dp).fillMaxWidth(),
 			) {
+				Row {
+					Icon(
+						imageVector = ImageVector.vectorResource(R.drawable.ic_gift),
+						contentDescription = null,
+						tint = MaterialTheme.colorScheme.primary,
+						modifier = Modifier.size(16.dp),
+					)
+					Spacer(Modifier.width(8.dp))
+					Text(
+						text = stringResource(R.string.select_plan_line_0),
+						style = MaterialTheme.typography.bodyMedium,
+						color = MaterialTheme.colorScheme.outline,
+						fontFamily = FontFamily(Font(R.font.lab_grotesque_regular)),
+					)
+				}
 				Row {
 					Icon(
 						painter = rememberVectorPainter(Icons.Filled.VerifiedUser),
@@ -176,7 +194,7 @@ fun SelectPlanScreen(
 						tint = MaterialTheme.colorScheme.primary,
 						modifier = Modifier.size(16.dp),
 					)
-					Spacer(Modifier.width(4.dp))
+					Spacer(Modifier.width(8.dp))
 					Text(
 						text = stringResource(R.string.select_plan_line_1),
 						style = MaterialTheme.typography.bodyMedium,
@@ -191,7 +209,7 @@ fun SelectPlanScreen(
 						tint = MaterialTheme.colorScheme.primary,
 						modifier = Modifier.size(16.dp),
 					)
-					Spacer(Modifier.width(4.dp))
+					Spacer(Modifier.width(8.dp))
 					Text(
 						text = stringResource(R.string.select_plan_line_2),
 						style = MaterialTheme.typography.bodyMedium,
@@ -206,7 +224,7 @@ fun SelectPlanScreen(
 						tint = MaterialTheme.colorScheme.primary,
 						modifier = Modifier.size(16.dp),
 					)
-					Spacer(Modifier.width(6.dp))
+					Spacer(Modifier.width(8.dp))
 					Text(
 						text = stringResource(R.string.select_plan_line_3),
 						style = MaterialTheme.typography.bodyMedium,
