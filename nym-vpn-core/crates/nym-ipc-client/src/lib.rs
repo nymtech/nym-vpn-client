15d770ac2bdfd6f43eee46d87927e80ec731b1b3
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2026-02-02 15:21:27 +0200

    Propagate authentication failure to tauri app

--- nym-vpn-core/crates/nym-ipc-client/src/lib.rs
+++ nym-vpn-core/crates/nym-ipc-client/src/lib.rs
@@ -1,6 +1,9 @@
 // Copyright 2026 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
+#[cfg(target_os = "linux")]
+pub mod authentication;
+
 use std::{io::Result, path::PathBuf};
 
 use hyper_util::rt::TokioIo;
@@ -9,7 +12,18 @@ use hyper_util::rt::TokioIo;
 #[cfg(windows)]
 const PIPE_AVAILABILITY_TIMEOUT: std::time::Duration = std::time::Duration::from_secs(5);
 
-#[cfg(unix)]
+#[cfg(target_os = "linux")]
+pub async fn connect(socket_path: PathBuf) -> Result<TokioIo<tokio::net::UnixStream>> {
+    let mut conn = tokio::net::UnixStream::connect(socket_path).await?;
+    let auth_res = authentication::AuthenticaticationResult::recv(&mut conn).await;
+    if auth_res.accepted() {
+        Ok(TokioIo::new(conn))
+    } else {
+        Err(std::io::ErrorKind::PermissionDenied.into())
+    }
+}
+
+#[cfg(target_os = "macos")]
 pub async fn connect(socket_path: PathBuf) -> Result<TokioIo<tokio::net::UnixStream>> {
     Ok(TokioIo::new(
         tokio::net::UnixStream::connect(socket_path).await?,
