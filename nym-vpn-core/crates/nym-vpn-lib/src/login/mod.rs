564d7d4e5be8e8b85d67c98657337c46468ccc3e
Andy Duplain <trojanfoe@gmail.com>
2026-02-02 15:28:39 +0000

    Fix unit test.

--- nym-vpn-core/crates/nym-vpn-lib/src/login/mod.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/login/mod.rs
@@ -55,15 +55,19 @@ mod tests {
     }
 
     #[test]
-    fn parse_hex_signature() {
-        let hex_signature = String::from(
-            "a564a87ccbed5cb5be4929201e555f5b5e26cb01d300d621520d724e57c582c33fa374caf21fd0c5e3118d70d14894845a32acfee47da7f347a0b9a57cba07931c",
-        );
+    fn parse_privy_mnemonic() {
+        let mnemonic = Mnemonic::generate(24).unwrap();
+        let parsed_mnemonic = parse_account_request(&StoreAccountRequest::Privy {
+            mnemonic: mnemonic.to_string(),
+        })
+        .unwrap();
+        assert_eq!(mnemonic, parsed_mnemonic);
+
         assert!(
             parse_account_request(&StoreAccountRequest::Privy {
-                mnemonic: hex_signature
+                mnemonic: String::from("invalid mnemonic")
             })
-            .is_ok()
+            .is_err()
         );
     }
 }
