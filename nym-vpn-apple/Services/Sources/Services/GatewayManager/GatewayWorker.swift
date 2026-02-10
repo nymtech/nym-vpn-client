6c6188bea0f90ce8a695d76fdf21c568f6db5739
Andrej Mihajlov <andrej@nymtech.net>
2026-02-10 09:46:52 +0100

    ios: reset gateway cache when changing environment, hold NymGatewayCache in memory to benefit from caching (#4597)

--- nym-vpn-apple/Services/Sources/Services/GatewayManager/GatewayWorker.swift
+++ nym-vpn-apple/Services/Sources/Services/GatewayManager/GatewayWorker.swift
@@ -7,12 +7,30 @@ import NymVPNLib
 #elseif os(macOS)
 import GRPCManager
 #endif
+import Logging
+
+#if os(iOS)
+enum State {
+    /// Actor is initialized but does not hold gateway cache
+    case initial
+
+    /// Actor is loading gateway cache
+    case loading(Task<NymGatewayCache, Error>)
+
+    /// Actor is ready, gateway cache is initialized
+    case ready(NymGatewayCache)
+}
+#endif
 
 actor GatewayWorker {
     let appSettings: AppSettings
     let configurationManager: ConfigurationManager
+    private let logger = Logger(label: "GatewayWorker")
 #if os(macOS)
     let grpcManager: GRPCManager
+#endif
+#if os(iOS)
+    var state = State.initial
 #endif
 
 #if os(iOS)
@@ -29,29 +47,73 @@ actor GatewayWorker {
 #endif
 
 #if os(iOS)
+
+    /// Reset internal gateway cache in order for it to be re-initialized during the next attempt to fetch gateways
+    /// Use that only on environment changes because that wipes out in-memory gateway cache
+    func reset() async {
+        switch state {
+        case .initial:
+            break
+
+        case let .loading(task):
+            task.cancel()
+            state = .initial
+
+        case .ready:
+            state = .initial
+        }
+    }
+
     func fetchGateways() async throws -> (entry: [GatewayNode], exit: [GatewayNode], vpn: [GatewayNode]) {
-        let result = try await Task { @MainActor in
-            let offlineMonitor = await NymOfflineMonitor()
-            let gatewayCache = try await NymGatewayCache(
-                userAgent: .appUserAgent,
-                environment: configurationManager.networkEnv,
-                offlineMonitor: offlineMonitor
-            )
+        let gatewayCache: NymGatewayCache
 
-            let entryNodes = try await gatewayCache.getGateways(gwType: .mixnetEntry)
-            let exitNodes = try await gatewayCache.getGateways(gwType: .mixnetExit)
-            let vpnNodes = try await gatewayCache.getGateways(gwType: .wg)
+        switch state {
+        case .initial:
+            let task = Task {
+                let networkName = await configurationManager.networkEnv.networkName()
+                logger.info("Setup gateway cache with \(networkName)")
 
-            let entry = entryNodes.map { GatewayNode(with: $0) }
-            let exit = exitNodes.map { GatewayNode(with: $0) }
-            let vpn = vpnNodes.map { GatewayNode(with: $0) }
+                let offlineMonitor = await NymOfflineMonitor()
+                let gatewayCache = try await NymGatewayCache(
+                    userAgent: .appUserAgent,
+                    environment: configurationManager.networkEnv,
+                    offlineMonitor: offlineMonitor
+                )
 
-            await gatewayCache.shutdownAndWait()
+                try Task.checkCancellation()
 
-            return (entry, exit, vpn)
-        }.value
+                return gatewayCache
+            }
 
-        return result
+            state = .loading(task)
+
+            do {
+                gatewayCache = try await task.value
+                state = .ready(gatewayCache)
+            } catch {
+                state = .initial
+                if !(error is CancellationError) {
+                    logger.error("Failed to initialize gateway cache: \(error)")
+                }
+                throw error
+            }
+
+        case let .loading(task):
+            gatewayCache = try await task.value
+
+        case let .ready(gwCache):
+            gatewayCache = gwCache
+        }
+
+        let entryNodes = try await gatewayCache.getGateways(gwType: .mixnetEntry)
+        let exitNodes = try await gatewayCache.getGateways(gwType: .mixnetExit)
+        let vpnNodes = try await gatewayCache.getGateways(gwType: .wg)
+
+        let entry = entryNodes.map { GatewayNode(with: $0) }
+        let exit = exitNodes.map { GatewayNode(with: $0) }
+        let vpn = vpnNodes.map { GatewayNode(with: $0) }
+
+        return (entry, exit, vpn)
     }
 #elseif os(macOS)
     func fetchGateways() async throws -> (entry: [GatewayNode], exit: [GatewayNode], vpn: [GatewayNode]) {
