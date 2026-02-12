3c39f46a4c29dfa48b6a8e29069b75cc94335450
Karol Sawicki <agentpietrucha@gmail.com>
2026-02-12 19:55:36 +0100

    tauri: Use core MixnetTrafficDefaults (#4627)

--- nym-vpn-app/src/screens/settings/mixnet-tuning/context/index.ts
+++ nym-vpn-app/src/screens/settings/mixnet-tuning/context/index.ts
@@ -1,3 +1,2 @@
 export { default as MixnetTrafficConfigProvider } from './provider';
 export { useMixnetTrafficConfig } from './context';
-export { reducer, DEFAULT_MIXNET_TRAFFIC_CONFIG } from './reducer';
