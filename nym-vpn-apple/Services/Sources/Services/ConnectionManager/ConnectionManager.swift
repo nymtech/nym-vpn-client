c191bbc57d59b5e9771901a2bf717eec2cf9dd74
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2026-02-18 10:04:50 +0200

    APPLE: fix do not reconnect if same gateway is selected as connected

--- nym-vpn-apple/Services/Sources/Services/ConnectionManager/ConnectionManager.swift
+++ nym-vpn-apple/Services/Sources/Services/ConnectionManager/ConnectionManager.swift
@@ -285,11 +285,36 @@ extension ConnectionManager {
         guard let oldConfig else { return true }
         guard oldConfig != connectionStorage.connectionConfig else { return false }
 
-        if connectionStorage.connectionConfig .enableTwoHop == true,
-           oldConfig.mixnetTuningConfig != connectionStorage.connectionConfig.mixnetTuningConfig {
+        guard shouldReconnectMixnetTunningSettings(with: oldConfig),
+              shouldEntryReconnect() || shouldExitRecconnect()
+        else {
             return false
-        } else {
+        }
+        return true
+    }
+
+    func shouldReconnectMixnetTunningSettings(with oldConfig: ConnectionConfig) -> Bool {
+        guard connectionStorage.connectionConfig.enableTwoHop == true,
+              oldConfig.mixnetTuningConfig == connectionStorage.connectionConfig.mixnetTuningConfig
+        else {
+            return false
+        }
+        return true
+    }
+
+    func shouldEntryReconnect() -> Bool {
+        guard connectionStorage.connectionConfig.entry.gatewayId == connectionInfoData?.entryGatewayId
+        else {
             return true
         }
+        return false
+    }
+
+    func shouldExitRecconnect() -> Bool {
+        guard connectionStorage.connectionConfig.exit.gatewayId == connectionInfoData?.exitGatewayId
+        else {
+            return true
+        }
+        return false
     }
 }
