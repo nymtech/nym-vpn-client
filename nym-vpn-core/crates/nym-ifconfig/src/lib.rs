4d988c82dbdd753946330049061a78db0d028e37
Andrej Mihajlov <andrej@nymtech.net>
2026-02-20 18:19:57 +0100

    linux: fix

--- nym-vpn-core/crates/nym-ifconfig/src/lib.rs
+++ nym-vpn-core/crates/nym-ifconfig/src/lib.rs
@@ -6,7 +6,7 @@
 #[cfg(any(target_os = "macos", target_os = "ios"))]
 mod apple;
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "android"))]
 mod linux;
 
 #[cfg(target_os = "macos")]
