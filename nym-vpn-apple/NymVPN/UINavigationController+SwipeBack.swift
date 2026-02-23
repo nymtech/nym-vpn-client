a97304ef21976228d4bdf0a8928b84608f549713
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2026-02-23 22:40:24 +0200

    APPLE: add iOS swipe back gesture (#4717)

---
+++ nym-vpn-apple/NymVPN/UINavigationController+SwipeBack.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        viewControllers.count > 1
+    }
+
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        viewControllers.count > 1
+    }
+}
