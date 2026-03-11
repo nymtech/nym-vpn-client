e13a63813bb23bbd05fdfe7ed40945539ca62f07
mrkiwi <sirakandrew@gmail.com>
2026-03-11 10:46:58 +0100

    Revert free trial changes

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/account/plan/SelectPlanScreen.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/account/plan/SelectPlanScreen.kt
@@ -37,10 +37,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
@@ -167,21 +169,21 @@ fun SelectPlanScreen(
 				verticalArrangement = Arrangement.spacedBy(18.dp, Alignment.CenterVertically),
 				modifier = Modifier.padding(horizontal = 52.dp).fillMaxWidth(),
 			) {
-// 				Row {
-// 					Icon(
-// 						imageVector = ImageVector.vectorResource(R.drawable.ic_gift),
-// 						contentDescription = null,
-// 						tint = MaterialTheme.colorScheme.primary,
-// 						modifier = Modifier.size(16.dp),
-// 					)
-// 					Spacer(Modifier.width(8.dp))
-// 					Text(
-// 						text = stringResource(R.string.select_plan_line_0),
-// 						style = MaterialTheme.typography.bodyMedium,
-// 						color = MaterialTheme.colorScheme.outline,
-// 						fontFamily = FontFamily(Font(R.font.lab_grotesque_regular)),
-// 					)
-// 				}
+				Row {
+					Icon(
+						imageVector = ImageVector.vectorResource(R.drawable.ic_gift),
+						contentDescription = null,
+						tint = MaterialTheme.colorScheme.primary,
+						modifier = Modifier.size(16.dp),
+					)
+					Spacer(Modifier.width(8.dp))
+					Text(
+						text = stringResource(R.string.select_plan_line_0),
+						style = MaterialTheme.typography.bodyMedium,
+						color = MaterialTheme.colorScheme.outline,
+						fontFamily = FontFamily(Font(R.font.lab_grotesque_regular)),
+					)
+				}
 				Row {
 					Icon(
 						painter = rememberVectorPainter(Icons.Filled.VerifiedUser),
