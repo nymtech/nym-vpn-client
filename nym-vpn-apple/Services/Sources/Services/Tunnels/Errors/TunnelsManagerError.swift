1ddaed35b8471a20e745d66878a48e2e0372af72
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2026-03-13 10:47:35 +0200

    APPLE: improve not authorised tunnel error message (#4866)
    (cherry picked from commit fdf8a3e5077d9502ed8f421adb5f65a339d8dfc0)

--- nym-vpn-apple/Services/Sources/Services/Tunnels/Errors/TunnelsManagerError.swift
+++ nym-vpn-apple/Services/Sources/Services/Tunnels/Errors/TunnelsManagerError.swift
@@ -1,10 +1,37 @@
 import Foundation
+import NetworkExtension
 
-public enum TunnelsManagerError: Error {
+public enum TunnelsManagerError: LocalizedError {
     case emptyName
     case alreadyExists
     case tunnelList(error: Error)
     case addTunnel(error: Error)
     case modifyTunnel(error: Error)
     case removeTunnel(error: Error)
+
+    public var errorDescription: String? {
+        switch self {
+        case .addTunnel(let error) where Self.isVPNPermissionDenied(error):
+            String(
+                localized: "error.vpnPermissionDenied",
+                defaultValue: "VPN permission was not granted. Please allow the VPN configuration to connect.",
+                bundle: .main
+            )
+        case .addTunnel(let error), .tunnelList(let error), .modifyTunnel(let error), .removeTunnel(let error):
+            error.localizedDescription
+        case .emptyName, .alreadyExists:
+            String(
+                localized: "error.unexpected",
+                defaultValue: "An unexpected error occurred.",
+                bundle: .main
+            )
+        }
+    }
+
+    private static func isVPNPermissionDenied(_ error: Error) -> Bool {
+        let nsError = error as NSError
+        // User denied/cancelled VPN profile installation
+        return nsError.domain == NEVPNErrorDomain
+            && nsError.code == NEVPNError.configurationReadWriteFailed.rawValue
+    }
 }
