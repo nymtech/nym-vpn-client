2a9df773da86c8a595d7910dd755f66555dc164c
dynco-nym <173912580+dynco-nym@users.noreply.github.com>
2026-03-20 14:08:29 +0100

    Update docs (#4930)

--- nym-vpn-core/crates/test/scripts/install_deps_linux.sh
+++ nym-vpn-core/crates/test/scripts/install_deps_linux.sh
@@ -6,7 +6,24 @@
 set -eu
 
 # requirements for the host system running test-manager
-sudo apt update && sudo apt install -y \
-    git build-essential pkg-config libpcap-dev libssl-dev protobuf-compiler rootlesskit procps nftables wireguard dnsmasq curl
 
-sudo apt install qemu-system qemu-utils podman
+sudo apt update && sudo apt install -y pkg-config \
+    git \
+    build-essential \
+    pkg-config \
+    protobuf-compiler \
+    libpcap-dev \
+    libssl-dev \
+    libdbus-1-dev \
+    procps \
+    nftables \
+    wireguard \
+    dnsmasq \
+    curl \
+    qemu-system \
+    qemu-utils \
+    swtpm \
+    ovmf \
+    podman \
+    rootlesskit \
+    slirp4netns
