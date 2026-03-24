5d9fb0ed60bfbcf01c0d8bac5c25370c54cf3d3c
Andy Duplain <trojanfoe@gmail.com>
2026-03-24 10:35:15 +0000

    Check file exists before signing.

--- nym-vpn-app/src-tauri/scripts/sign.sh
+++ nym-vpn-app/src-tauri/scripts/sign.sh
@@ -5,19 +5,27 @@ SSL_COM_CREDENTIAL_ID=$4
 SSL_COM_TOTP_SECRET=$5
 CODE_SIGNING_TOOL="/c/actions-runner/_work/nym-vpn-client/nym-vpn-client/nym-vpn-app/src-tauri/CodeSignTool.sh"
 
+echo "Code signing in directory $(pwd)"
+
 for exe in nym-vpnd.exe nymvpn-split-tunnel.sys $NYM_VPN_EXE_PATH; do
-	program="${exe%.*}"
+	if [ ! -f "$exe" ]; then
+		echo "Cannot code sign $exe: file does not exist" >&2
+		exit 1
+	fi
+
+	echo "Code signing $exe"
+
 	if ! $CODE_SIGNING_TOOL \
 		sign \
 		-username $SSL_COM_USERNAME \
 		-password $SSL_COM_PASSWORD \
 		-credential_id $SSL_COM_CREDENTIAL_ID \
 		-totp_secret $SSL_COM_TOTP_SECRET \
-		-program_name $program \
+		-program_name "${exe%.*}" \
 		-input_file_path $exe \
 		-override; then
 			echo "Failed to sign $exe" >&2
-			exit 1
+			exit 2
 		fi
 done
 
