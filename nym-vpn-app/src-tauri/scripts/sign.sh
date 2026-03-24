0767688b443d30c5aeb9d743008fc97db9333191
Andy Duplain <trojanfoe@gmail.com>
2026-03-24 11:38:53 +0000

    We are over-signing.

--- nym-vpn-app/src-tauri/scripts/sign.sh
+++ nym-vpn-app/src-tauri/scripts/sign.sh
@@ -1,9 +1,17 @@
+#!/bin/bash
+#
+# This custom code signing script is used to sign more than just the bundle (NymVPN.exe),
+# but when it is used to sign that, it will also sign the daemon and the split-tunnel driver
+# artifacts.
+#
 # This assumes the cwd is src-tauri/.
+#
 
-echo "Code signing in directory $(pwd)"
+echo "Code signing in directory $(pwd). Arg: $1"
 
-for exe in nym-vpnd.exe nymvpn-split-tunnel.sys target/release/NymVPN.exe; do
-	if [ ! -f "$exe" ]; then
+function sign {
+	local exe="$1"
+	if [[ ! -f "$exe" ]]; then
 		echo "Cannot code sign $exe: file does not exist" >&2
 		exit 1
 	fi
@@ -22,6 +30,17 @@ for exe in nym-vpnd.exe nymvpn-split-tunnel.sys target/release/NymVPN.exe; do
 			echo "Failed to sign $exe" >&2
 			exit 2
 		fi
-done
+	fi
+}
+
+exe=$1
+
+# If the exe is NymVPN.exe then sign the daemon and split-tunnel driver first
+if [[ "$exe" == "*NymVPN.exe" ]]; then
+	sign "nym-vpnd.exe"
+	sign "nymvpn-split-tunnel.sys"
+fi
+
+sign "$exe"
 
 exit 0
