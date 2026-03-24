d0b54ddb1e1db059da2389ea6ef5eac4a2567bda
Andy Duplain <trojanfoe@gmail.com>
2026-03-24 14:22:20 +0000

    NYM-915: Sign Windows Split Tunnel Driver. (#4948)

--- nym-vpn-app/src-tauri/scripts/sign.sh
+++ nym-vpn-app/src-tauri/scripts/sign.sh
@@ -1,32 +1,52 @@
-NYM_VPN_EXE_PATH=$1
-SSL_COM_USERNAME=$2
-SSL_COM_PASSWORD=$3
-SSL_COM_CREDENTIAL_ID=$4
-SSL_COM_TOTP_SECRET=$5
-CODE_SIGNING_TOOL="/c/actions-runner/_work/nym-vpn-client/nym-vpn-client/nym-vpn-app/src-tauri/CodeSignTool.sh"
+#!/bin/bash
+#
+# This custom code signing script is used to sign more than just the Tauri app (NymVPN.exe),
+# but when it is used to sign that, it will also sign the daemon and the split-tunnel driver
+# artifacts, ready for bundling.
+#
+# This assumes the cwd is src-tauri/.
+#
 
-if ! $CODE_SIGNING_TOOL_PATH \
-sign \
--username $SSL_COM_USERNAME \
--password $SSL_COM_PASSWORD \
--credential_id $SSL_COM_CREDENTIAL_ID \
--totp_secret $SSL_COM_TOTP_SECRET \
--program_name nym-vpnd \
--input_file_path /c/actions-runner/_work/nym-vpn-client/nym-vpn-client/nym-vpn-app/src-tauri/nym-vpnd.exe \
--override; then
-	echo "Failed to sign nym-vpnd.exe" >&2
-	exit 1
+code_sign_tool="./CodeSignTool.sh"
+
+# If CodeSignTool.sh then we aren't code-signing this build.
+if [[ ! -f "$code_sign_tool" ]]; then
+	echo "Not code signing $1"
+	exit 0
 fi
 
-if ! $CODE_SIGNING_TOOL \
-sign \
--username $SSL_COM_USERNAME \
--password $SSL_COM_PASSWORD \
--credential_id $SSL_COM_CREDENTIAL_ID \
--totp_secret $SSL_COM_TOTP_SECRET \
--program_name NymVPN \
--input_file_path $NYM_VPN_EXE_PATH \
--override; then
-	echo "Failed to sign $NYM_VPN_EXE_PATH" >&2
-	exit 1
+function sign {
+	local exe="$1"
+	if [[ ! -f "$exe" ]]; then
+		echo "Cannot code sign $exe: file does not exist" >&2
+		exit 1
+	fi
+
+	echo "Code signing $exe"
+
+	if ! "$code_sign_tool" \
+		sign \
+		-username "$SSL_COM_USERNAME" \
+		-password "$SSL_COM_PASSWORD" \
+		-credential_id "$SSL_COM_CREDENTIAL_ID" \
+		-totp_secret "$SSL_COM_TOTP_SECRET" \
+		-program_name "${exe%.*}" \
+		-input_file_path "$exe" \
+		-override; then
+			echo "Failed to sign $exe" >&2
+			exit 2
+	fi
+}
+
+exe=$1
+
+# If we are signing the Tauri app, then also sign the daemon and split-tunnel driver.
+if [[ "$exe" == *NymVPN.exe ]]; then
+	for additonal in "nym-vpnd.exe" "nymvpn-split-tunnel.sys" "nymvpn-split-tunnel.cat"; do
+		sign "$additonal"
+	done
 fi
+
+sign "$exe"
+
+exit 0
