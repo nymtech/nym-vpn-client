62902683228a09b5a3f3249c11fe537c370f8508
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2026-03-25 08:42:53 +0200

    APPLE: update localizations

--- nym-vpn-apple/UIComponents/Sources/UIComponents/Buttons/HopButton/HopType.swift
+++ nym-vpn-apple/UIComponents/Sources/UIComponents/Buttons/HopButton/HopType.swift
@@ -4,15 +4,6 @@ public enum HopType: Codable, Hashable, Sendable {
     case entry
     case exit
 
-    public var selectHopLocalizedTitle: String {
-        switch self {
-        case .entry:
-            "firstHopSelection".localizedString
-        case .exit:
-            "lastHopSelection".localizedString
-        }
-    }
-
     public var hopLocalizedTitle: String {
         switch self {
         case .entry:
