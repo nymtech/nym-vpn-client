f088c8c6be6d55c5059335b21a946915187baa1c
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2026-03-26 03:35:48 -0700

    Fix xpc read (#4985)
    * Add unit tests replicating two bug scenarios
    
    * Consume remainder of data and return
    
    This is a fix that makes the two added unit tests pass

--- nym-vpn-core/crates/nym-ipc/src/xpc/common.rs
+++ nym-vpn-core/crates/nym-ipc/src/xpc/common.rs
@@ -110,19 +110,15 @@ impl XpcConnection {
         self
     }
 
-    // Tries to fill the destination buffer and returns true if it got filled
-    // and there is not more data to be copied, and false if there's still left
-    // data to be copied
-    fn try_to_fill(&mut self, mut src: Vec<u8>, dst: &mut tokio::io::ReadBuf<'_>) -> bool {
+    // Tries to fill the destination buffer
+    fn try_to_fill(&mut self, mut src: Vec<u8>, dst: &mut tokio::io::ReadBuf<'_>) {
         if dst.remaining() >= src.len() {
             // we can consume the entire data
             dst.put_slice(&src);
-            false
         } else {
             // we have to store some of it for a later call
             self.to_be_copied = Some(src.split_off(dst.remaining()));
             dst.put_slice(&src);
-            true
         }
     }
 
@@ -147,9 +143,8 @@ impl AsyncRead for XpcConnection {
         // we check for invalidation, but we could still consume the buffered
         // data, so don't take any action on possible invalidation
         self.underlaying_conn_invalidated();
-        if let Some(to_be_copied) = self.to_be_copied.take()
-            && self.try_to_fill(to_be_copied, buf)
-        {
+        if let Some(to_be_copied) = self.to_be_copied.take() {
+            self.try_to_fill(to_be_copied, buf);
             return std::task::Poll::Ready(Ok(()));
         }
         match Pin::new(&mut self.data_stream_rx).poll_next(cx) {
@@ -210,11 +205,12 @@ impl Connected for XpcConnection {
 
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
+
     use tokio::{
-        io::{AsyncReadExt, AsyncWriteExt},
+        io::{AsyncReadExt, AsyncWriteExt, ReadBuf},
         sync::mpsc,
     };
-    use tokio_util::bytes::BytesMut;
 
     use super::*;
 
@@ -236,8 +232,13 @@ mod tests {
         assert_eq!(remote_rx.recv().await.unwrap(), data);
     }
 
+    // simulate 100 gRPC calls of 100 bytes per call, and the same 8KB buffer reused until full,
+    // at which point it gets re-created
+    // this behavior follows what was observed when running 100 calls with vpnc
+    // reproducing bug NYM-967 where the second read would hang even though the last piece of data
+    // was being written into the buffer, but Pending was being returned
     #[tokio::test]
-    async fn read_from_conn() {
+    async fn fixed_buf_multiple_big_sends() {
         let (own_tx, own_rx) = mpsc::unbounded_channel();
         let _own_interface = ConnectionInterfaceObj::new(own_tx.clone());
         let (remote_tx, _remote_rx) = mpsc::unbounded_channel();
@@ -249,23 +250,114 @@ mod tests {
         let mut own_conn =
             XpcConnection::new(remote_proxy, own_rx.into(), CancellationToken::new());
 
-        let data = vec![1, 2, 3, 4, 5];
-        own_tx.send(data.clone()).unwrap();
+        let fut = async move {
+            let data: Vec<u8> = (1u8..=100).collect();
+            let mut read_buf = [0; 8192];
+            let mut buffer = ReadBuf::new(&mut read_buf);
+            for _ in 0..100 {
+                own_tx.send(data.clone()).unwrap();
 
-        let mut buffer = BytesMut::with_capacity(2);
-        own_conn.read_buf(&mut buffer).await.unwrap();
-        assert_eq!(buffer, vec![1, 2]);
+                let remaining = buffer.remaining();
+                if remaining < 100 {
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                    buffer = ReadBuf::new(&mut read_buf);
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                } else {
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                }
+            }
+        };
+        // if a read happens correctly but the data is written without returning Ready,
+        // the read call will stall and the timeout will trigger
+        tokio::time::timeout(Duration::from_secs(1), fut)
+            .await
+            .unwrap();
+    }
 
-        // reactivate the poll
-        own_tx.send(vec![]).unwrap();
-        let mut buffer = BytesMut::with_capacity(2);
-        own_conn.read_buf(&mut buffer).await.unwrap();
-        assert_eq!(buffer, vec![3, 4]);
+    // simulate 100 gRPC calls of 50 + 50 bytes per call, and the same 8KB buffer reused until full,
+    // at which point it gets re-created
+    // this behavior follows what was observed when running 100 calls with vpnc
+    // reproducing bug NYM-967 where the third read when having buffer capacity >50 and <100 would not only take
+    // the data from the previous send (which was addressed to the current read iteration) but also data from the
+    // next iteration's write. The overlap caused corruption of data
+    #[tokio::test]
+    async fn fixed_buf_multiple_couple_sends() {
+        const READ_BUF_SIZE: usize = 8192;
+        let (own_tx, own_rx) = mpsc::unbounded_channel();
+        let _own_interface = ConnectionInterfaceObj::new(own_tx.clone());
+        let (remote_tx, _remote_rx) = mpsc::unbounded_channel();
+        let remote_proxy = unsafe {
+            Retained::cast_unchecked::<ProtocolObject<dyn NSConnectionInterface + Send + Sync>>(
+                ConnectionInterfaceObj::new(remote_tx),
+            )
+        };
+        let mut own_conn =
+            XpcConnection::new(remote_proxy, own_rx.into(), CancellationToken::new());
 
-        // reactivate the poll
-        own_tx.send(vec![]).unwrap();
-        let mut buffer = BytesMut::with_capacity(2);
-        own_conn.read_buf(&mut buffer).await.unwrap();
-        assert_eq!(buffer, vec![5]);
+        tokio::spawn(async move {
+            let data1: Vec<u8> = (1u8..=50).collect();
+            let data2: Vec<u8> = (51u8..=100).collect();
+            for _ in 0..100 {
+                own_tx.send(data1.clone()).unwrap();
+                own_tx.send(data2.clone()).unwrap();
+            }
+        })
+        .await
+        .unwrap();
+        let fut = async move {
+            let mut read_buf = [0; READ_BUF_SIZE];
+            // use a double sized buffer to more easily verify data that otherwise
+            // gets wrapped and written to the beginning of a new buffer
+            let mut expected_buf = [0u8; 2 * READ_BUF_SIZE].to_vec();
+            let mut reinited = false;
+            let mut buffer = ReadBuf::new(&mut read_buf);
+            for i in 0..100 {
+                // compute and place the 1, 2, ..., 100 bytes we expect to get
+                // in this iteration
+                for idx in 0..100 {
+                    expected_buf[100 * i + idx] = (idx % 100 + 1) as u8;
+                }
+
+                let remaining = buffer.remaining();
+                if remaining < 50 {
+                    // this branch doesn't happen in this case because 8192 % 100 = 92 > 50
+                    // but we'll just keep it around for completion
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+
+                    assert_eq!(buffer.initialized(), &expected_buf[..READ_BUF_SIZE]);
+                    read_buf = [0; READ_BUF_SIZE];
+                    buffer = ReadBuf::new(&mut read_buf);
+                    reinited = true;
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                } else if remaining < 100 {
+                    // we read the first write correctly, the second write gets partially read...
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+
+                    // we verify the first half before moving to the next wrapped partial read..
+                    assert_eq!(buffer.initialized(), &expected_buf[..READ_BUF_SIZE]);
+                    read_buf = [0; READ_BUF_SIZE];
+                    buffer = ReadBuf::new(&mut read_buf);
+                    reinited = true;
+                    // we read the remaining part of the second write
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                } else {
+                    // reads and writes have enough buffer space to be matched
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                    own_conn.read_buf(&mut buffer).await.unwrap();
+                }
+                if reinited {
+                    // the first half of the buffer has been completely verified, it's time we verify the second half
+                    assert_eq!(buffer.initialized(), &expected_buf[READ_BUF_SIZE..]);
+                } else {
+                    // we haven't reached the end of the initial 8KB buffer, so we only verify the first half
+                    assert_eq!(buffer.initialized(), &expected_buf[..READ_BUF_SIZE]);
+                }
+            }
+        };
+        // not needed in this test but good to have a timeout just in case
+        tokio::time::timeout(Duration::from_secs(1), fut)
+            .await
+            .unwrap();
     }
 }
