6408104bd4f4d26063bfb45f5bb01cea72305b83
Andrej Mihajlov <andrej@nymtech.net>
2026-04-03 22:50:16 +0200

    Add copyright notices

--- nym-vpn-core/crates/nym-cgroup/src/lib.rs
+++ nym-vpn-core/crates/nym-cgroup/src/lib.rs
@@ -1,3 +1,7 @@
+// Copyright 2016-2026 Mullvad VPN AB. All Rights Reserved.
+// Copyright 2026 Nym Technologies SA <contact@nymtech.net>
+// SPDX-License-Identifier: GPL-3.0-only
+
 #![cfg(target_os = "linux")]
 use anyhow::{Context as _, anyhow};
 use std::{ffi::OsStr, fs, os::unix::ffi::OsStrExt, path::PathBuf};
--- nym-vpn-core/crates/nym-split-tunnel/src/linux/mod.rs
+++ nym-vpn-core/crates/nym-split-tunnel/src/linux/mod.rs
@@ -1,3 +1,4 @@
+// Copyright 2016-2026 Mullvad VPN AB. All Rights Reserved.
 // Copyright 2026 Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
