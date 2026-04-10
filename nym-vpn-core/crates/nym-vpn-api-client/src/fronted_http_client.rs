39053b6a4eb4a559c0e8d049f71a5df83eddcff0
Andy Duplain <trojanfoe@gmail.com>
2026-04-10 12:21:15 +0100

    Revert "Try `non_shared()`.  Not saying it will be a permanent change."
    This reverts commit 0981264f419ab9e66d8c2e2625d2c5fd3221068e.

--- nym-vpn-core/crates/nym-vpn-api-client/src/fronted_http_client.rs
+++ nym-vpn-core/crates/nym-vpn-api-client/src/fronted_http_client.rs
@@ -28,8 +28,7 @@ pub fn fronted_http_client_builder(
 
     let mut builder = ClientBuilder::new_with_urls(urls)
         .map_err(Box::new)
-        .map_err(VpnApiClientError::CreateVpnApiClient)?
-        .non_shared(); // forces a dedicated reqwest::Client per instance
+        .map_err(VpnApiClientError::CreateVpnApiClient)?;
 
     if let Some(user_agent) = user_agent {
         builder = builder.with_user_agent(user_agent.clone());
