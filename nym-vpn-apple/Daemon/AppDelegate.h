750fef7a204c928cc443d3fc20037d89acf66828
Andrej Mihajlov <andrej@nymtech.net>
2026-04-14 13:00:59 +0200

    Put daemon into separate app to support individual provisioning profile

---
+++ nym-vpn-apple/Daemon/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  daemon
+//
+//  Created by Andrej Mihajlov on 14/04/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ nym-vpn-apple/Daemon/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  daemon
+//
+//  Created by Andrej Mihajlov on 14/04/2026.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
