c25e8f772eee19a985f07b61fae4181c5e823dc0
Andrej Mihajlov <andrej@nymtech.net>
2026-04-14 17:49:41 +0200

    Remove app specific info.plist keys

--- nym-vpn-apple/Daemon/main.m
+++ nym-vpn-apple/Daemon/main.m
@@ -8,8 +8,5 @@
 #import <Cocoa/Cocoa.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // Setup code that might create autoreleased objects goes here.
-    }
-    return NSApplicationMain(argc, argv);
+    return 0;
 }
