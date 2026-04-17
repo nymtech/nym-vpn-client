87e59a46f4e89e6bad2e934a124660ed2322769c
Matthew Watt <matt@twopoint.dev>
2026-04-16 21:22:48 -0500

    Enable UI toggle in iOS settings for ad blocking

--- nym-vpn-apple/Settings/Sources/Settings/SettingsViewModel.swift
+++ nym-vpn-apple/Settings/Sources/Settings/SettingsViewModel.swift
@@ -403,7 +403,6 @@ private extension SettingsViewModel {
                 action: {}
             )
         )
-#if os(macOS)
         let adBlockSubtitle = appSettings.isAdBlockerEnabled
         ? "settings.adblock.subtitle.on".localizedString
         : "settings.adblock.subtitle.off".localizedString
@@ -432,6 +431,7 @@ private extension SettingsViewModel {
             }
             .store(in: &cancellables)
         viewModels.append(adBlockViewModel)
+#if os(macOS)
         viewModels.append(
             SettingsListItemViewModel(
                 accessory: .toggle(
