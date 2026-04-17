21360044628a4398b0205b73eea117c554823c4a
Andy Duplain <trojanfoe@gmail.com>
2026-04-17 15:08:32 +0100

    Remove linux implementation of default interface watcher.

--- nym-vpn-core/crates/nym-socks5-proxy/src/default_addrs/mod.rs
+++ nym-vpn-core/crates/nym-socks5-proxy/src/default_addrs/mod.rs
@@ -1,8 +1,6 @@
 // Copyright 2026 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-#[cfg(target_os = "linux")]
-mod linux;
 #[cfg(target_os = "macos")]
 mod macos;
 #[cfg(target_os = "windows")]
@@ -19,5 +17,8 @@ pub async fn start_monitor() -> watch::Receiver<InterfaceAddresses> {
     return macos::start_monitor().await;
 
     #[cfg(target_os = "linux")]
-    return linux::start_monitor().await;
+    {
+        let (_, rx) = watch::channel(InterfaceAddresses::default());
+        return rx;
+    }
 }
