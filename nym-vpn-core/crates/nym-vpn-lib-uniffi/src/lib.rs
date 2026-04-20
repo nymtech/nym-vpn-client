0f5d58900ae1d8c2a9effb4eaf20d26cda7bbfd0
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2026-04-20 13:00:49 +0300

    Pass selection algorithm from uniffi

--- nym-vpn-core/crates/nym-vpn-lib-uniffi/src/lib.rs
+++ nym-vpn-core/crates/nym-vpn-lib-uniffi/src/lib.rs
@@ -201,8 +201,8 @@ use ipnetwork::{IpNetwork, Ipv4Network, Ipv6Network};
 use tokio::runtime::Runtime;
 
 use nym_vpn_lib_types::{
-    EntryPoint, ExitPoint, MixnetTrafficConfig, NetworkStatisticsConfig, PrivyDerivationMessage,
-    SplitTunnelSettings, UserAgent, VpnServiceConfig,
+    EntryPoint, ExitPoint, GatewaySelectionAlgorithm, MixnetTrafficConfig, NetworkStatisticsConfig,
+    PrivyDerivationMessage, SplitTunnelSettings, UserAgent, VpnServiceConfig,
 };
 
 #[cfg(target_os = "android")]
@@ -261,6 +261,7 @@ pub struct VPNConfig {
 
     pub mixnet_traffic: Option<MixnetTrafficConfig>,
     pub network_stats: Option<NetworkStatisticsConfig>,
+    pub gateway_selection_algorithm: GatewaySelectionAlgorithm,
     pub user_agent: UserAgent,
     #[cfg(target_os = "ios")]
     tun_provider: Arc<dyn OSTunProvider>,
@@ -295,6 +296,7 @@ impl VPNConfig {
             min_gateway_vpn_performance: None,
             mixnet_traffic: self.mixnet_traffic.clone().unwrap_or_default(),
             network_stats: self.network_stats.unwrap_or_default(),
+            gateway_selection_algorithm: self.gateway_selection_algorithm.clone(),
 
             // Not available via vpn service on mobile platforms
             split_tunnel: SplitTunnelSettings::default(),
