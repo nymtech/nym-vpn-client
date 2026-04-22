a83cde4aa574f495da56ab948fb2a07099bfe34d
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2026-04-22 15:17:42 +0300

    Fix uniffi

--- nym-vpn-core/crates/nym-vpn-lib-uniffi/src/lib.rs
+++ nym-vpn-core/crates/nym-vpn-lib-uniffi/src/lib.rs
@@ -201,8 +201,9 @@ use ipnetwork::{IpNetwork, Ipv4Network, Ipv6Network};
 use tokio::runtime::Runtime;
 
 use nym_vpn_lib_types::{
-    EntryPoint, ExitPoint, GatewaySelectionAlgorithm, MixnetTrafficConfig, NetworkStatisticsConfig,
-    PrivyDerivationMessage, SplitTunnelSettings, UserAgent, VpnServiceConfig,
+    EntryPoint, ExitPoint, GatewaySelectionAlgorithmConfig, MixnetTrafficConfig,
+    NetworkStatisticsConfig, PrivyDerivationMessage, SplitTunnelSettings, UserAgent,
+    VpnServiceConfig,
 };
 
 #[cfg(target_os = "android")]
@@ -261,7 +262,7 @@ pub struct VPNConfig {
 
     pub mixnet_traffic: Option<MixnetTrafficConfig>,
     pub network_stats: Option<NetworkStatisticsConfig>,
-    pub gateway_selection_algorithm: GatewaySelectionAlgorithm,
+    pub gateway_selection_algorithm_config: GatewaySelectionAlgorithmConfig,
     pub user_agent: UserAgent,
     #[cfg(target_os = "ios")]
     tun_provider: Arc<dyn OSTunProvider>,
@@ -296,7 +297,7 @@ impl VPNConfig {
             min_gateway_vpn_performance: None,
             mixnet_traffic: self.mixnet_traffic.clone().unwrap_or_default(),
             network_stats: self.network_stats.unwrap_or_default(),
-            gateway_selection_algorithm: self.gateway_selection_algorithm.clone(),
+            gateway_selection_algorithm_config: self.gateway_selection_algorithm_config.clone(),
 
             // Not available via vpn service on mobile platforms
             split_tunnel: SplitTunnelSettings::default(),
