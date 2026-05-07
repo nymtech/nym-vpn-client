f6cd8d85790c4f0fe061c0b4239ba3cb448eb159
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2026-05-07 16:41:26 +0300

    Feature/fix missing rpc (#5234)

--- nym-vpn-core/crates/nym-vpn-lib-types/src/gateway_selection_algorithm.rs
+++ nym-vpn-core/crates/nym-vpn-lib-types/src/gateway_selection_algorithm.rs
@@ -5,6 +5,8 @@ use std::fmt;
 
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
+#[cfg(feature = "typescript-bindings")]
+use ts_rs::TS;
 
 #[derive(Debug, Default, Copy, Clone, Eq, PartialEq)]
 #[cfg_attr(feature = "uniffi-bindings", derive(uniffi::Enum))]
