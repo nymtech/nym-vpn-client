210782cc38d7bc02635eff5e11367a671922d792
jmwample <jmwample@users.noreply.github.com>
2026-05-13 16:38:13 -0600

    remove commented out string

--- nym-vpn-core/crates/nym-vpn-lib-types/src/diagnostic.rs
+++ nym-vpn-core/crates/nym-vpn-lib-types/src/diagnostic.rs
@@ -114,7 +114,6 @@ pub struct HttpReport {
 pub struct DiagnosticEndpointResponse {
     pub status: String,
     pub url: ApiUrl,
-    // pub front_policy: String,
 }
 
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
