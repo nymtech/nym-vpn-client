cfcebe2ad53dd353d2d1eb7c4b3f5643d74f81c0
Rokas Ambrazevicius <rokasambrazevicius@gmail.com>
2026-05-26 20:24:32 +0300

    APPLE: fix Testfligh builds

--- nym-vpn-apple/Home/Sources/Servers/GatewaysView.swift
+++ nym-vpn-apple/Home/Sources/Servers/GatewaysView.swift
@@ -180,15 +180,17 @@ private extension GatewaysView {
                     algorithm: viewModel.connectionManager.connectionConfig.gatewaySelectionAlgorithmConfig.algorithm
                 )
             case .exit:
-                GatewayRandomCell(
-                    type: .exit,
-                    kind: .bestServer,
-                    path: $viewModel.path,
-                    entryGateway: entryGatewayBinding,
-                    exitRouter: exitRouterBinding,
-                    algorithm: viewModel.connectionManager.connectionConfig.gatewaySelectionAlgorithmConfig.algorithm,
-                    onTap: { viewModel.applyExitBestServerTap() }
-                )
+                if !viewModel.isFastTab {
+                    GatewayRandomCell(
+                        type: .exit,
+                        kind: .bestServer,
+                        path: $viewModel.path,
+                        entryGateway: entryGatewayBinding,
+                        exitRouter: exitRouterBinding,
+                        algorithm: viewModel.connectionManager.connectionConfig.gatewaySelectionAlgorithmConfig.algorithm,
+                        onTap: { viewModel.applyExitBestServerTap() }
+                    )
+                }
                 GatewayRandomCell(
                     type: .exit,
                     kind: .random,
