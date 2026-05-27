04bc419dc04bb62da407ed02825e84a186a31ee5
Bogdan-Ștefan Neacşu <bogdan@nymtech.net>
2026-05-27 13:33:58 +0300

    Update latest_tunnel_settings on setter

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel/gateway_provider/mod.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel/gateway_provider/mod.rs
@@ -41,7 +41,7 @@ type SelectedGatewaysStream =
 #[derive(Clone)]
 pub struct GatewayProvider<C: GatewayCache> {
     gateway_cache: C,
-    latest_tunnel_settings: TunnelSettings,
+    latest_tunnel_settings: Arc<Mutex<TunnelSettings>>,
     tunnel_settings_tx: mpsc::Sender<SelectAndSend>,
     selected_gateways_stream: SelectedGatewaysStream,
     blacklisted_entry_gateways: BlacklistedGateways,
@@ -73,7 +73,7 @@ impl<C: GatewayCache> GatewayProvider<C> {
         wg_keys_db: WireguardKeysDb,
         shutdown_token: CancellationToken,
     ) -> (Self, JoinHandle<()>) {
-        let latest_tunnel_settings = tunnel_settings.clone();
+        let latest_tunnel_settings = Arc::new(Mutex::new(tunnel_settings.clone()));
         let (tunnel_settings_tx, tunnel_settings_rx) = mpsc::channel(1);
         let blacklisted_entry_gateways = BlacklistedGateways::new();
         let (query_control_tx, query_control_rx) = mpsc::unbounded_channel();
@@ -236,7 +236,13 @@ impl<C: GatewayCache> GatewayProvider<C> {
                 .enable_geo_location,
         )
         .await;
-        *self.selected_gateways_stream.lock().await =
+
+        let (latest_tunnel_settings, selected_gateways_stream) = (
+            self.latest_tunnel_settings.lock().await,
+            self.selected_gateways_stream.lock().await,
+        );
+        *latest_tunnel_settings = tunnel_settings.clone();
+        *selected_gateways_stream =
             Self::inner_set_tunnel_settings(&self.tunnel_settings_tx, tunnel_settings).await?;
         Ok(())
     }
@@ -248,7 +254,7 @@ impl<C: GatewayCache> GatewayProvider<C> {
         self.gateway_cache.refresh_all().await.ok();
     }
 
-    pub async fn clear_blacklisted_entry_gateways(&self) {
+    pub async fn clear_blacklisted_entry_gateways(&mut self) {
         match self.blacklisted_entry_gateways.is_empty() {
             Ok(is_empty) => {
                 if !is_empty {
@@ -256,7 +262,10 @@ impl<C: GatewayCache> GatewayProvider<C> {
                     if let Err(e) = self.blacklisted_entry_gateways.clear() {
                         tracing::error!("Failed to clear blacklisted entry gateway list: {e}");
                     } else {
-                        let _ = self.set_tunnel_settings(self.latest_tunnel_settings.clone())
+                        let latest_tunnel_settings =
+                            self.latest_tunnel_settings.lock().await.clone();
+                        // Re-create gateway selection stream to reflect the cleared blacklist.
+                        let _ = self.set_tunnel_settings(latest_tunnel_settings)
                             .await
                             .inspect_err(|err| {
                                 tracing::warn!("Could not re-create gateway selection stream after blacklisting a gateway: {err:?}");
@@ -282,7 +291,9 @@ impl<C: GatewayCache> GatewayProvider<C> {
                 "Blacklisted entry gateway {} due to repeated connection failure",
                 entry_gateway_identifier
             );
-            let _ = self.set_tunnel_settings(self.latest_tunnel_settings.clone())
+            let latest_tunnel_settings = self.latest_tunnel_settings.lock().await.clone();
+            // Re-create gateway selection stream to reflect the addition to the blacklist.
+            let _ = self.set_tunnel_settings(latest_tunnel_settings)
                 .await
                 .inspect_err(|err| {
                     tracing::warn!(
