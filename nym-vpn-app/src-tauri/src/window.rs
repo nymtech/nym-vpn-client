252cd329ed734e7462050f23cae4f7fa060d7e58
agentpietrucha <agentpietrucha@gmail.com>
2026-06-01 19:41:06 +0200

    Set OS unique default window width

--- nym-vpn-app/src-tauri/src/window.rs
+++ nym-vpn-app/src-tauri/src/window.rs
@@ -80,7 +80,10 @@ impl AppWindow {
         .maximizable(false)
         .center()
         .focused(true)
-        .inner_size(328.0, 710.0)
+        .inner_size(
+            if cfg!(target_os = "windows") { 328.0 } else { 328.0 },
+            if cfg!(target_os = "windows") { 710.0 } else { 710.0 },
+        )
         .min_inner_size(250.0, 200.0)
         .max_inner_size(800.0, 1400.0)
         .initialization_script(format!("window._APP = {win_env};"))
