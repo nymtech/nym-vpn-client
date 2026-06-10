028d4e21af58e4fea67585c0687326e2bb0f57e0
Andrej Mihajlov <andrej@nymtech.net>
2026-06-10 18:54:48 +0200

    Migrate to using nym-ifconfig

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tun_ipv6.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tun_ipv6.rs
@@ -1,27 +1,40 @@
 // Copyright 2023 - Nym Technologies SA <contact@nymtech.net>
 // SPDX-License-Identifier: GPL-3.0-only
 
-use std::{io, net::Ipv6Addr, process::Command};
+use std::net::Ipv6Addr;
+
+use ipnetwork::Ipv6Network;
 
 #[cfg(target_os = "linux")]
-pub fn set_ipv6_addr(device_name: &str, ipv6_addr: Ipv6Addr) -> io::Result<()> {
-    Command::new("ip")
-        .args([
-            "-6",
-            "addr",
-            "add",
-            &ipv6_addr.to_string(),
-            "dev",
-            device_name,
-        ])
-        .output()?;
+pub async fn set_ipv6_addr(device_name: &str, ipv6_addr: Ipv6Addr) -> nym_ifconfig::Result<()> {
+    use ipnetwork::IpNetwork;
+    use nym_ifconfig::Session;
+
+    let sess = Session::new()?;
+    sess.add_address(
+        device_name,
+        IpNetwork::V6(Ipv6Network::from(ipv6_addr)),
+        None,
+    )
+    .await?;
+
     Ok(())
 }
 
 #[cfg(target_os = "macos")]
-pub fn set_ipv6_addr(device_name: &str, ipv6_addr: Ipv6Addr) -> io::Result<()> {
-    Command::new("ifconfig")
-        .args([device_name, "inet6", "add", &ipv6_addr.to_string()])
-        .output()?;
+pub async fn set_ipv6_addr(device_name: &str, ipv6_addr: Ipv6Addr) -> nym_ifconfig::Result<()> {
+    use nym_ifconfig::{AddAddressRequestV6, Ipv6AddrFlags, Ipv6AddrLifetime, Session};
+
+    let mut sess = Session::default();
+    sess.add_address(
+        device_name,
+        AddAddressRequestV6 {
+            address: Ipv6Network::from(ipv6_addr),
+            destination: None,
+            lifetime: Ipv6AddrLifetime::default(),
+            flags: Ipv6AddrFlags::IN6_IFF_NODAD,
+        },
+    )?;
+
     Ok(())
 }
--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel_monitor.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/tunnel_monitor.rs
@@ -959,7 +959,7 @@ impl TunnelMonitor {
         #[cfg(any(target_os = "ios", target_os = "android"))]
         let tun_name = {
             let tun_fd = unsafe { BorrowedFd::borrow_raw(tun_device.deref().as_raw_fd()) };
-            tun_name::get_tun_name(&tun_fd).map_err(Error::GetTunDeviceName)?
+            tun_name::get_tun_name(tun_fd).map_err(Error::GetTunDeviceName)?
         };
 
         tracing::info!("Created tun device: {}", tun_name);
@@ -1192,7 +1192,8 @@ impl TunnelMonitor {
             self.enable_ipv6().then_some(conn_data.exit.private_ipv6),
             Some(conn_data.entry.private_ipv4.into()),
             exit_tun_mtu,
-        )?;
+        )
+        .await?;
         let exit_tun_name = exit_tun
             .deref()
             .tun_name()
@@ -1370,7 +1371,8 @@ impl TunnelMonitor {
             self.enable_ipv6().then_some(conn_data.entry.private_ipv6),
             None,
             entry_mtu,
-        )?;
+        )
+        .await?;
         let entry_tun_name = entry_tun
             .deref()
             .tun_name()
@@ -1395,7 +1397,8 @@ impl TunnelMonitor {
             // todo: this needs to be able to set both destinations?
             Some(conn_data.entry.private_ipv4.into()),
             exit_mtu,
-        )?;
+        )
+        .await?;
         let exit_tun_name = exit_tun
             .deref()
             .tun_name()
@@ -1648,7 +1651,7 @@ impl TunnelMonitor {
 
         let tun_device = self.create_tun_device(packet_tunnel_settings).await?;
         let tun_fd = unsafe { BorrowedFd::borrow_raw(tun_device.deref().as_raw_fd()) };
-        let interface = tun_name::get_tun_name(&tun_fd).map_err(Error::GetTunDeviceName)?;
+        let interface = tun_name::get_tun_name(tun_fd).map_err(Error::GetTunDeviceName)?;
         let mut ips = vec![IpAddr::V4(conn_data.exit.private_ipv4)];
         if self.enable_ipv6() {
             ips.push(IpAddr::V6(conn_data.exit.private_ipv6));
@@ -1748,6 +1751,7 @@ impl TunnelMonitor {
                 .tun_name()
                 .map_err(Error::GetTunDeviceName)?;
             tun_ipv6::set_ipv6_addr(&tun_name, interface_ipv6)
+                .await
                 .map_err(Error::SetTunDeviceIpv6Addr)?;
         }
 
@@ -1774,7 +1778,7 @@ impl TunnelMonitor {
     }
 
     #[cfg(any(target_os = "linux", target_os = "macos"))]
-    fn create_wireguard_device(
+    async fn create_wireguard_device(
         interface_ipv4: Ipv4Addr,
         interface_ipv6: Option<Ipv6Addr>,
         destination: Option<IpAddr>,
@@ -1806,6 +1810,7 @@ impl TunnelMonitor {
 
         if let Some(interface_ipv6) = interface_ipv6 {
             tun_ipv6::set_ipv6_addr(&tun_name, interface_ipv6)
+                .await
                 .map_err(Error::SetTunDeviceIpv6Addr)?;
         }
 
