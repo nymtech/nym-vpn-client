49c514aeb05b4653a5cd1d81c0b78a8657fe68a6
agentpietrucha <agentpietrucha@gmail.com>
2026-06-16 15:23:08 +0200

    Fix missing `InfoBanner` color classes

--- nym-vpn-app/src/ui/InfoBanner.tsx
+++ nym-vpn-app/src/ui/InfoBanner.tsx
@@ -4,22 +4,35 @@ import MsIcon from './MsIcon';
 type InfoBannerProps = {
   text: string;
   icon: string;
-  variant: 'info' | 'warning' | 'error' | 'cornflower';
+  variant: 'info' | 'warning' | 'error';
 };
 
 function InfoBanner({ text, icon, variant }: InfoBannerProps) {
   return (
-    <div className="border-status-warning bg-status-warning/10 flex items-center gap-3 rounded-r-lg border-l-4 p-3">
+    <div
+      className={clsx('flex items-center gap-3 rounded-r-lg border-l-4 p-3', {
+        'border-status-warning bg-status-warning/10': variant === 'warning',
+        'border-status-error bg-status-error/10': variant === 'error',
+        'border-status-info bg-status-info/10': variant === 'info',
+      })}
+    >
       <MsIcon
         icon={icon}
         className={clsx('leading-none', {
           'text-status-warning': variant === 'warning',
           'text-status-error': variant === 'error',
-          'text-status-cornflower': variant === 'cornflower',
           'text-status-info': variant === 'info',
         })}
       />
-      <p className="text-status-warning text-sm">{text}</p>
+      <p
+        className={clsx('text-sm', {
+          'text-status-warning': variant === 'warning',
+          'text-status-error': variant === 'error',
+          'text-status-info': variant === 'info',
+        })}
+      >
+        {text}
+      </p>
     </div>
   );
 }
