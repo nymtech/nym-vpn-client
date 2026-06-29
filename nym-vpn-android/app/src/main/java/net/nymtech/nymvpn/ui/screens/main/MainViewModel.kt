a8d6b00851ec0c2322e01915a75c38b0cc7863b4
Andrii <sirakandrew@gmail.com>
2026-06-29 14:17:24 +0200

    [Android] Fixes for node family dialog (#5726)

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/main/MainViewModel.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/ui/screens/main/MainViewModel.kt
@@ -68,6 +68,8 @@ constructor(
 
 	private var timerJob: Job? = null
 	private var lastConnectedAt: Long? = null
+	private var pendingNodeFamiliesConfirmAction: (suspend () -> Unit)? = null
+	private var nodeFamiliesEventHandled = false
 
 	init {
 		viewModelScope.launch {
@@ -84,7 +86,12 @@ constructor(
 				handleTunnelStateChange(state.tunnelState, state.connectionData?.connectedAt)
 				val event = state.backendUiEvent
 				if (event is BackendUiEvent.Failure && event.reason is ErrorStateReason.NeedsRelaxedIndependenceCriteria) {
-					handleNeedsRelaxedIndependenceCriteria()
+					if (!nodeFamiliesEventHandled) {
+						nodeFamiliesEventHandled = true
+						handleNeedsRelaxedIndependenceCriteria()
+					}
+				} else {
+					nodeFamiliesEventHandled = false
 				}
 			}
 		}
@@ -162,9 +169,16 @@ constructor(
 
 	fun onNodeFamiliesConfirm() = viewModelScope.launch {
 		Timber.tag(TAG).i("NodeFamiliesModalConfirmed")
+		val action = pendingNodeFamiliesConfirmAction
+		pendingNodeFamiliesConfirmAction = null
 		runCatching {
-			backendManager.startTunnel(relaxGatewayIndependence = true)
+			action?.invoke()
 		}.onFailure { Timber.tag(TAG).e(it, "NodeFamiliesConnectFailed") }
+	}
+
+	fun onNodeFamiliesCancel() {
+		Timber.tag(TAG).i("NodeFamiliesModalCancelled")
+		pendingNodeFamiliesConfirmAction = null
 	}
 
 	private suspend fun resolveNodeFamiliesInteraction(onSilent: suspend () -> Unit) {
@@ -172,6 +186,7 @@ constructor(
 			vpnConfigRepository.getConfig().nodeFamiliesNotificationsEnabled
 		}.getOrDefault(true)
 		if (notificationsEnabled) {
+			pendingNodeFamiliesConfirmAction = onSilent
 			_events.tryEmit(MainUiEvent.ShowNodeFamiliesDialog)
 		} else {
 			onSilent()
