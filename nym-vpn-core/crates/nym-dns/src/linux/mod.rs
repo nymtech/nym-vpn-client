515a528b306206704d918adf2f2e26732b46f9e8
Andy Duplain <trojanfoe@gmail.com>
2026-07-07 17:13:27 +0100

    Ignore errors when last-ditch resetting DNS.

--- nym-vpn-core/crates/nym-dns/src/linux/mod.rs
+++ nym-vpn-core/crates/nym-dns/src/linux/mod.rs
@@ -75,7 +75,7 @@ impl super::DnsMonitorT for DnsMonitor {
             inner.reset().await?;
         } else if let Ok(mut inner) = DnsMonitorHolder::new() {
             // This helps reset the DNS configuration when the system is rebooted while the VPN is still connected.
-            inner.reset().await?;
+            let _ = inner.reset().await;
         }
         Ok(())
     }
