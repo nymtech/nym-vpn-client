f14b851ddbe1ee08278dd8f327932fa7a456b7dd
Rokas Ambrazevicius <2559446+rokas-ambrazevicius@users.noreply.github.com>
2026-07-08 15:51:46 +0300

    APPLE: add onboarding (#5770)

--- nym-vpn-apple/Home/Sources/26/AppFeature/AppFeatureView.swift
+++ nym-vpn-apple/Home/Sources/26/AppFeature/AppFeatureView.swift
@@ -53,6 +53,8 @@ public struct AppFeatureView: View {
     private var appearance: AppSetting.Appearance = .automatic
     @AppStorage(AppSettingKey.credenitalExists.rawValue)
     private var isCredentialImported = false
+    @AppStorage(AppSettingKey.onboardingDidDisplay.rawValue)
+    private var onboardingDidDisplay = false
 
     public init(viewModel: AppFeatureViewModel) {
         _viewModel = State(wrappedValue: viewModel)
@@ -152,6 +154,15 @@ private extension AppFeatureView {
                     }
                 }
             }
+            .overlay {
+                if !onboardingDidDisplay, !isCredentialImported {
+                    OnboardingView {
+                        appSettings.onboardingDidDisplay = true
+                    }
+                    .transition(.opacity)
+                }
+            }
+            .animation(.easeInOut, value: onboardingDidDisplay)
             .preferredColorScheme(appearance.colorScheme)
             .onAppear { wireMacOSDaemonNavigation() }
 #if os(macOS)
