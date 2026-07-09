e480c73035839ac4309a1359462339de268fed06
Andy Duplain <trojanfoe@gmail.com>
2026-07-09 16:02:17 +0100

    Tidy-up `get_paths()` a bit more.

--- nym-vpn-core/crates/nym-vpnd/src/paths.rs
+++ nym-vpn-core/crates/nym-vpnd/src/paths.rs
@@ -14,21 +14,23 @@ const DEFAULT_CONFIG_DIR: &str = "/etc/nym";
 
 #[cfg(windows)]
 pub fn get_paths() -> Paths {
-    let program_data_path = PathBuf::from(
+    let program_data_dir = PathBuf::from(
         std::env::var("ProgramData").unwrap_or_else(|_| "C:\\ProgramData".to_string()),
     );
 
+    let nym_vpnd_dir = program_data_dir.join("nym_vpnd");
+
     let data_dir = std::env::var("NYM_VPND_DATA_DIR")
         .map(PathBuf::from)
-        .unwrap_or_else(|_| program_data_path.join("nym-vpnd").join("data"));
+        .unwrap_or_else(|_| nym_vpnd_dir.join("data"));
 
     let config_dir = std::env::var("NYM_VPND_CONFIG_DIR")
         .map(PathBuf::from)
-        .unwrap_or_else(|_| program_data_path.join("nym-vpnd").join("config"));
+        .unwrap_or_else(|_| nym_vpnd_dir.join("config"));
 
     let log_dir = std::env::var("NYM_VPND_LOG_DIR")
         .map(PathBuf::from)
-        .unwrap_or_else(|_| program_data_path.join("nym-vpnd").join("log"));
+        .unwrap_or_else(|_| nym_vpnd_dir.join("log"));
 
     Paths {
         data_dir,
