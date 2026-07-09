4db770f0dca1276e97bf8016767a807c11fb38c5
jmwample <jmwample@users.noreply.github.com>
2026-07-09 12:16:40 -0600

    move indication to state machine so firewall is not reliant on http client

--- nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/connected_state.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/tunnel_state_machine/states/connected_state.rs
@@ -167,6 +167,7 @@ impl ConnectedState {
     ) -> Result<()> {
         let policy = params.as_policy();
 
+        nym_http_api_client::network_reconfigured();
         shared_state
             .firewall
             .apply_policy(policy)
