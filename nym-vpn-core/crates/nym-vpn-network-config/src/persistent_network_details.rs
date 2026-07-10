1ff63c565966a5a2763981ea604602f8fc427917
Andrej Mihajlov <andrej@nymtech.net>
2026-07-10 11:48:33 +0200

    Fix logging of cache deserialization errors

--- nym-vpn-core/crates/nym-vpn-network-config/src/persistent_network_details.rs
+++ nym-vpn-core/crates/nym-vpn-network-config/src/persistent_network_details.rs
@@ -62,7 +62,7 @@ impl PersistentNetworkDetails {
                 }
             }
             Err(err) if err.should_overwrite_file() => {
-                if err.is_file_not_found() {
+                if !err.is_file_not_found() {
                     trace_err_chain!(err, "failed to deserialize cache");
                 }
                 if network_name == "mainnet" {
