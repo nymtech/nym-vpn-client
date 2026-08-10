5b51c3b1f4df2ac39675e312689c3d324080e9a6
Andrej Mihajlov <andrej@nymtech.net>
2026-08-10 11:00:46 +0200

    Apple: display single version (#6032)
    * Commit automatic changes by Xcode 26.5
    
    * Move apply-version to actions
    
    * Embed real version info Info.plist

--- nym-vpn-apple/Settings/Sources/Settings/SettingsViewModel.swift
+++ nym-vpn-apple/Settings/Sources/Settings/SettingsViewModel.swift
@@ -43,7 +43,7 @@ import Theme
 #endif
 
     var versionTitle: String {
-        let base = "\("version".localizedString) \(AppVersionProvider.appVersion()) (\(AppVersionProvider.libVersion))"
+        let base = "\("version".localizedString) \(AppVersionProvider.realAppVersion())"
         let env = configurationManager.currentEnvString
         guard env != Env.mainnet.rawValue else { return base }
         return "\(base) - \(env)"
