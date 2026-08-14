57132935fd64a0471e639c02f5f97be0ecf1d66f
mrkiwi <sirakandrew@gmail.com>
2026-08-14 18:28:25 +0200

    Android: Handle NeedsDeviceLocation error

--- nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/util/extensions/UiExtensions.kt
+++ nym-vpn-android/app/src/main/java/net/nymtech/nymvpn/util/extensions/UiExtensions.kt
@@ -182,6 +182,7 @@ fun ErrorStateReason.toUserMessage(context: Context): String = when (this) {
 
 	is ErrorStateReason.Internal -> context.getString(R.string.unexpected_error, this.v1)
 	ErrorStateReason.NeedsRelaxedIndependenceCriteria -> context.getString(R.string.node_families_error_message)
+	ErrorStateReason.NeedsDeviceLocation -> context.getString(R.string.error_needs_device_location)
 }
 
 fun VpnException.toUserMessage(context: Context): String = when (this) {
