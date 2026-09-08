8f75e74ff577824df87b5add5bc0af0c56374b5d
Andy Duplain <trojanfoe@gmail.com>
2026-09-08 12:10:43 +0100

    Address review comments.

--- nym-vpn-core/crates/nym-vpn-lib/src/service/vpn_service.rs
+++ nym-vpn-core/crates/nym-vpn-lib/src/service/vpn_service.rs
@@ -2478,7 +2478,7 @@ impl NymVpnService {
         self.recents_manager
             .get_recent(tunnel_type)
             .await
-            .map_err(|error| ListGatewaysError::GetRecentGateways(Box::new(error)))
+            .map_err(ListGatewaysError::GetRecentGateways)
     }
 
     async fn handle_set_profile(&mut self, profile: Profile) {
